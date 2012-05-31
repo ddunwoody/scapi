cfbf4f3698b7b5a4662182b547aecb49dae269c3
Moriya Farbstein <moriyaw@gmail.com>
2012-05-31 06:18:20 +0000

    1. changed function name form getElement to generateElement. 2. added comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1294 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/DlogEllipticCurve.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/DlogEllipticCurve.java
@@ -2,30 +2,37 @@ package edu.biu.scapi.primitives.dlog;
 
 import java.math.BigInteger;
 
-import edu.biu.scapi.exceptions.UnInitializedException;
 
 /**
  * Marker interface. Every class that implements it is signed as elliptic curve.
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
-public interface DlogEllipticCurve extends DlogGroup{
-	
+public interface DlogEllipticCurve extends DlogGroup {
+
 	/**
 	 * Creates a point with the given x,y values 
 	 * @param x
 	 * @param y
 	 * @return the created ECPoint (x,y)
-	 * @throws UnInitializedException 
 	 */
-	public ECElement getElement(BigInteger x, BigInteger y);
+	public ECElement generateElement(BigInteger x, BigInteger y) throws IllegalArgumentException;
 	
 	/**
 	 * 
 	 * @return the infinity point of this dlog group
 	 */
 	public ECElement getInfinity();
+
+	/**
+	 * 
+	 * @return the name of the curve. For example - P-192.
+	 */
 	public String getCurveName();
-	
+
+	/**
+	 * 
+	 * @return the properties file where the curves are defined.
+	 */
 	public String getFileName();
 }
