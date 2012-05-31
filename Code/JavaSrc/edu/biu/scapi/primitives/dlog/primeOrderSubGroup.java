045aea2839e7578314c172e43c2bf530c6f3f0fd
Moriya Farbstein <moriyaw@gmail.com>
2012-05-31 06:48:46 +0000

    added marker interface for Dlog groups that has a prime order sub-group.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1300 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/primeOrderSubGroup.java
@@ -0,0 +1,11 @@
+package edu.biu.scapi.primitives.dlog;
+
+/**
+ * Marker interface for Dlog groups that has a prime order sub-group. 
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public interface primeOrderSubGroup extends DlogGroup{
+
+}
