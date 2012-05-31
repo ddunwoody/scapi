36509c2d5a2cbcac557675284016ded6533d3a30
Moriya Farbstein <moriyaw@gmail.com>
2012-05-31 06:21:01 +0000

    1. changed function name form getElement to generateElement. 2. deleted unused import.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1296 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/DlogZp.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/DlogZp.java
@@ -2,8 +2,6 @@ package edu.biu.scapi.primitives.dlog;
 
 import java.math.BigInteger;
 
-import edu.biu.scapi.exceptions.UnInitializedException;
-
 /**
  * Marker interface. Every class that implements it is signed as Zp*
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
@@ -15,7 +13,6 @@ public interface DlogZp extends DlogGroup{
 	 * Creates an element with the given x value 
 	 * @param x
 	 * @return the created element
-	 * @throws UnInitializedException 
 	 */
-	public ZpElement getElement (BigInteger x, Boolean bCheckMembership);
+	public ZpElement generateElement (BigInteger x, Boolean bCheckMembership);
 }
