cc0a77ae06d5f3497523ff4f62913e496569e83f
Moriya Farbstein <moriyaw@gmail.com>
2012-05-31 06:32:20 +0000

    added interfaces for elliptic curve point over every field. (Currently Fp and F2m). These interfaces are used when there is a functionality common to the field of the elliptic curve. Every concrete class of a point must implement the appropriate interface.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1298 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/ECF2mPoint.java
@@ -0,0 +1,11 @@
+package edu.biu.scapi.primitives.dlog;
+
+/**
+ * Marker interface for elliptic curve point over the field F2m.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public interface ECF2mPoint extends ECElement{
+
+}
---
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/ECFpPoint.java
@@ -0,0 +1,11 @@
+package edu.biu.scapi.primitives.dlog;
+
+/**
+ * Marker interface for elliptic curve point over the field Fp.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public interface ECFpPoint extends ECElement{
+
+}
