7f7a3f18fa32cfe64675c492a0399d3f1122f4d3
Moriya Farbstein <moriyaw@gmail.com>
2012-06-04 06:46:21 +0000

    added changes after adding RSA Pss abstract class: 1. This class extends the abs class instead of implementing the interface. 2. the functions isKeySet, getAlgorithmName and generateKey moved to the abstract class. 3. the attribute isKeySet moved to the abstract class.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1333 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/BcRSAPss.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/BcRSAPss.java
@@ -1,19 +1,13 @@
 package edu.biu.scapi.midLayer.asymmetricCrypto.digitalSignature;
 
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-import java.security.spec.AlgorithmParameterSpec;
-import java.security.spec.InvalidParameterSpecException;
-import java.security.spec.RSAKeyGenParameterSpec;
 
 import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.CryptoException;
@@ -36,11 +30,10 @@ import edu.biu.scapi.tools.Translation.BCParametersTranslator;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
-public class BcRSAPss implements RSABasedSignature{
+public class BcRSAPss extends RSAPssAbs{
 
 	private RSAPublicKey publicKey;
-	private RSAPrivateKey privateKey;
-	private boolean isKeySet = false;				//set to false until setKey is called
+	private RSAPrivateKey privateKey;				//set to false until setKey is called
 	private CipherParameters privateParameters;		//parameters contains the private key and the random
 	private CipherParameters publicParameters;		//parameters contains the public key and the random
 	private Digest digest;							//the underlying hash to use
@@ -134,20 +127,7 @@ public class BcRSAPss implements RSABasedSignature{
 		
 	}
 
-	@Override
-	public boolean isKeySet() {
-		
-		return isKeySet;
-	}
-
-	/**
-	 * @return this algorithm name - "RSA/PSS"
-	 */
-	@Override
-	public String getAlgorithmName() {
-		
-		return "RSA/PSS";
-	}
+	
 
 	/**
 	 * Signs the given message
@@ -215,41 +195,6 @@ public class BcRSAPss implements RSABasedSignature{
 		
 	}
 
-	/**
-	 * Generate an RSA key pair using the given source of randomness.
-	 * @param keyParams RSAKeyGenParameterSpec
-	 * @return KeyPair contains keys for this RSAPss object
-	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAKeyGenParameterSpec
-	 */
-	@Override
-	public KeyPair generateKey(AlgorithmParameterSpec keyParams)
-			throws InvalidParameterSpecException {
-		//if keyParams is not the expected, throw exception
-		if (!(keyParams instanceof RSAKeyGenParameterSpec)){
-			throw new InvalidParameterSpecException("keyParams should be instance of RSAKeyGenParameterSpec");
-		}
-		
-		try {
-			//generates keys using the KeyPairGenerator
-			KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-			generator.initialize(keyParams, random);
-			return generator.generateKeyPair(); 
-		} catch(InvalidAlgorithmParameterException e){
-			//shouldn't occur since the parameterSpec is valid for RSA
-		} catch (NoSuchAlgorithmException e) {
-			//shouldn't occur since RSA is a valid algorithm
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	/**
-	 * This function is not supported in this class. 
-	 * Use generateKey(AlgorithmParameterSpec keyParams) instead.
-	 */
-	@Override
-	public KeyPair generateKey() {
-		throw new UnsupportedOperationException("To generate keys for this RSAPss use generateKey(AlgorithmParameterSpec keyParams) function");
-	}
+	
 
 }
