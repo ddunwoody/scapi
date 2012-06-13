c3d0b5a75e026a9e5622a0831d630653390368a5
Moriya Farbstein <moriyaw@gmail.com>
2012-06-13 11:34:03 +0000

    1. changed the name to ByteArrayPlaintext. This name is more specific. 2. added implementation of equals function.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1356 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/plaintext/ByteArrayPlaintext.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/plaintext/ByteArrayPlaintext.java
@@ -1,21 +1,39 @@
-/**
- * 
- */
 package edu.biu.scapi.midLayer.plaintext;
 
 /**
- * This class holds the basic data of any plain-text.
+ * This class holds the plaintext as a ByteArray.
+ * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  *
  */
-public class BasicPlaintext implements Plaintext {
+public class ByteArrayPlaintext implements Plaintext {
 	private byte[] text = null;
 	
-	public BasicPlaintext (byte[] text) {
+	public ByteArrayPlaintext (byte[] text) {
 		this.text = text;
 	}
 	
 	public byte[] getText() {
 		return text;
+	}
+	
+	@Override
+	public boolean equals(Object plaintext){
+		if (!(plaintext instanceof ByteArrayPlaintext)){
+			return false;
+		}
+		byte[] text2 = ((ByteArrayPlaintext) plaintext).getText();
+		
+		if (text.length != text2.length){
+			return false;
+		}
+		
+		for (int i=0; i<text.length; i++){
+			if (text[i] != text2[i]){
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
