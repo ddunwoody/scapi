e33ab8887db0d118d21506674b120dbd9e98b797
Moriya Farbstein <moriyaw@gmail.com>
2012-06-13 09:31:41 +0000

    1. added and fixed comments. 2. changed the exception thrown in setKey functions to be invalidKeyException instead of illegalArgumentException. 3. from now on we don't kep the public and private keys, just the cipherparameters that have been generated from them. 4. added a check in encrypt function if the given plaintext is an instance of ByteArrayPlaintext.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1347 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/BcRSAOaep.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/BcRSAOaep.java
@@ -1,5 +1,6 @@
 package edu.biu.scapi.midLayer.asymmetricCrypto.encryption;
 
+import java.security.InvalidKeyException;
 import java.security.KeyException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -15,146 +16,154 @@ import org.bouncycastle.crypto.engines.RSABlindedEngine;
 import edu.biu.scapi.exceptions.ScapiRuntimeException;
 import edu.biu.scapi.midLayer.ciphertext.BasicAsymCiphertext;
 import edu.biu.scapi.midLayer.ciphertext.Ciphertext;
-import edu.biu.scapi.midLayer.plaintext.BasicPlaintext;
+import edu.biu.scapi.midLayer.plaintext.ByteArrayPlaintext;
 import edu.biu.scapi.midLayer.plaintext.Plaintext;
 import edu.biu.scapi.tools.Translation.BCParametersTranslator;
 
 /**
- * This class performs the RSA-OAEP encryption and decryption scheme.
- * By definition, this encryption scheme is CCA-secure.
+ * RSA-OAEP encryption scheme based on BC library's implementation.
+ * By definition, this encryption scheme is CCA-secure and NonMalleable.
  * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
 public class BcRSAOaep extends RSAOaepAbs {
 	
-	private OAEPEncoding bcBlockCipher;				//the underlying BC OAEP encoding
-	private CipherParameters privateParameters;		//parameters contains the private key and the random
-	private CipherParameters publicParameters;		//parameters contains the public key and the random
+	private OAEPEncoding bcBlockCipher;				//The underlying OAEP encoding of BC.
+	private CipherParameters privateParameters;		//Parameters contains the private key and the random.
+	private CipherParameters publicParameters;		//Parameters contains the public key and the random.
 	private boolean forEncryption = true;
-	private RSAPrivateKey privateKey;				
-	private RSAPublicKey publicKey;					
-	
 
 	
 	/**
-	 * Default constructor
+	 * Default constructor. Uses default implementation of SecureRandom as source of randomness.
 	 */
 	public BcRSAOaep(){
+		//Calls the constructor with default SecureRandom implementation.
 		this(new SecureRandom());
 	}
 	
 	/**
-	 * Constructor that lets the user choose the source of randomness
-	 * @param random
+	 * Constructor that lets the user choose the source of randomness.
+	 * @param random source of randomness.
 	 */
 	public BcRSAOaep(SecureRandom random){
 		this.random = random;
-		//creates the OAEP encoding with RSABlindedEngine of BC
+		//Creates the OAEP encoding with RSABlindedEngine of BC.
 		this.bcBlockCipher = new OAEPEncoding(new RSABlindedEngine());
 
 	}
 	/**
-	 * Set this RSAOAEP encryption scheme with a Public/Private key pair.
-	 * In thiscase the user can encrypt and decrypt messages.
-	 * @param publicKey should be RSAPublicKey
-	 * @param privateKey should be RSAPrivateKey
+	 * Sets this RSAOAEP encryption scheme with a (Public,Private) key pair.
+	 * In this case the user can encrypt and decrypt messages.
+	 * @param publicKey should be RSAPublicKey.
+	 * @param privateKey should be RSAPrivateKey.
+	 * @throws InvalidKeyException if the given keys are not instances of RSA keys.
 	 */
 	@Override
-	public void setKey(PublicKey publicKey, PrivateKey privateKey) {
-		//key should be RSA keys
+	public void setKey(PublicKey publicKey, PrivateKey privateKey) throws InvalidKeyException{
+		//Keys should be RSA keys.
 		if(!(publicKey instanceof RSAPublicKey)){
-			throw new IllegalArgumentException("keys should be instances of RSA keys");
+			throw new InvalidKeyException("keys should be instances of RSA keys");
 		}
 		if(privateKey!= null && !(privateKey instanceof RSAPrivateKey)){
-				throw new IllegalArgumentException("keys should be instances of RSA keys");
+				throw new InvalidKeyException("keys should be instances of RSA keys");
 		}
-		//set the parameters
-		this.publicKey = (RSAPublicKey) publicKey;
-		this.privateKey = (RSAPrivateKey) privateKey; //What happens if null?
 				
-		//create BC objects and initialize them
-		initBCCipher();
+		//Creates BC objects and initializes them.
+		initBCCipher((RSAPublicKey) publicKey, (RSAPrivateKey)privateKey);
 		isKeySet = true;
 	}
 
 
 
 	/**
-	 * Set this RSAOAEP encryption scheme only with public key.
+	 * Sets this RSAOAEP encryption scheme only with public key.
 	 * In this case the user can encrypt messages but can not decrypt messages.
 	 * @param publicKey should be RSAPublicKey
+	 * @throws InvalidKeyException if the given key is not instance of RSAPublickey.
 	 */
 	@Override
-	public void setKey(PublicKey publicKey) {
+	public void setKey(PublicKey publicKey) throws InvalidKeyException {
 		setKey(publicKey, null);
 	}
+	
 	/**
-	 * Creates BC OAEPEncoding with BC RSABlindedEngine, translate the keys and random to BC CipherParameters
-	 * and initialize BC object in encrypt mode.
-	 * In order to decript, the decrypt function initialize them again to decrypt mode.
+	 * Creates BC OAEPEncoding with BC RSABlindedEngine, translates the keys and random to BC CipherParameters
+	 * and initializes BC object in encrypt mode.
+	 * In order to decrypt, the decrypt function initializes them again to decrypt mode.
+	 * @param privateKey 
+	 * @param publicKey 
 	 */
-	private void initBCCipher(){
+	private void initBCCipher(RSAPublicKey publicKey, RSAPrivateKey privateKey){
 		
-		//translate the keys and random to BC parameters
-		privateParameters = BCParametersTranslator.getInstance().translateParameter(privateKey, random);
+		//Translates the keys and random to BC parameters.
+		if (privateKey != null){
+			privateParameters = BCParametersTranslator.getInstance().translateParameter(privateKey, random);
+		}
 		publicParameters = BCParametersTranslator.getInstance().translateParameter(publicKey, random);
-		//initialize the OAEP object with the cipherPerameters and for encryption
+		//Initializes the OAEP object with the cipherPerameters and for encryption.
 		bcBlockCipher.init(forEncryption, publicParameters);
 	}
 	
 	
 	/**
 	 * Encrypts the given plaintext according to the RSAOAEP algorithm using BC OAEPEncoding.
-	 * @param plaintext the plaintext to encrypt
+	 * @param plaintext the plaintext to encrypt. MUST be an instance of ByteArrayPlaintext.
 	 * @return Ciphertext contains the encrypted plaintext
+	 * @throws IllegalStateException if no public key was set.
+	 * @throws IllegalArgumentException if the given Plaintext is not instance of ByteArrayPlaintext.
+	 * @throws ScapiRuntimeException if the exception InvalidCipherTextException of BC is thrown. This exception is thrown when there is something unexpected in a message.
 	 */
 	@Override
-	public Ciphertext encrypt(Plaintext plaintext) {		
-		//if the underlying BC object used to the encryption is in decrypt mode - change it
-		//TODO make sure that this mechanism covers all possibilities.
+	public Ciphertext encrypt(Plaintext plaintext){	
+		//If there is no public key can not encrypt, throws exception.
+		if (!isKeySet()){
+			throw new IllegalStateException("in order to encrypt a message this object must be initialized with public key");
+		}
+		
+		if (!(plaintext instanceof ByteArrayPlaintext)){
+			throw new IllegalArgumentException("plaintext should be instance of ByteArrayPlaintext");
+		}
+		//If the underlying BC object used to the encryption is in decrypt mode - changes it.
 		if (!forEncryption){
 			forEncryption = true;
 			bcBlockCipher.init(forEncryption, publicParameters);
 		}
 		
-		byte[] plaintextBytes = ((BasicPlaintext) plaintext).getText(); //get the plaintext bytes
-		int inputBlockSize = bcBlockCipher.getInputBlockSize();
-		
-		//calculate the number of whole blocks
-		//int numberBlocksInCipher = plaintextBytes.length / inputBlockSize;
-		//int remainder = plaintextBytes.length % inputBlockSize;
+		byte[] plaintextBytes = ((ByteArrayPlaintext) plaintext).getText(); //Gets the plaintext bytes.
 		
 		byte[] ciphertext;
 		try {
-			//encrypt block using BC OAEP object
+			//Encrypts the plaintext using BC OAEP object.
 			ciphertext = bcBlockCipher.encodeBlock(plaintextBytes, 0, plaintextBytes.length);
 		} catch (InvalidCipherTextException e) {
 			throw new ScapiRuntimeException(e.getMessage());
 		}
 
-		//return a ciphertext with the encrypted plaintext
+		//Returns a ciphertext with the encrypted plaintext.
 		return new BasicAsymCiphertext(ciphertext);
 	}
 
 	/**
 	 * Decrypts the given ciphertext according to the RSAOAEP algorithm using BC OAEPEncoding.
-	 * @param cipher the ciphertext to decrypt
-	 * @return Plaintext contains the decrypted ciphertext
-	 * @throws KeyException 
+	 * @param cipher the ciphertext to decrypt. Must be an instance of BasicAsymCiphertext.
+	 * @return Plaintext contains the decrypted ciphertext.
+	 * @throws KeyException if no private key was set.
+	 * @throws IllegalArgumentException if the given cipher is not instance of BasicAsymCiphertext.
+	 * @throws ScapiRuntimeException if the exception InvalidCipherTextException of BC is thrown. This exception is thrown when there is something unexpected in a message.
 	 */
 	@Override
-	public Plaintext decrypt(Ciphertext cipher) throws KeyException {
-		//if there is no private key can not decrypt, throw exception
-		if (privateKey == null){
+	public Plaintext decrypt(Ciphertext cipher) throws KeyException{
+		//If there is no private key can not decrypt, throws exception.
+		if (privateParameters == null){
 			throw new KeyException("in order to decrypt a message, this object must be initialized with private key");
 		}
-		//cipher must be of type BasicAsymCiphertext
+		//Cipher must be of type BasicAsymCiphertext.
 		if (!(cipher instanceof BasicAsymCiphertext)){
 			throw new IllegalArgumentException("The ciphertext has to be of type BasicAsymCiphertext");
 		}
-		//if the underlying BC object used to the decryption is in encrypt mode - change it
-		//TODO make sure that this mechanism covers all possibilities.
+		//If the underlying BC object used to the decryption is in encrypt mode - changes it.
 		if (forEncryption){
 			forEncryption = false;
 			bcBlockCipher.init(forEncryption, privateParameters);
@@ -164,13 +173,13 @@ public class BcRSAOaep extends RSAOaepAbs {
 
 		byte[] plaintext;
 		try {
-			//decrypt block using BC OAEP object
+			//Decrypts the ciphertext using BC OAEP object.
 			plaintext = bcBlockCipher.decodeBlock(ciphertext, 0, ciphertext.length);
 		} catch (InvalidCipherTextException e) {
 			throw new ScapiRuntimeException(e.getMessage());
 		}
-		//return a plaintext with the decrypted ciphertext
-		return new BasicPlaintext(plaintext);
+		//Returns a plaintext with the decrypted ciphertext.
+		return new ByteArrayPlaintext(plaintext);
 	}
 	
 }
