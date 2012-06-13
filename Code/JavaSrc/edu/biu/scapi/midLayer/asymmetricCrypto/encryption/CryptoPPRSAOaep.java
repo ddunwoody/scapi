bf1cd6aa676c3ee0f3f0ad403c353907abab6999
Moriya Farbstein <moriyaw@gmail.com>
2012-06-13 09:42:19 +0000

    1. added and fixed comments. 2. deleted the isKeySet member. There is such boolean already in RSAOaepAbs. 3. deleted unused functions: getRSAModulus and getPubExponent. 4. creation of native object is done in the setKey function and not in the constructor. There is no need to create an object if setKey will not be call. 5. changed the setKey functions. Now the setKey that receives publicKey call the other function with null private key. 6. added a check in encrypt function if the given plaintext is an instance of ByteArrayPlaintext. 7. added a check in encrypt function if the public key was set.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1348 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/CryptoPPRSAOaep.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/CryptoPPRSAOaep.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package edu.biu.scapi.midLayer.asymmetricCrypto.encryption;
 
 import java.math.BigInteger;
@@ -16,122 +13,169 @@ import java.security.interfaces.RSAPublicKey;
 
 import edu.biu.scapi.midLayer.ciphertext.BasicAsymCiphertext;
 import edu.biu.scapi.midLayer.ciphertext.Ciphertext;
-import edu.biu.scapi.midLayer.plaintext.BasicPlaintext;
+import edu.biu.scapi.midLayer.plaintext.ByteArrayPlaintext;
 import edu.biu.scapi.midLayer.plaintext.Plaintext;
 
 /**
+ * RSA-OAEP encryption scheme based on Crypto++ library's implementation.
+ * By definition, this encryption scheme is CCA-secure and NonMalleable.
+ * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  *
  */
 public class CryptoPPRSAOaep extends RSAOaepAbs {
 	
-	private long encryptor;
-	private long decryptor;
+	private long encryptor; //Pointer to encryptor object (RSAES_OAEP_SHA_Encryptor) in Crypto++.
+	private long decryptor; //Pointer to decryptor object (RSAES_OAEP_SHA_Decryptor) in Crypto++.
 	
-	private boolean isKeySet;
-
+	//Native functions that compute the encryption scheme functionality.
 	private native long createRSAEncryptor();
 	private native long createRSADecryptor();
 	private native void initRSAEncryptor(long encryptor, byte[] modulus, byte[] exponent);
+	//Used to initialize the decryptor object if the given private key contains N, e, d.
 	private native void initRSADecryptor(long decryptor, byte[] modulus, byte[] exponent, byte[] d);
+	//Used to initialize the decryptor object if the given private key is RSA-CRT key which contain N, e, d, p, q, dp, dq, crt.
 	private native void initRSACrtDecryptor(long decryptor, byte[] modulus, byte[] exponent, byte[] d, byte[] p, byte[] q, byte[] dp, byte[]dq, byte[] crt);
-	
-	private native byte[] getRSAModulus(long encryptor);
-	private native byte[] getPubExponent(long encryptor);
 	
 	private native byte[] doEncrypt(long encryptor, byte[] plaintext);
 	private native byte[] doDecrypt(long decryptor, byte[] ciphertext);
 	
+	
+	/**
+	 * Default constructor. Uses default implementation of SecureRandom as source of randomness.
+	 */
 	public CryptoPPRSAOaep(){
+		//Calls the constructor with default SecureRandom implementation.
 		this(new SecureRandom());
 	}
 	
+	/**
+	 * Constructor that lets the user choose the source of randomness.
+	 * @param random source of randomness.
+	 */
 	public CryptoPPRSAOaep(SecureRandom secureRandom){
 		this.random = secureRandom;
-		this.encryptor = createRSAEncryptor();
-		this.decryptor = createRSADecryptor();
 	}
 		
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#setKey(java.security.PublicKey, java.security.PrivateKey)
+	/**
+	 * Sets this RSAOAEP encryption scheme with a (Public,Private) key pair.
+	 * In this case the user can encrypt and decrypt messages.
+	 * @param publicKey should be RSAPublicKey.
+	 * @param privateKey should be RSAPrivateKey.
+	 * @throws InvalidKeyException if the given keys are not instances of RSA keys.
 	 */
 	@Override
 	public void setKey(PublicKey publicKey, PrivateKey privateKey) throws InvalidKeyException {
 		
-		if (!(publicKey instanceof RSAPublicKey) || !(privateKey instanceof RSAPrivateKey)) {
-			throw new InvalidKeyException("Key type doesn't match the RSA encryption scheme type");
+		//Keys should be RSA keys.
+		if(!(publicKey instanceof RSAPublicKey)){
+			throw new InvalidKeyException("keys should be instances of RSA keys");
 		}
-			
-		/* gets the values of modulus (N), pubExponent (e), privExponent (d)*/
+		if(privateKey!= null && !(privateKey instanceof RSAPrivateKey)){
+				throw new InvalidKeyException("keys should be instances of RSA keys");
+		}
+		
+		//Creates the native encryptor object.
+		this.encryptor = createRSAEncryptor();
+		// Gets the values of modulus (N), pubExponent (e), 
 		BigInteger pubExponent = ((RSAPublicKey) publicKey).getPublicExponent();
-		BigInteger privExponent = ((RSAPrivateKey) privateKey).getPrivateExponent();
 		BigInteger modN = ((RSAKey) publicKey).getModulus();
 		
-		//if private key is CRT private key
-		if (privateKey instanceof RSAPrivateCrtKey)
-		{
-			//gets all the crt parameters
-			RSAPrivateCrtKey key = (RSAPrivateCrtKey) privateKey;
-			BigInteger p = key.getPrimeP();
-			BigInteger q = key.getPrimeQ();
-			BigInteger dp = key.getPrimeExponentP();
-			BigInteger dq = key.getPrimeExponentQ();
-			BigInteger crt = key.getCrtCoefficient();
-			
-			//initializes the native object
-			initRSACrtDecryptor(decryptor, modN.toByteArray(), pubExponent.toByteArray(), privExponent.toByteArray(), 
-					p.toByteArray(), q.toByteArray(), dp.toByteArray(), dq.toByteArray(), crt.toByteArray());
-			
-		//if private key is key with N, e, d
-		} else {
-			
-			//init the native object with the RSA parameters - n, e, d
-			initRSADecryptor(decryptor, modN.toByteArray(), pubExponent.toByteArray(), privExponent.toByteArray());
+		//Initializes the native encryptor object with N, e.
+		initRSAEncryptor(encryptor, modN.toByteArray(), pubExponent.toByteArray());
+		
+		if (privateKey != null){
+			//Creates the native decryptor object.
+			this.decryptor = createRSADecryptor();
+			//Gets the value of privExponent(d)
+			BigInteger privExponent = ((RSAPrivateKey) privateKey).getPrivateExponent();
+			//If private key is CRT private key.
+			if (privateKey instanceof RSAPrivateCrtKey)
+			{
+				//Gets all the crt parameters.
+				RSAPrivateCrtKey key = (RSAPrivateCrtKey) privateKey;
+				BigInteger p = key.getPrimeP();
+				BigInteger q = key.getPrimeQ();
+				BigInteger dp = key.getPrimeExponentP();
+				BigInteger dq = key.getPrimeExponentQ();
+				BigInteger crt = key.getCrtCoefficient();
+				
+				//Initializes the native decryptor object.
+				initRSACrtDecryptor(decryptor, modN.toByteArray(), pubExponent.toByteArray(), privExponent.toByteArray(), 
+						p.toByteArray(), q.toByteArray(), dp.toByteArray(), dq.toByteArray(), crt.toByteArray());
+				
+			//If private key is key with N, e, d.
+			} else {
+				
+				//Initializes the native decryptor object with the RSA parameters - n, e, d.
+				initRSADecryptor(decryptor, modN.toByteArray(), pubExponent.toByteArray(), privExponent.toByteArray());
+			}
 		}
 		
-		initRSAEncryptor(encryptor, modN.toByteArray(), pubExponent.toByteArray());
 		isKeySet = true;
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#setKey(java.security.PublicKey)
+	/**
+	 * Sets this RSAOAEP encryption scheme only with public key.
+	 * In this case the user can encrypt messages but can not decrypt messages.
+	 * @param publicKey should be RSAPublicKey.
+	 * @throws InvalidKeyException if the given key is not instance of RSAPublickey.
 	 */
 	@Override
 	public void setKey(PublicKey publicKey) throws InvalidKeyException {
-		if(!(publicKey instanceof RSAPublicKey)){
-			throw new InvalidKeyException("Key type doesn't match the RSA encryption scheme type");
-		}
-	
-		RSAPublicKey pub = (RSAPublicKey) publicKey;
-		BigInteger pubExponent = pub.getPublicExponent();
-		BigInteger modN = pub.getModulus();
-		
-		initRSAEncryptor(encryptor, modN.toByteArray(), pubExponent.toByteArray());
-		isKeySet = true;
+		setKey(publicKey, null);
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#encrypt(edu.biu.scapi.midLayer.plaintext.Plaintext)
+	/**
+	 * Encrypts the given plaintext according to the RSAOAEP algorithm using Crypto++ implementation.
+	 * @param plaintext the plaintext to encrypt. MUST be an instance of ByteArrayPlaintext.
+	 * @return Ciphertext contains the encrypted plaintext.
+	 * @throws IllegalStateException if no public key was set.
+	 * @throws IllegalArgumentException if the given Plaintext is not instance of ByteArrayPlaintext.
+	 * 
 	 */
 	@Override
-	public Ciphertext encrypt(Plaintext plainText) {
+	public Ciphertext encrypt(Plaintext plaintext){
+		// If there is no public key can not encrypt, throws exception.
+		if (!isKeySet()){
+			throw new IllegalStateException("in order to encrypt a message this object must be initialized with public key");
+		}
 		
-		byte[] ciphertext = doEncrypt(encryptor, ((BasicPlaintext)plainText).getText());
-		//return a ciphertext with the encrypted plaintext
+		if (!(plaintext instanceof ByteArrayPlaintext)){
+			throw new IllegalArgumentException("plaintext should be instance of ByteArrayPlaintext");
+		}
+		
+		// Calls native function that computes the encryption.
+		byte[] ciphertext = doEncrypt(encryptor, ((ByteArrayPlaintext)plaintext).getText());
+		// Returns a ciphertext with the encrypted plaintext.
 		return new BasicAsymCiphertext(ciphertext);
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.encryption.AsymmetricEnc#decrypt(edu.biu.scapi.midLayer.ciphertext.Ciphertext)
+	/**
+	 * Decrypts the given ciphertext according to the RSAOAEP algorithm using Crypto++ implementation.
+	 * @param cipher the ciphertext to decrypt. Must be an instance of BasicAsymCiphertext.
+	 * @return Plaintext contains the decrypted ciphertext.
+	 * @throws KeyException if no private key was set.
+	 * @throws IllegalArgumentException if the given cipher is not instance of BasicAsymCiphertext.
 	 */
 	@Override
-	public Plaintext decrypt(Ciphertext cipher) throws KeyException {
+	public Plaintext decrypt(Ciphertext cipher) throws KeyException{
 		
+		// If there is no private key can not decrypt, throws exception.
+		if (decryptor == 0){
+			throw new KeyException("in order to decrypt a message, this object must be initialized with private key");
+		}
+		// Cipher must be of type BasicAsymCiphertext.
+		if (!(cipher instanceof BasicAsymCiphertext)){
+			throw new IllegalArgumentException("The ciphertext has to be of type BasicAsymCiphertext");
+		}
+		
+		// Calls native function that computes the decryption.
 		byte[] plaintext =  doDecrypt(decryptor, ((BasicAsymCiphertext)cipher).getBytes());
-		return new BasicPlaintext(plaintext);
+		return new ByteArrayPlaintext(plaintext);
 	}
 	
-	 static {
-	        System.loadLibrary("CryptoPPJavaInterface");
-	 }
+	static {
+	       System.loadLibrary("CryptoPPJavaInterface");
+	}
 }
