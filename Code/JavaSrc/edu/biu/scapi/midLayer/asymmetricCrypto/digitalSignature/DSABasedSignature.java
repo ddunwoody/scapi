8b7ed60dd936c2bde891627b4dc7d22304dd76d3
Moriya Farbstein <moriyaw@gmail.com>
2012-06-18 06:34:49 +0000

    added comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1365 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/DSABasedSignature.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/DSABasedSignature.java
@@ -3,6 +3,12 @@ package edu.biu.scapi.midLayer.asymmetricCrypto.digitalSignature;
 import edu.biu.scapi.securityLevel.ACMA;
 import edu.biu.scapi.securityLevel.UnlimitedTimes;
 
+/**
+ * General interface for DSA signature scheme. Every concrete implementation of DSA signature should implement this interface.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
 public interface DSABasedSignature extends DigitalSignature, UnlimitedTimes, ACMA{
 
 }
--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/RSABasedSignature.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/RSABasedSignature.java
@@ -3,6 +3,13 @@ package edu.biu.scapi.midLayer.asymmetricCrypto.digitalSignature;
 import edu.biu.scapi.securityLevel.ACMA;
 import edu.biu.scapi.securityLevel.UnlimitedTimes;
 
+/**
+ * General interface for RSA PSS signature scheme. Every concrete implementation of RSA PSS signature should implement this interface.
+ * The RSA PSS (Probabilistic Signature Scheme) is a provably secure way of creating signatures with RSA.
+ *  
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
 public interface RSABasedSignature extends DigitalSignature, UnlimitedTimes, ACMA{
 
 }
