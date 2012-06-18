c019ce6c43cfa7d9e22fee3c01fe835d720c8aee
Moriya Farbstein <moriyaw@gmail.com>
2012-06-18 06:36:03 +0000

    1. fixed and added comments. 2. added public key as class member and getPublicKey function.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1366 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/RSAPssAbs.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/digitalSignature/RSAPssAbs.java
@@ -4,26 +4,40 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.InvalidParameterSpecException;
-import java.security.spec.RSAKeyGenParameterSpec;
 
 /**
- * General interface for RSA PSS signature scheme. Every concrete implementation of RSA PSS signature should implement this interface.
+ * Abstract class for RSA PSS signature scheme. This class implements some common functionality of RSA signature scheme.
  * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
 public abstract class RSAPssAbs implements RSABasedSignature{
 
-	protected SecureRandom random;		//source of randomness
-	protected boolean isKeySet = false;
-	
+	protected SecureRandom random;		//Source of randomness
+	protected boolean isKeySet;
+	protected RSAPublicKey publicKey;
 	
 	@Override
 	public boolean isKeySet() {
-		return isKeySet();
+		return isKeySet;
+	}
+	
+	/**
+	 * Returns the PublicKey of this RSA encryption scheme.
+	 * @return the RSAPublicKey
+	 * @throws IllegalStateException if no public key was set.
+	 */
+	public PublicKey getPublicKey(){
+		if (!isKeySet()){
+			throw new IllegalStateException("no PublicKey was set");
+		}
+		
+		return publicKey;
 	}
 	
 	/**
@@ -36,28 +50,25 @@ public abstract class RSAPssAbs implements RSABasedSignature{
 	}
 	
 	/**
-	 * Generate an RSA key pair using the given source of randomness.
-	 * @param keyParams RSAKeyGenParameterSpec
-	 * @return KeyPair contains keys for this RSAPss object
-	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAKeyGenParameterSpec
+	 * Generate an RSA key pair using the given parameters.
+	 * @param keyParams RSAKeyGenParameterSpec.
+	 * @return KeyPair contains keys for this RSAPss object.
+	 * @throws InvalidParameterSpecException if keyParams is not instance of RSAKeyGenParameterSpec.
 	 */
 	@Override
 	public KeyPair generateKey(AlgorithmParameterSpec keyParams)
 			throws InvalidParameterSpecException {
-		//if keyParams is not the expected, throw exception
-		if (!(keyParams instanceof RSAKeyGenParameterSpec)){
-			throw new InvalidParameterSpecException("keyParams should be instance of RSAKeyGenParameterSpec");
-		}
 		
 		try {
-			//generates keys using the KeyPairGenerator
+			//Generates keys using the KeyPairGenerator.
 			KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
 			generator.initialize(keyParams, random);
 			return generator.generateKeyPair(); 
 		} catch(InvalidAlgorithmParameterException e){
-			//shouldn't occur since the parameterSpec is valid for RSA
+			//Throws the same exception with different message.
+			throw new InvalidParameterSpecException("keyParams should be instance of RSAKeyGenParameterSpec");
 		} catch (NoSuchAlgorithmException e) {
-			//shouldn't occur since RSA is a valid algorithm
+			//Shouldn't occur since RSA is a valid algorithm.
 			e.printStackTrace();
 		}
 		return null;
@@ -66,6 +77,7 @@ public abstract class RSAPssAbs implements RSABasedSignature{
 	/**
 	 * This function is not supported in this class. 
 	 * Use generateKey(AlgorithmParameterSpec keyParams) instead.
+	 * @throws UnsupportedOperationException
 	 */
 	@Override
 	public KeyPair generateKey() {
