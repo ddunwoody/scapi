6a9e9bebd460b6f04309708547e7bc0e677f518f
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-07-24 12:45:12 +0000

    1) Removed intialization of field util before constructor. 2) Have only one constructor do all the work and all the other call that one. 3) Changed names of conversion functions to encode... and decode...Since F2m does not have implementation for encoding and decoding they return null. 4) Added byte[] mapAnyGroupElementToByteArray(GroupElement groupElement). 5) Have getGroupType() call util.getGroupType().
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1391 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/primitives/dlog/bc/BcDlogECFp.java
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/bc/BcDlogECFp.java
@@ -13,6 +13,8 @@ import edu.biu.scapi.primitives.dlog.ECElement;
 import edu.biu.scapi.primitives.dlog.ECFpUtility;
 import edu.biu.scapi.primitives.dlog.GroupElement;
 import edu.biu.scapi.primitives.dlog.groupParams.ECFpGroupParams;
+import edu.biu.scapi.primitives.dlog.groupParams.GroupParams;
+import edu.biu.scapi.primitives.dlog.miracl.ECFpPointMiracl;
 import edu.biu.scapi.securityLevel.DDH;
 
 /**
@@ -22,7 +24,8 @@ import edu.biu.scapi.securityLevel.DDH;
  */
 public class BcDlogECFp extends BcAdapterDlogEC implements DlogECFp, DDH {
 	
-	private ECFpUtility util = new ECFpUtility();
+	private ECFpUtility util;
+	
 	
 	/**
 	 * Default constructor. Initializes this object with P-192 NIST curve.
@@ -42,56 +45,48 @@ public class BcDlogECFp extends BcAdapterDlogEC implements DlogECFp, DDH {
 	 * @throws IllegalAccessException
 	 */
 	public BcDlogECFp(String curveName) throws IllegalArgumentException, IOException {
-		
-		Properties ecProperties = getProperties(PROPERTIES_FILES_PATH); // get properties object containing the curve data
-	
-		// checks that the curveName is in the file
-		if (!ecProperties.containsKey(curveName)) {
-			throw new IllegalArgumentException("no such NIST elliptic curve");
-		}
-		this.curveName = curveName;
-		// check that the given curve is in the field that matches the group
-		if (!curveName.startsWith("P-")) {
-			throw new IllegalArgumentException( "curveName is not a curve over Fp field and doesn't match the DlogGroup type");
-		}
-
-		doInit(ecProperties, curveName); // set the data and initialize the curve
+		this(PROPERTIES_FILES_PATH, curveName);
 	}
 
 	/**
 	 * Extracts the parameters of the curve from the properties object and initialize the groupParams, 
 	 * generator and the underlying curve
 	 * @param ecProperties properties object contains the curve file data
-	 * @param curveName the curve name as it called in the file
+	 * @param curveName the curve name as it is called in the file
 	 */
 	protected void doInit(Properties ecProperties, String curveName) {
-
-		// get the curve parameters
-		BigInteger p = new BigInteger(ecProperties.getProperty(curveName));
-		BigInteger a = new BigInteger(ecProperties.getProperty(curveName + "a"));
-		BigInteger b = new BigInteger(1, Hex.decode(ecProperties.getProperty(curveName + "b")));
-		BigInteger x = new BigInteger(1, Hex.decode(ecProperties.getProperty(curveName + "x")));
-		BigInteger y = new BigInteger(1, Hex.decode(ecProperties.getProperty(curveName + "y")));
-		BigInteger q = new BigInteger(ecProperties.getProperty(curveName + "r"));
-		BigInteger h = new BigInteger(ecProperties.getProperty(curveName + "h"));
-		
-		// create the GroupParams
-		groupParams = new ECFpGroupParams(q, x, y, p, a, b, h);
-		
-		// create the ECCurve
-		curve = new ECCurve.Fp(p, a, b);
-		
-		generator = new ECFpPointBc(x, y, this);
+		util = new ECFpUtility();
+		groupParams = util.checkAndCreateInitParams(ecProperties, curveName);
+		//Now that we have p, we can calculate k which is the maximum length in bytes of a string to be converted to a Group Element of this group. 
+		BigInteger p = ((ECFpGroupParams)groupParams).getP();
+		k = util.calcK(p);
+		createUnderlyingCurveAndGenerator(groupParams);
 	}
+	
+
+	/**
+	 * Extracts the parameters of the curve from the groupParams, and create the corresponding BC curve and generator. 
+	 * generator and the underlying curve
+	 * @param params of type ECFpGroupParams
+	 */
+	private void createUnderlyingCurveAndGenerator(GroupParams params){
+		//There is no need to check that the params passed are an instance of ECFpGroupParams since this function is only used by SCAPI.
+		ECFpGroupParams fpParams = (ECFpGroupParams)params;
+		// create the ECCurve
+		curve = new ECCurve.Fp(fpParams.getP(), fpParams.getA(), fpParams.getB());
+				
+		generator = new ECFpPointBc(fpParams.getXg(), fpParams.getYg(), this);
+	}
+	
 	
 	/**
 	 * @return the type of the group - ECFp
 	 */
 	public String getGroupType() {
-		return "elliptic curve over Fp";
+		return util.getGroupType();
 	}
 	
-	/*
+	/**
 	 * Checks if the given element is a member of this Dlog group
 	 * @param element 
 	 * @return true if the given element is member of this group; false, otherwise.
@@ -113,7 +108,7 @@ public class BcDlogECFp extends BcAdapterDlogEC implements DlogECFp, DDH {
 		// A point (x, y) is a member of a Dlog group with prime order q over an Elliptic Curve if it meets the following two conditions:
 		// 1)	P = (x,y) is a point in the Elliptic curve, i.e (x,y) is a solution of the curve’s equation.
 		// 2)	P = (x,y) is a point in the q-order group which is a sub-group of the Elliptic Curve.
-		// those two checks is done in two steps:
+		// those two checks are done in two steps:
 		// 1.	Checking that the point is on the curve, performed by checkCurveMembership
 		// 2.	Checking that the point is in the Dlog group,performed by checkSubGroupMembership
 
@@ -173,15 +168,14 @@ public class BcDlogECFp extends BcAdapterDlogEC implements DlogECFp, DDH {
 	
 	/**
 	 * Converts a byte array to an ECFpPointBc.
-	 * 	
 	 * @param binaryString the byte array to convert
+	 * @throws IndexOutOfBoundsException if the length of the binary array to encode is longer than k
 	 * @return the created group Element
 	 */
-	public GroupElement convertByteArrayToGroupElement(byte[] binaryString) {
-		
-		//currently we don't support this conversion. 
-		//will be implemented in the future.
-		return null;
+	public GroupElement encodeByteArrayToGroupElement(byte[] binaryString) {
+		ECFpUtility.FpPoint fpPoint = util.findPointRepresentedByByteArray((ECFpGroupParams) groupParams, binaryString, k); 
+		ECElement element = generateElement(fpPoint.getX(), fpPoint.getY());
+		return element;
 	}
 	
 	/**
@@ -189,13 +183,36 @@ public class BcDlogECFp extends BcAdapterDlogEC implements DlogECFp, DDH {
 	 * @param groupElement the element to convert
 	 * @return the created byte array
 	 */
-	public byte[] convertGroupElementToByteArray(GroupElement groupElement) {
+	public byte[] decodeGroupElementToByteArray(GroupElement groupElement) {
 		if (!(groupElement instanceof ECFpPointBc)) {
 			throw new IllegalArgumentException("element type doesn't match the group type");
 		}
-		//currently we don't support this conversion. 
-		//will be implemented in the future.
-		return null;
+		ECFpPointBc point = (ECFpPointBc) groupElement;
+		byte[] b1 = util.getKLeastSignBytes(point.getX(), k +1);
+		byte[] b2 = new byte[b1.length -1];
+		System.arraycopy(b1, 1, b2, 0, b2.length);
+		return b2;
 	}
+
+	/**
+	 * This function maps a group element of this dlog group to a byte array.<p>
+	 * This function does not have an inverse function, that is, it is not possible to re-construct the original group element from the resulting byte array.<p>
+	 * Moreover, the implementation of this function is such that for a given group element (point in the curve),<p>
+	 * the result of applying this function (mapAnyGroupElementToByteArray) and the result of applying decodeGroupElementToByteArray are not equal.    
+	 * @return a byte array representation of the given group element
+	 */
+	public byte[] mapAnyGroupElementToByteArray(GroupElement groupElement) {
+		//This function simply returns an array which is the result of concatenating 
+		//the byte array representation of x with the byte array representation of y.
+		if (!(groupElement instanceof ECFpPointBc)) {
+			throw new IllegalArgumentException("element type doesn't match the group type");
+		}
+		ECFpPointBc point = (ECFpPointBc) groupElement;
+		//The actual work is implemented in ECFpUtility since it is independent of the underlying library (BC, Miracl, or other)
+		//If we ever decide to change the implementation there will only one place to change it.
+		return util.mapAnyGroupElementToByteArray(point.getX(), point.getY());
+	}
+
+
 	
 }
