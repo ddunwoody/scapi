40f334eba8f1f4f3ad17f4b4852c08f9f84b5fcc
Moriya Farbstein <moriyaw@gmail.com>
2012-08-01 10:51:14 +0000

    added AsymmetricCiphertext interface that all the asymmetric ciphertexts will implement.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1422 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/midLayer/ciphertext/AsymmetricCiphertext.java
@@ -0,0 +1,9 @@
+package edu.biu.scapi.midLayer.ciphertext;
+/**
+ * This is a marker interface for all cipher-texts.
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
+ *
+ */
+public interface AsymmetricCiphertext {
+
+}
