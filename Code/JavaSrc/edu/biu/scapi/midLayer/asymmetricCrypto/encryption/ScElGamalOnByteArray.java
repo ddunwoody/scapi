d1b1e206f409abd527288c8b846fa6ba27e4a062
Moriya Farbstein <moriyaw@gmail.com>
2012-08-01 11:19:26 +0000

    separated the implementation of ElGamal to two different classes since there are two different types of ElGamal encryptions.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1439 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/ScElGamalOnByteArray.java
@@ -0,0 +1,217 @@
+package edu.biu.scapi.midLayer.asymmetricCrypto.encryption;
+
+import java.security.KeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.exceptions.FactoriesException;
+import edu.biu.scapi.exceptions.NoMaxException;
+import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ElGamalPrivateKey;
+import edu.biu.scapi.midLayer.ciphertext.AsymmetricCiphertext;
+import edu.biu.scapi.midLayer.ciphertext.ElGamalOnByteArrayCiphertext;
+import edu.biu.scapi.midLayer.plaintext.ByteArrayPlaintext;
+import edu.biu.scapi.midLayer.plaintext.Plaintext;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.kdf.HKDF;
+import edu.biu.scapi.primitives.kdf.KeyDerivationFunction;
+import edu.biu.scapi.primitives.prf.bc.BcHMAC;
+import edu.biu.scapi.tools.Factories.KdfFactory;
+
+/**
+ * This class performs the El Gamal encryption scheme that perform the encryption on a ByteArray.
+ * The general encryption of a message usually uses this type of encryption. <p>
+ * 
+ * By definition, this encryption scheme is CPA-secure and Indistinguishable.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class ScElGamalOnByteArray extends ElGamalAbs{
+
+	private KeyDerivationFunction kdf; 	// The underlying KDF to use in the encryption.
+	
+	/**
+	 * Default constructor. Uses the default implementations of DlogGroup, CryptographicHash and SecureRandom.
+	 */
+	public ScElGamalOnByteArray(){
+		super();
+		//Creates a default implementation of KDF.
+		setKdf(new HKDF(new BcHMAC()));
+	}
+
+	private void setKdf(KeyDerivationFunction kdf){
+		this.kdf = kdf;
+	}
+	
+	/**
+	 * Constructor that gets a DlogGroup and sets it to the underlying group.
+	 * It lets SCAPI choose and source of randomness.
+	 * @param dlogGroup must be DDH secure.
+	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level.
+	 */
+	public ScElGamalOnByteArray(DlogGroup dlogGroup, KeyDerivationFunction kdf) {
+		super(dlogGroup, new SecureRandom());
+		setKdf(kdf);
+	}
+	/**
+	 * Constructor that gets a DlogGroup and source of randomness.
+	 * @param dlogGroup must be DDH secure.
+	 * @param random source of randomness.
+	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level.
+	 */
+	public ScElGamalOnByteArray(DlogGroup dlogGroup, KeyDerivationFunction kdf, SecureRandom random) {
+		super(dlogGroup, random);
+		//Sets the given KDF.
+		setKdf(kdf);
+	}
+	
+	/**
+	 * Constructor that gets a DlogGroup name to create and sets it to the underlying group.
+	 * Uses default implementation of SecureRandom.
+	 * @param dlogGroup must be DDH secure.
+	 * @throws FactoriesException if the creation of the dlog failed.
+	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level. 
+	 */
+	public ScElGamalOnByteArray(String dlogName, String kdfName) throws FactoriesException{
+		super(dlogName);
+		//Sets the given KDF.
+		setKdf(KdfFactory.getInstance().getObject(kdfName));
+	}
+	
+	
+	/**
+	 * Constructor that gets a DlogGroup name to create and random number generator to use.
+	 * @param dlogGroup must be DDH secure.
+	 * @throws FactoriesException if the creation of the dlog failed.
+	 * @throws NoSuchAlgorithmException if the given random number generator is not supported.
+	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level.
+	 */
+	public ScElGamalOnByteArray(String dlogName, String kdfName, String randNumGenAlg) throws FactoriesException, NoSuchAlgorithmException{
+		super(dlogName, randNumGenAlg);
+		//Sets the given KDF.
+		setKdf(KdfFactory.getInstance().getObject(kdfName));
+	}
+	
+
+	/**
+	 * ElGamal decrypt function can be optimized if, instead of using the x value in the private key as is, 
+	 * we change it to be q-x, while q is the dlog group order.
+	 * This function computes this changing and saves the new private value as the private key member.
+	 * @param privateKey to change.
+	 */
+	protected void initPrivateKey(PrivateKey privateKey){
+		//Sets the given PrivateKey.
+		this.privateKey = (ElGamalPrivateKey) privateKey;
+	}
+	
+	/**
+	 * ElGamalOnByteArray encryption scheme has no limit of the byte array length to generate a plaintext from.
+	 * @return false. 
+	 */
+	public boolean hasMaxByteArrayLengthForPlaintext(){
+		return false;
+	}
+	
+	/**
+	 * ElGamalOnByteArray encryption can get any plaintext length.
+	 * @throws NoMaxException.
+	 */
+	public int getMaxLengthOfByteArrayForPlaintext(){
+		throw new NoMaxException("ElGamalOnByteArray encryption can get any plaintext length");
+	}
+	
+	/**
+	 * Generates a Plaintext suitable to ElGamal encryption scheme from the given message.
+	 * @param msg byte array to convert to a Plaintext object.
+	 */
+	public Plaintext generatePlaintext(byte[] text){
+		
+		return new ByteArrayPlaintext(text);
+	}
+	
+	/**
+	 * Completes the encryption operation.
+	 * @param plaintext contains message to encrypt. MUST be of type ByteArrayPlaintext.
+	 * @return Ciphertext of type ElGamalOnByteArrayCiphertext containing the encrypted message.
+	 * @throws IllegalArgumentException if the given Plaintext is not an instance of ByteArrayPlaintext.
+	 */
+	protected AsymmetricCiphertext completeEncryption(GroupElement c1, GroupElement hy, Plaintext plaintext){
+		
+		if (!(plaintext instanceof ByteArrayPlaintext)){
+			throw new IllegalArgumentException("plaintext should be instance of ByteArrayPlaintext");
+		}
+	
+		//Gets the message.
+		byte[] msg = ((ByteArrayPlaintext) plaintext).getText();
+	
+		byte[] hyBytes = dlog.mapAnyGroupElementToByteArray(hy);
+		byte[] c2 = kdf.derivateKey(hyBytes, 0, hyBytes.length, msg.length).getEncoded();
+		
+		//Xores the result from the kdf with the plaintext.
+		for(int i=0; i<msg.length; i++){
+			c2[i] = (byte) (c2[i] ^ msg[i]);
+		}
+		
+		//Returns an ElGamalOnByteArrayCiphertext with c1, c2.
+		return new ElGamalOnByteArrayCiphertext(c1, c2);
+	}
+
+	/**
+	 * Decrypts the given ciphertext using ElGamal encryption scheme.
+	 *
+	 * @param CipherText MUST be of type ElGamalOnByteArrayCiphertext contains the cipher to decrypt.
+	 * @return Plaintext of type ByteArrayPlaintext which containing the decrypted message.
+	 * @throws KeyException if no private key was set.
+	 * @throws IllegalArgumentException if the given cipher is not instance of ElGamalOnByteArrayCiphertext.
+	 */
+	public Plaintext decrypt(AsymmetricCiphertext cipher) throws KeyException {
+		/*  
+		 * Pseudo-code:
+		 * 	•	Calculate s = ciphertext.getC1() ^ x
+		 *	•	Calculate m = KDF(s) XOR ciphertext.getC2() 
+		 */
+		
+		//If there is no private key, throws exception.
+		if (privateKey == null){
+			throw new KeyException("in order to decrypt a message, this object must be initialized with private key");
+		}
+		//Ciphertext should be ElGamal ciphertext.
+		if (!(cipher instanceof ElGamalOnByteArrayCiphertext)){
+			throw new IllegalArgumentException("ciphertext should be instance of ElGamalOnByteArrayCiphertext");
+		}
+
+		ElGamalOnByteArrayCiphertext ciphertext = (ElGamalOnByteArrayCiphertext) cipher;
+		//Calculates s = ciphertext.getC1() ^ x.
+		GroupElement s = dlog.exponentiate(ciphertext.getC1(), privateKey.getX());
+		byte[] sBytes = dlog.mapAnyGroupElementToByteArray(s);
+		byte[] c2 = ciphertext.getC2();
+		//Calculates the plaintext element m = KDF(s) ^ c2.
+		byte[] m = kdf.derivateKey(sBytes, 0, sBytes.length, c2.length).getEncoded();
+		
+		//Xores the result from the kdf with the plaintext.
+		for(int i=0; i<c2.length; i++){
+			m[i] = (byte) (m[i] ^ c2[i]);
+		}
+		
+		//Creates a plaintext object with the element and returns it.
+		return new ByteArrayPlaintext(m);
+	}
+
+	/**
+	 * Generates a byte array from the given plaintext. 
+	 * This function should be used when the user does not know the specific type of the Asymmetric encryption he has, 
+	 * and therefore he is working on byte array.
+	 * @param plaintext to generates byte array from. MUST be an instance of ByteArrayPlaintext.
+	 * @return the byte array generated from the given plaintext.
+	 * @throws IllegalArgumentException if the given plaintext is not an instance of ByteArrayPlaintext.
+	 */
+	public byte[] generateBytesFromPlaintext(Plaintext plaintext){
+		if (!(plaintext instanceof ByteArrayPlaintext)){
+			throw new IllegalArgumentException("plaintext should be an instance of ByteArrayPlaintext");
+		}
+		
+		return ((ByteArrayPlaintext) plaintext).getText();
+	}
+}
--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/ScElGamalOnGroupElement.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/encryption/ScElGamalOnGroupElement.java
@@ -1,60 +1,40 @@
 package edu.biu.scapi.midLayer.asymmetricCrypto.encryption;
 
-import java.io.IOException;
 import java.math.BigInteger;
-import java.security.InvalidKeyException;
 import java.security.KeyException;
-import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.security.SecureRandom;
-import java.security.spec.AlgorithmParameterSpec;
-import java.security.spec.InvalidParameterSpecException;
 
 import org.bouncycastle.util.BigIntegers;
 
 import edu.biu.scapi.exceptions.FactoriesException;
+import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ElGamalPrivateKey;
 import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ScElGamalPrivateKey;
-import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ScElGamalPublicKey;
-import edu.biu.scapi.midLayer.ciphertext.Ciphertext;
-import edu.biu.scapi.midLayer.ciphertext.ElGamalCiphertext;
+import edu.biu.scapi.midLayer.ciphertext.AsymmetricCiphertext;
+import edu.biu.scapi.midLayer.ciphertext.ElGamalOnGroupElementCiphertext;
 import edu.biu.scapi.midLayer.plaintext.GroupElementPlaintext;
 import edu.biu.scapi.midLayer.plaintext.Plaintext;
 import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.GroupElement;
-import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECFp;
-import edu.biu.scapi.securityLevel.DDH;
-import edu.biu.scapi.tools.Factories.DlogGroupFactory;
 
 /**
- * This class performs the El Gamal encryption scheme.
+ * This class performs the El Gamal encryption scheme that perform the encryption on a GroupElement. <P>
+ * In some cases there are protocols that do multiple calculations and might want to keep working on a close group. 
+ * For those cases we provide encryption on a group element. <P>
+ * 
  * By definition, this encryption scheme is CPA-secure and Indistinguishable.
  * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
-public class ScElGamal implements ElGamalEnc{
-	
-	private DlogGroup dlog;						//The underlying DlogGroup
-	private ScElGamalPrivateKey privateKey;		//ElGamal private key (contains x)
-	private ScElGamalPublicKey publicKey;		//ElGamal public key (contains h)
-	private SecureRandom random;				//Source of randomness
-	private boolean isKeySet;
-	private BigInteger qMinusOne;				//We keep this value to save unnecessary calculations.
-	
+public class ScElGamalOnGroupElement extends ElGamalAbs implements AsymMultiplicativeHomomorphicEnc{
 	
 	/**
 	 * Default constructor. Uses the default implementations of DlogGroup, CryptographicHash and SecureRandom.
 	 */
-	public ScElGamal(){
-		try {
-			dlog = new MiraclDlogECFp("P-192");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		qMinusOne = dlog.getOrder().subtract(BigInteger.ONE);
-		this.random = new SecureRandom();
+	public ScElGamalOnGroupElement(){
+		super();
 	}
 
 	/**
@@ -63,8 +43,8 @@ public class ScElGamal implements ElGamalEnc{
 	 * @param dlogGroup must be DDH secure.
 	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level.
 	 */
-	public ScElGamal(DlogGroup dlogGroup) {
-		this(dlogGroup, new SecureRandom());
+	public ScElGamalOnGroupElement(DlogGroup dlogGroup) {
+		super(dlogGroup, new SecureRandom());
 	}
 	/**
 	 * Constructor that gets a DlogGroup and source of randomness.
@@ -72,14 +52,8 @@ public class ScElGamal implements ElGamalEnc{
 	 * @param random source of randomness.
 	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level.
 	 */
-	public ScElGamal(DlogGroup dlogGroup, SecureRandom random) {
-		//The underlying dlog group must be DDH secure.
-		if (!(dlogGroup instanceof DDH)){
-			throw new IllegalArgumentException("DlogGroup should have DDH security level");
-		}
-		dlog = dlogGroup;
-		qMinusOne = dlog.getOrder().subtract(BigInteger.ONE);
-		this.random = random;
+	public ScElGamalOnGroupElement(DlogGroup dlogGroup, SecureRandom random) {
+		super(dlogGroup, random);
 	}
 	
 	/**
@@ -89,9 +63,8 @@ public class ScElGamal implements ElGamalEnc{
 	 * @throws FactoriesException if the creation of the dlog failed.
 	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level. 
 	 */
-	public ScElGamal(String dlogName) throws FactoriesException{
-		//Create a dlog group object with relevant factory, and then use regular constructor.
-		this(DlogGroupFactory.getInstance().getObject(dlogName));
+	public ScElGamalOnGroupElement(String dlogName) throws FactoriesException{
+		super(dlogName);
 	}
 	
 	
@@ -102,51 +75,10 @@ public class ScElGamal implements ElGamalEnc{
 	 * @throws NoSuchAlgorithmException if the given random number generator is not supported.
 	 * @throws IllegalArgumentException if the given dlog group does not have DDH security level.
 	 */
-	public ScElGamal(String dlogName, String randNumGenAlg) throws FactoriesException, NoSuchAlgorithmException{
-		//Creates a dlog group object with relevant factory.
-		//Creates a SecureRandom object that implements the specified Random Number Generator (RNG) algorithm.
-		//Then use regular constructor.
-		this(DlogGroupFactory.getInstance().getObject(dlogName), SecureRandom.getInstance(randNumGenAlg));
+	public ScElGamalOnGroupElement(String dlogName, String randNumGenAlg) throws FactoriesException, NoSuchAlgorithmException{
+		super(dlogName, randNumGenAlg);
 	}
 	
-	/**
-	 * Initializes this ElGamal encryption scheme with (public, private) key pair.
-	 * After this initialization the user can encrypt and decrypt messages.
-	 * @param publicKey should be ElGamalPublicKey.
-	 * @param privateKey should be ElGamalPrivateKey.
-	 * @throws InvalidKeyException if the given keys are not instances of ElGamal keys.
-	 */
-	public void setKey(PublicKey publicKey, PrivateKey privateKey) throws InvalidKeyException{
-		//Key should be ElGamalPublicKey.
-		if(!(publicKey instanceof ScElGamalPublicKey)){
-			throw new InvalidKeyException("keys should be instances of ElGamal keys");
-		}
-		
-		//Key should be ElGamalPrivateKey.
-		if(privateKey!= null && !(privateKey instanceof ScElGamalPrivateKey)){
-			throw new InvalidKeyException("keys should be instances of ElGamal keys");
-		}
-		
-		//Sets the keys.
-		this.publicKey = (ScElGamalPublicKey) publicKey;
-		
-		if (privateKey != null){
-			//Computes an optimization of the private key.
-			initPrivateKey(privateKey);
-		}
-		
-		isKeySet = true;
-	}
-	
-	/**
-	 * Initializes this ElGamal encryption scheme with public key.
-	 * Setting only the public key the user can encrypt messages but can not decrypt messages.
-	 * @param publicKey should be ElGamalPublicKey
-	 * @throws InvalidKeyException if the given key is not instances of ElGamalPuclicKey.
-	 */
-	public void setKey(PublicKey publicKey) throws InvalidKeyException {
-		setKey(publicKey, null);
-	}
 
 	/**
 	 * ElGamal decrypt function can be optimized if, instead of using the x value in the private key as is, 
@@ -154,62 +86,52 @@ public class ScElGamal implements ElGamalEnc{
 	 * This function computes this changing and saves the new private value as the private key member.
 	 * @param privateKey to change.
 	 */
-	private void initPrivateKey(PrivateKey privateKey){
+	protected void initPrivateKey(PrivateKey privateKey){
 		//Gets the a value from the private key.
-		BigInteger x = ((ScElGamalPrivateKey) privateKey).getX();
+		BigInteger x = ((ElGamalPrivateKey) privateKey).getX();
 		//Gets the q-x value.
 		BigInteger xInv = dlog.getOrder().subtract(x);
 		//Sets the q-x value as the private key.
 		this.privateKey = new ScElGamalPrivateKey(xInv);
 	}
 	
-	@Override
-	public boolean isKeySet(){
-		return isKeySet;
+	/**
+	 * El-Gamal encryption scheme has a limit of the byte array length to generate a plaintext from.
+	 * @return true. 
+	 */
+	public boolean hasMaxByteArrayLengthForPlaintext(){
+		return true;
 	}
 	
 	/**
-	 * Returns the PublicKey of this ElGamal encryption scheme.
-	 * This function should not be use to check if the key has been set. 
-	 * To check if the key has been set use isKeySet function.
-	 * @return the ElGamalPublicKey
-	 * @throws IllegalStateException if no public key was set.
+	 * Returns the maximum size of the byte array that can be passed to generatePlaintext function. 
+	 * This is the maximum size of a byte array that can be converted to a Plaintext object suitable to this encryption scheme.
+	 * @return the maximum size of the byte array that can be passed to generatePlaintext function. 
 	 */
-	public PublicKey getPublicKey(){
-		if (!isKeySet()){
-			throw new IllegalStateException("no PublicKey was set");
+	public int getMaxLengthOfByteArrayForPlaintext(){
+		return dlog.getMaxLengthOfByteArrayForEncoding();
+	}
+	
+	/**
+	 * Generates a Plaintext suitable to ElGamal encryption scheme from the given message.
+	 * @param msg byte array to convert to a Plaintext object.
+	 * @throws IllegalArgumentException if the given message's length is greater than the maximum. 
+	 */
+	public Plaintext generatePlaintext(byte[] text){
+		if (text.length > getMaxLengthOfByteArrayForPlaintext()){
+			throw new IllegalArgumentException("the given text is too big for plaintext");
 		}
 		
-		return publicKey;
+		return new GroupElementPlaintext(dlog.encodeByteArrayToGroupElement(text));
 	}
 	
 	/**
-	 * @return the name of this AsymmetricEnc - ElGamal and the underlying dlog group type
+	 * Completes the encryption operation.
+	 * @param plaintext contains message to encrypt. MUST be of type GroupElementPlaintext.
+	 * @return Ciphertext of type ElGamalOnGroupElementCiphertext containing the encrypted message.
+	 * @throws IllegalArgumentException if the given Plaintext is not an instance of GroupElementPlaintext.
 	 */
-	public String getAlgorithmName(){
-		return "ElGamal/"+dlog.getGroupType();
-	}
-	
-	/**
-	 * Encrypts the given message using ElGamal encryption scheme.
-	 * 
-	 * @param plaintext contains message to encrypt. MUST be an instance of GroupElementPlaintext.
-	 * @return Ciphertext of type ElGamalCiphertext containing the encrypted message.
-	 * @throws IllegalStateException if no public key was set.
-	 * @throws IllegalArgumentException if the given Plaintext is not instance of GroupElementPlaintext.
-	 */
-	public Ciphertext encrypt(Plaintext plaintext) {
-		/* 
-		 * Pseudo-code:
-		 * 	•	Choose a random  y <- Zq.
-		 *	•	Calculate c1 = g^y mod p //Mod p operation are performed automatically by the group.
-		 *	•	Calculate c2 = h^y * plaintext.getText() mod p.
-		 */
-		
-		// If there is no public key can not encrypt, throws exception.
-		if (!isKeySet()){
-			throw new IllegalStateException("in order to encrypt a message this object must be initialized with public key");
-		}
+	protected AsymmetricCiphertext completeEncryption(GroupElement c1, GroupElement hy, Plaintext plaintext){
 		
 		if (!(plaintext instanceof GroupElementPlaintext)){
 			throw new IllegalArgumentException("plaintext should be instance of GroupElementPlaintext");
@@ -218,30 +140,22 @@ public class ScElGamal implements ElGamalEnc{
 		//Gets the element.
 		GroupElement msgElement = ((GroupElementPlaintext) plaintext).getElement();
 	
-		//Chooses a random value y<-Zq.
-		BigInteger y = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
-		
-		//Calculates c1 = g^y and c2 = msg * h^y.
-		GroupElement generator = dlog.getGenerator();
-		GroupElement c1 = dlog.exponentiate(generator, y);
-		GroupElement hy = dlog.exponentiate(publicKey.getH(), y);
 		GroupElement c2 = dlog.multiplyGroupElements(hy, msgElement);
 		
 		//Returns an ElGamalCiphertext with c1, c2.
-		ElGamalCiphertext cipher = new ElGamalCiphertext(c1, c2);
+		ElGamalOnGroupElementCiphertext cipher = new ElGamalOnGroupElementCiphertext(c1, c2);
 		return cipher;
-		
 	}
 
 	/**
 	 * Decrypts the given ciphertext using ElGamal encryption scheme.
 	 *
-	 * @param CipherText MUST be of type ElGamalCiphertext contains the cipher to decrypt.
+	 * @param CipherText MUST be of type ElGamalOnGroupElementCiphertext contains the cipher to decrypt.
 	 * @return Plaintext of type GroupElementPlaintext which containing the decrypted message.
 	 * @throws KeyException if no private key was set.
-	 * @throws IllegalArgumentException if the given cipher is not instance of ElGamalCiphertext.
+	 * @throws IllegalArgumentException if the given cipher is not instance of ElGamalOnGroupElementCiphertext.
 	 */
-	public Plaintext decrypt(Ciphertext cipher) throws KeyException {
+	public Plaintext decrypt(AsymmetricCiphertext cipher) throws KeyException {
 		/*  
 		 * Pseudo-code:
 		 * 	•	Calculate s = ciphertext.getC1() ^ x^(-1) //x^(-1) is kept in the private key because of the optimization computed in the function initPrivateKey.
@@ -253,48 +167,34 @@ public class ScElGamal implements ElGamalEnc{
 			throw new KeyException("in order to decrypt a message, this object must be initialized with private key");
 		}
 		//Ciphertext should be ElGamal ciphertext.
-		if (!(cipher instanceof ElGamalCiphertext)){
-			throw new IllegalArgumentException("ciphertext should be instance of ElGamalCiphertext");
+		if (!(cipher instanceof ElGamalOnGroupElementCiphertext)){
+			throw new IllegalArgumentException("ciphertext should be instance of ElGamalOnGroupElementCiphertext");
 		}
 
-		ElGamalCiphertext ciphertext = (ElGamalCiphertext) cipher;
-		//Calculates s = ciphertext.getC1() ^ x.
-		GroupElement s = dlog.exponentiate(ciphertext.getC1(), privateKey.getX());
-		//Calculates the plaintext element m = ciphertext.getC2() * s.
-		GroupElement m = dlog.multiplyGroupElements(ciphertext.getC2(), s);
+		ElGamalOnGroupElementCiphertext ciphertext = (ElGamalOnGroupElementCiphertext) cipher;
+		//Calculates sInv = ciphertext.getC1() ^ x.
+		GroupElement sInv = dlog.exponentiate(ciphertext.getC1(), privateKey.getX());
+		//Calculates the plaintext element m = ciphertext.getC2() * sInv.
+		GroupElement m = dlog.multiplyGroupElements(ciphertext.getC2(), sInv);
 		
 		//Creates a plaintext object with the element and returns it.
-		Plaintext plaintext = new GroupElementPlaintext(m);
-		return plaintext;
+		return new GroupElementPlaintext(m);
 	}
 
 	/**
-	 * Generates a KeyPair containing a set of ElGamalPublicKEy and ElGamalPrivateKey using the source of randomness and the dlog specified upon construction.
-	 * @return KeyPair contains keys for this ElGamal object.
+	 * Generates a byte array from the given plaintext. 
+	 * This function should be used when the user does not know the specific type of the Asymmetric encryption he has, 
+	 * and therefore he is working on byte array.
+	 * @param plaintext to generates byte array from. MUST be an instance of GroupElementPlaintext.
+	 * @return the byte array generated from the given plaintext.
+	 * @throws IllegalArgumentException if the given plaintext is not an instance of GroupElementPlaintext.
 	 */
-	public KeyPair generateKey() {
-		
-		//Chooses a random value in Zq.
-		BigInteger x = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
-		GroupElement generator = dlog.getGenerator();
-		//Calculates h = g^x.
-		GroupElement h = dlog.exponentiate(generator, x);
-		//Creates an ElGamalPublicKey with h and ElGamalPrivateKey with x.
-		ScElGamalPublicKey publicKey = new ScElGamalPublicKey(h);
-		ScElGamalPrivateKey privateKey = new ScElGamalPrivateKey(x);
-		//Creates a KeyPair with the created keys.
-		KeyPair pair = new KeyPair(publicKey, privateKey);
-		return pair;
-	}
-	
-	/**
-	 * This function is not supported for this encryption scheme, since there is no need for parameters to generate an ElGamal key pair.
-	 * @throws UnsupportedOperationException
-	 */
-	@Override
-	public KeyPair generateKey(AlgorithmParameterSpec keyParams) throws InvalidParameterSpecException {
-		//No need for parameters to generate an El Gamal key pair. 
-		throw new UnsupportedOperationException("To Generate ElGamal keys use the generateKey() function");
+	public byte[] generateBytesFromPlaintext(Plaintext plaintext){
+		if (!(plaintext instanceof GroupElementPlaintext)){
+			throw new IllegalArgumentException("plaintext should be an instance of GroupElementPlaintext");
+		}
+		GroupElement el = ((GroupElementPlaintext) plaintext).getElement();
+		return dlog.decodeGroupElementToByteArray(el);
 	}
 	
 	/**
@@ -302,10 +202,10 @@ public class ScElGamal implements ElGamalEnc{
 	 * Both ciphertexts have to have been generated with the same public key and DlogGroup as the underlying objects of this ElGamal object.
 	 * @throws IllegalStateException if no public key was set.
 	 * @throws IllegalArgumentException in the following cases:
-	 * 		1. If one or more of the given ciphertexts is not instance of ElGamalCiphertext.
+	 * 		1. If one or more of the given ciphertexts is not instance of ElGamalOnGroupElementCiphertext.
 	 * 		2. If one or more of the GroupElements in the given ciphertexts is not a member of the underlying DlogGroup of this ElGamal encryption scheme.
 	 */
-	public Ciphertext multiply(Ciphertext cipher1, Ciphertext cipher2) {
+	public AsymmetricCiphertext multiply(AsymmetricCiphertext cipher1, AsymmetricCiphertext cipher2) {
 		/* 
 		 * Pseudo-Code:
 		 * 	c1 = (u1, v1); c2 = (u2, v2) 
@@ -321,11 +221,11 @@ public class ScElGamal implements ElGamalEnc{
 		}
 		
 		// Cipher1 and cipher2 should be ElGamal ciphertexts.
-		if (!(cipher1 instanceof ElGamalCiphertext) || !(cipher2 instanceof ElGamalCiphertext)){
+		if (!(cipher1 instanceof ElGamalOnGroupElementCiphertext) || !(cipher2 instanceof ElGamalOnGroupElementCiphertext)){
 			throw new IllegalArgumentException("ciphertexts should be instance of ElGamalCiphertext");
 		}
-		ElGamalCiphertext c1 = (ElGamalCiphertext)cipher1;
-		ElGamalCiphertext c2 = (ElGamalCiphertext)cipher2;
+		ElGamalOnGroupElementCiphertext c1 = (ElGamalOnGroupElementCiphertext)cipher1;
+		ElGamalOnGroupElementCiphertext c2 = (ElGamalOnGroupElementCiphertext)cipher2;
 		
 		//Gets the groupElements of the ciphers.
 		GroupElement u1 = c1.getC1();
@@ -349,7 +249,7 @@ public class ScElGamal implements ElGamalEnc{
 		GroupElement hExpWmultV1 = dlog.multiplyGroupElements(hExpW, c1.getC2());
 		GroupElement v = dlog.multiplyGroupElements(hExpWmultV1, c2.getC2());
 		
-		return new ElGamalCiphertext(u,v);
+		return new ElGamalOnGroupElementCiphertext(u,v);
 	}
 
 	
