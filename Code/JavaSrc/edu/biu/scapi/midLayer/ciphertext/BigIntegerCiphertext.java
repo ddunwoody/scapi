37fd669f2b7881a7f2666ef02ff99d4888383fad
Moriya Farbstein <moriyaw@gmail.com>
2012-08-01 10:59:29 +0000

    1. changed the name From DJCiphertext to BigIntegerCiphertext. 2. changed the implemented interface from ciphertext to asymmetricCiphertext.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1427 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/ciphertext/BigIntegerCiphertext.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/ciphertext/BigIntegerCiphertext.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package edu.biu.scapi.midLayer.ciphertext;
 
 import java.math.BigInteger;
@@ -9,10 +6,10 @@ import java.math.BigInteger;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  *
  */
-public class DJCiphertext implements Ciphertext {
+public class BigIntegerCiphertext implements AsymmetricCiphertext {
 	private BigInteger cipher;
 
-	public DJCiphertext(BigInteger cipher) {
+	public BigIntegerCiphertext(BigInteger cipher) {
 		super();
 		this.cipher = cipher;
 	}
