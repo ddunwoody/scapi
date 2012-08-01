4cdd73432ab12075af2d1bd48c16ff582cf3a802
Moriya Farbstein <moriyaw@gmail.com>
2012-08-01 11:25:52 +0000

    1. added comments. 2. changed "ciphertext" to "SymmetricCiphertext", since there is no longer an abstract "cipher" interface.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1441 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/symmetricCrypto/encryption/SymmetricEnc.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/symmetricCrypto/encryption/SymmetricEnc.java
@@ -7,7 +7,6 @@ import java.security.spec.InvalidParameterSpecException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 
-import edu.biu.scapi.midLayer.ciphertext.Ciphertext;
 import edu.biu.scapi.midLayer.ciphertext.SymmetricCiphertext;
 import edu.biu.scapi.midLayer.plaintext.Plaintext;
 import edu.biu.scapi.securityLevel.Eav;
@@ -27,15 +26,69 @@ import edu.biu.scapi.securityLevel.Indistinguishable;
  *
  */
 public interface SymmetricEnc extends Eav, Indistinguishable{
-
+	/**
+	 * Sets the secret key for this symmetric encryption.
+	 * The key can be changed at any time. 
+	 * @param secretKey secret key.
+	 * @throws InvalidKeyException if the given key does not match this encryption scheme.
+	 */
 	public void setKey(SecretKey secretKey) throws InvalidKeyException;
+	
+	/**
+	 * An object trying to use an instance of symmetric encryption needs to check if it has already been initialized.
+	 * @return true if the object was initialized by calling the function setKey.
+	 */
 	public boolean isKeySet();
+	
+	/**
+	 * Returns the name of this symmetric encryption.
+	 * @return the name of this symmetric encryption.
+	 */
 	public String getAlgorithmName();
-	public SecretKey generateKey(AlgorithmParameterSpec keyParams ) throws InvalidParameterSpecException;
+	
+	/**
+	 * Generates a secret key to initialize this symmetric encryption.
+	 * @param keyParams algorithmParameterSpec contains  parameters for the key generation of this symmetric encryption.
+	 * @return the generated secret key.
+	 * @throws InvalidParameterSpecException if the given keyParams does not match this symmetric encryption.
+	 */
+	public SecretKey generateKey(AlgorithmParameterSpec keyParams) throws InvalidParameterSpecException;
+	
+	/**
+	 * Generates a secret key to initialize this symmetric encryption.
+	 * @param keySize is the required secret key size in bits.
+	 * @return the generated secret key.
+	 */
 	public SecretKey generateKey(int keySize);
+	
+	/**
+	 * Encrypts a plaintext. It lets the system choose the random IV.
+	 * @param plaintext.
+	 * @return  an IVCiphertext, which contains the IV used and the encrypted data.
+	 * @throws IllegalStateException if no secret key was set.
+	 * @throws IllegalArgumentException if the given plaintext does not match this encryption scheme.
+	 */
 	public SymmetricCiphertext encrypt(Plaintext plaintext);
+	
+	/**
+	 * This function encrypts a plaintext. It lets the user choose the random IV.
+	 * @param plaintext.
+	 * @param IV random bytes to use in the encryption pf the message.
+	 * @return an IVCiphertext, which contains the IV used and the encrypted data. 
+	 * @throws IllegalStateException if no secret key was set.
+	 * @throws IllegalArgumentException if the given plaintext does not match this encryption scheme.
+	 * @throws IllegalBlockSizeException if the given IV length is not as the block size.
+	 */
 	public SymmetricCiphertext encrypt(Plaintext plaintext, byte[] iv)throws IllegalBlockSizeException;
-	public Plaintext decrypt(Ciphertext ciphertext);
+	
+	/**
+	 * This function performs the decryption of a ciphertext returning the corresponding decrypted plaintext.
+	 * @param ciphertext The Ciphertext to decrypt. 
+	 * @return the decrypted plaintext.
+	 * @throws IllegalArgumentException if the given ciphertext does not match this encryption scheme.
+	 * @throws IllegalStateException if no secret key was set.
+	 */
+	public Plaintext decrypt(SymmetricCiphertext ciphertext);
 	
 	
 }
