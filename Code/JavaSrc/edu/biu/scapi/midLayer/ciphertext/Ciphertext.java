dc659eadac24d626654ffd81ccd01c62e028a8a6
Moriya Farbstein <moriyaw@gmail.com>
2012-08-01 10:46:10 +0000

    deleted the Ciphertext interface since there is no meaning to have a general "ciphertext", as we do not have a general "encryption". We separated the ciphertext hierarchy to SymmetricCiphertext and AsymmetricCiphertext.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1420 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/ciphertext/Ciphertext.java
+++
@@ -1,9 +0,0 @@
-package edu.biu.scapi.midLayer.ciphertext;
-/**
- * This is a marker interface for all cipher-texts.
- * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
- *
- */
-public interface Ciphertext {
-
-}
