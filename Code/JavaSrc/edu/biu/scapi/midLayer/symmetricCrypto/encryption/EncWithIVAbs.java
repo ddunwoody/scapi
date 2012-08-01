85aac213401fbc53e94dd202d6baa9d4c0075ee9
Moriya Farbstein <moriyaw@gmail.com>
2012-08-01 11:30:38 +0000

    1. changed the constructors implementation such that there will be just one constructor that perform the object creation ond the other constructors will call it. 2. added and updated comments. 3. changed the generateKey implementation to call the underlying prp's generateKey function. 4. added a check if the secretKey has been set in functions that need it.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1444 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/symmetricCrypto/encryption/EncWithIVAbs.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/symmetricCrypto/encryption/EncWithIVAbs.java
@@ -1,6 +1,8 @@
 package edu.biu.scapi.midLayer.symmetricCrypto.encryption;
 /**
- * This class implements common functionality of Symmetric Encryption Schemes that must use a random IV
+ * This class implements common functionality of Symmetric Encryption Schemes that must use a random IV.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  */
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -9,14 +11,12 @@ import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.InvalidParameterSpecException;
 
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 import edu.biu.scapi.exceptions.FactoriesException;
 import edu.biu.scapi.midLayer.ciphertext.IVCiphertext;
 import edu.biu.scapi.midLayer.ciphertext.SymmetricCiphertext;
-import edu.biu.scapi.midLayer.plaintext.BasicPlaintext;
+import edu.biu.scapi.midLayer.plaintext.ByteArrayPlaintext;
 import edu.biu.scapi.midLayer.plaintext.Plaintext;
 import edu.biu.scapi.primitives.prf.PseudorandomPermutation;
 import edu.biu.scapi.primitives.prf.bc.BcAES;
@@ -28,13 +28,11 @@ abstract class EncWithIVAbs implements SymmetricEnc {
 	
 	
 	/**
-	 * Default constructor
+	 * Default constructor. Uses default implementation of prp and SecureRandom.
 	 */
 	public EncWithIVAbs(){
-		//Sets the default PRP
-		this.prp = new BcAES();
-		//Sets default random
-		this.random = new SecureRandom();
+		//Calls the general constructor with AES and default SecureRandom object.
+		this(new BcAES(), new SecureRandom());
 	}
 	
 	/**
@@ -43,52 +41,46 @@ abstract class EncWithIVAbs implements SymmetricEnc {
 	 * @param prp specific Pseudorandom permutation, for example AES.
 	 */
 	public EncWithIVAbs(PseudorandomPermutation prp){
-		this.prp = prp;
-		//sets default random and padding scheme
-		this.random = new SecureRandom();
+		//Calls the general constructor with the given prp and default SecureRandom object.
+		this(prp, new SecureRandom());
 	}
 	
 	/**
 	 * By passing a specific Pseudorandom permutation we are setting the type of encryption scheme.<p>
 	 * Random object sets the source of randomness.
-	 * This constructor gets and initialized Pseudorandom permutation.
-	 * @param prp the name of a specific Pseudorandom permutation, for example "AES".
+	 * @param prp the specific Pseudorandom permutation.
 	 * @param random SecureRandom object to set the random member
 	 */
 	public EncWithIVAbs(PseudorandomPermutation prp, SecureRandom random) {
-		//sets the prp and random
+		//Sets the prp and random.
 		this.prp = prp;
 		this.random = random;
-		//create default padding scheme
 	}
 	
 	/**
 	 * By passing a specific Pseudorandom permutation we are setting the type of encryption scheme.<p>
 	 * This constructor gets the name of a Pseudorandom permutation and is responsible for creating a corresponding instance.<p>
 	 * A default source of randomness is used.
-	 * @param prp the name of a specific Pseudorandom permutation, for example "AES".
-	 * @throws FactoriesException 
+	 * @param prpName the name of a specific Pseudorandom permutation, for example "AES".
+	 * @throws FactoriesException if the given name is not a valid prp name.
 	 */
 	public EncWithIVAbs(String prpName) throws FactoriesException {
-		// Creates a prp object and set this.prp to it
-		prp = (PseudorandomPermutation) PrfFactory.getInstance().getObject(prpName);
-		random = new SecureRandom();
+		//Creates a prp using the factory and calls the general constructor with it.
+		this((PseudorandomPermutation) PrfFactory.getInstance().getObject(prpName));
 	}
 	
 	/**
 	 * By passing a specific Pseudorandom permutation we are setting the type of encryption scheme.<p>
 	 * This constructor gets the name of a Pseudorandom permutation and is responsible for creating a corresponding instance.<p>
 	 * It also gets the name of a Random Number Generator Algorithm to use to generate the source of randomness
-	 * @param prp the name of a specific Pseudorandom permutation, for example "AES".
-	 * @param randNumGenAlg  the name of the RNG algorithm, for example "SHA1PRNG"
-	 * @throws FactoriesException 
-	 * @throws NoSuchAlgorithmException 
+	 * @param prpName the name of a specific Pseudorandom permutation, for example "AES".
+	 * @param randNumGenAlg  the name of the RNG algorithm, for example "SHA1PRNG".
+	 * @throws FactoriesException if the given prpName is not a valid prp name.
+	 * @throws NoSuchAlgorithmException if the given randNumGenAlg is not a valid random number generator algorithm.
 	 */
 	public EncWithIVAbs(String prpName, String randNumGenAlg) throws FactoriesException, NoSuchAlgorithmException {
-		//Creates a prp object and set this.prp to it
-		prp = (PseudorandomPermutation) PrfFactory.getInstance().getObject(prpName);
-		//Creates a SecureRandom object that implements the specified Random Number Generator (RNG) algorithm. 
-		this.random = SecureRandom.getInstance(randNumGenAlg);
+		//Creates a prp and a SecureRandom object using the factories and calls the general constructor with the created objects.
+		this((PseudorandomPermutation) PrfFactory.getInstance().getObject(prpName), SecureRandom.getInstance(randNumGenAlg));
 	}
 	
 	
@@ -96,12 +88,12 @@ abstract class EncWithIVAbs implements SymmetricEnc {
 	 * Supply the encryption scheme with a Secret Key.
 	 */
 	public void setKey(SecretKey secretKey) throws InvalidKeyException{
-		prp.setKey(secretKey); //Do we need to check that prp is not null? What if it is? What if the concrete implementation doesn't instantiate the prp?
+		prp.setKey(secretKey); 
 	}
 
 	/**
-	 * Checks if this object has been given a SecretKey 
-	 * @return true, if already initialized <p> false, otherwise.
+	 * Checks if this object has been given a SecretKey .
+	 * @return true, if already initialized. <p> false, otherwise.
 	 * 
 	 */
 	public boolean isKeySet(){
@@ -110,90 +102,77 @@ abstract class EncWithIVAbs implements SymmetricEnc {
 	
 	
 	/**
-	 * Generates a secret key to initialize this encryption with IV  object.
+	 * Generates a secret key to initialize this encryption with IV object.
 	 * This function delegates the generation of the key to the underlying PRP. 
 	 * It should only be used if the Secret Key is not a string of random bits of a specified length.
 	 * @param keyParams parameters needed to create the key.
 	 * @return the generated secret key
-	 * @throws InvalidParameterSpecException 
+	 * @throws InvalidParameterSpecException if hte given keyParams does not match the prp type.
 	 */
 	public SecretKey generateKey(AlgorithmParameterSpec keyParams ) throws InvalidParameterSpecException{
+		//Delegates the key generation to the prp object.
 		return prp.generateKey(keyParams);
 	}
 	
 	/**
-	 * Generates a secret key to initialize this mac object.
-	 * @param keySize the length of the key to generate, it must be greater than zero.
+	 * Generates a secret key to initialize this encryption with IV object.
+	 * @param keySize the length of the key to generate.
 	 * @return the generated secret key
 	 */
 	public SecretKey generateKey(int keySize) {
-		//First looks for a default provider implementation of the key generation for the underlying prp.
-		//If found then return it. 
-		//Otherwise it generate a random string of bits of length keySize 
-		try {
-			//gets the KeyGenerator of this algorithm
-			KeyGenerator keyGen = KeyGenerator.getInstance(prp.getAlgorithmName());
-			//if the key size is zero or less - uses the default key size as implemented in the provider implementation
-			if(keySize <= 0){
-				keyGen.init(random);
-			//else, uses the keySize to generate the key
-			} else {
-				keyGen.init(keySize, random);
-			}
-			//generates the key
-			return keyGen.generateKey();
-		
-		//Could not find a default provider implementation, 
-		//then, generate a random string of bits of length keySize, which has to be greater that zero. 
-		} catch (NoSuchAlgorithmException e) {
-			//if the key size is zero or less - throw exception
-			if (keySize < 0){
-				throw new NegativeArraySizeException("key size must be greater than 0");
-			}
-			//creates a byte array of size keySize
-			byte[] genBytes = new byte[keySize];
-
-			//generates the bytes using the random
-			//Do we need to seed random??
-			random.nextBytes(genBytes);
-			//creates a secretKey from the generated bytes
-			SecretKey generatedKey = new SecretKeySpec(genBytes, "");
-			return generatedKey;
-		}
+		//Delegates the key generation to the prp object.
+		return prp.generateKey(keySize);
 	}	
 	
 	/**
 	 * This function encrypts a plaintext. It lets the system choose the random IV.
+	 * @param plaintext should be an instance of ByteArrayPlaintext.
 	 * @return  an IVCiphertext, which contains the IV used and the encrypted data.
+	 * @throws IllegalStateException if no secret key was set.
+	 * @throws IllegalArgumentException if the given plaintext is not an instance of ByteArrayPlaintext.
 	 */
 	public SymmetricCiphertext encrypt(Plaintext plaintext) {	
-		//Allocate space for the IV.
+		if (!isKeySet()){
+			throw new IllegalStateException("no SecretKey was set");
+		}
+		//Allocates space for the IV.
 		byte[] iv = new byte[prp.getBlockSize()];
-		//Generate a random IV
+		//Generates a random IV
 		this.random.nextBytes(iv);
 		
-		//Encrypt the plaintext with the just chosen random IV.
+		//Encrypts the plaintext with the just chosen random IV.
 		IVCiphertext cipher = null;
 		try {
 			cipher =  (IVCiphertext) encrypt(plaintext, iv);
 		} catch (IllegalBlockSizeException e) {
-			
+			//Should not occur since IV was created of size Block size.
 			e.printStackTrace();
 		}
 		return cipher;
 	}
 	
 	/**
-	 * This function encrypts a plaintext. It lets the system choose the random IV.
+	 * This function encrypts a plaintext. It lets the user choose the random IV.
+	 * @param plaintext should be an instance of ByteArrayPlaintext.
+	 * @param IV random bytes to use in the encryption pf the message.
 	 * @return an IVCiphertext, which contains the IV used and the encrypted data. 
+	 * @throws IllegalStateException if no secret key was set.
+	 * @throws IllegalArgumentException if the given plaintext is not an instance of ByteArrayPlaintext.
+	 * @throws IllegalBlockSizeException if the given IV length is not as the block size.
 	 */
 	public SymmetricCiphertext encrypt(Plaintext plaintext, byte[] iv) throws IllegalBlockSizeException{
-		//Check validity of IV's length:
+		if (!isKeySet()){
+			throw new IllegalStateException("no SecretKey was set");
+		}
+		//Checks validity of IV's length:
 		if(iv.length != prp.getBlockSize()){
 			throw new IllegalBlockSizeException("The length of the IV passed is not equal to the block size of current PRP");
 		}
+		if (!(plaintext instanceof ByteArrayPlaintext)){
+			throw new IllegalArgumentException("plaintext should be instance of ByteArrayPlaintext");
+		}
 		//Each implementing class must write the actual encryption algorithm in "encAlg" function. 
-		BasicPlaintext text = (BasicPlaintext)plaintext;
+		ByteArrayPlaintext text = (ByteArrayPlaintext)plaintext;
 		IVCiphertext cipher =  encAlg(text.getText(),iv);
 		return cipher;
 	}
