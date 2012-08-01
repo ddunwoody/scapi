1f2dac82f24e29315059818eb3cec46561be24ce
Moriya Farbstein <moriyaw@gmail.com>
2012-08-01 11:26:29 +0000

    1. added multiply function. 2. added comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1442 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/symmetricCrypto/encryption/SymMultiplicativeHomomorphicEnc.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/symmetricCrypto/encryption/SymMultiplicativeHomomorphicEnc.java
@@ -1,8 +1,21 @@
 package edu.biu.scapi.midLayer.symmetricCrypto.encryption;
+
+import edu.biu.scapi.midLayer.ciphertext.SymmetricCiphertext;
+
 /**
+ * General interface for symmetric multiplicative homomorphic encryption.
+ * Such encryption scheme can compute the encryption of m1*m2, given only the encryptions of m1 and m2.
+ * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  *
  */
 public interface SymMultiplicativeHomomorphicEnc extends SymmetricEnc {
 
+	/**
+	 * Given two ciphers c1 = enc(p1), c2 = enc(p2) this function returns c1 * c2 = enc(p1 * p2)
+	 * @param c1 the encryption of p1
+	 * @param c2 the encryption of p2
+	 * @return the multiplication of c1 and c2.
+	 */
+	public SymmetricCiphertext multiply(SymmetricCiphertext c1, SymmetricCiphertext c2);
 }
