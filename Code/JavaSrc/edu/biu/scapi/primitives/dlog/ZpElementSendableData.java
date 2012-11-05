c85be8729edb24f56a7fda422e7d0c99b6384632
Yael Ejgenberg <yaelnegje@yahoo.com>
2012-11-05 17:11:33 +0000

    Added support for Communication Layer.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1597 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/primitives/dlog/ZpElementSendableData.java
@@ -0,0 +1,41 @@
+/**
+ * This file is part of SCAPI.
+ * SCAPI is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * SCAPI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with SCAPI.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Any publication and/or code referring to and/or based on SCAPI must contain an appropriate citation to SCAPI, including a reference to http://crypto.cs.biu.ac.il/SCAPI.
+ * 
+ * SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+ *
+ */
+package edu.biu.scapi.primitives.dlog;
+
+import java.math.BigInteger;
+
+/**
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
+ *
+ */
+public class ZpElementSendableData implements GroupElementSendableData {
+
+	private static final long serialVersionUID = -4297988366522382659L;
+
+	BigInteger x;
+
+	public ZpElementSendableData(BigInteger x) {
+		super();
+		this.x = x;
+	}
+
+	public BigInteger getX() {
+		return x;
+	}
+
+	@Override
+	public String toString() {
+		return "ZpElementSendableData [x=" + x + "]";
+	}
+	
+	
+}
