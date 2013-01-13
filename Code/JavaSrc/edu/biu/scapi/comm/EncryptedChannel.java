018b9183af813f20d57e92e946003f50cb236c6a
Yael Ejgenberg <yaelnegje@yahoo.com>
2013-01-13 09:58:36 +0000

    1) Made this class public, in order to allow the user to perform the decoration of the PlainTcpChannel. 2) Added lots of comments for the Javadoc. 3) Cleaned up irrelevant code.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1720 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/comm/EncryptedChannel.java
+++ Code/JavaSrc/edu/biu/scapi/comm/EncryptedChannel.java
@@ -46,32 +46,61 @@ import edu.biu.scapi.midLayer.symmetricCrypto.encryption.SymmetricEnc;
 import edu.biu.scapi.securityLevel.Cpa;
 
 /** 
+ * This channel ensures CPA security level. The owner of the channel is responsible for setting the encryption scheme to use and make sure the 
+ * the encryption scheme is initialized with a suitable key. Then, every message sent via this channel is encrypted and decrypted using the underlying encryption scheme.<p>
+ * The user needs not to worry about any of the encryption or decryption tasks. The owner of this channel can rest assure that when an object gets sent over this channel 
+ * it gets encrypted with the defined encryption scheme. In the same way, when receiving a message sent over this channel (which was encrypted by the other party) 
+ * the owner of the channel receives an already decrypted object. 
+ *    
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  */
-class EncryptedChannel extends ChannelDecorator {
+public class EncryptedChannel extends ChannelDecorator {
 	private SymmetricEnc encScheme; 
 
 	/**
-	 * @throws SecurityLevelException 
-	 * 
-	 *  
+	 * This constructor can only be used by SCAPI's CommunicationSetup class.
+	 * Creates a new EncryptedChannel. The channel is connected between the local host and the IP address and port specified as arguments. The encryption scheme must be CPA-secure, otherwise
+	 * an exception is thrown. The encryption scheme does not need to be initialized with a key at this moment (even though it can), but before sending or receiving a message over this channel
+	 * the relevant secret key must be set.
+	 *   
+	 * @param ipAddress other party's IP address
+	 * @param port other party's port
+	 * @param encScheme a symmetric encryption scheme that is CPA-secure.
+	 * @throws SecurityLevelException if the encryption scheme is not CPA-secure 
 	 */
 	EncryptedChannel(InetAddress ipAddress, int port, SymmetricEnc encScheme) throws SecurityLevelException{
 		super(new PlainTCPChannel(ipAddress,  port));
 		doConstruct( encScheme);
 	}
 	
+	/**
+	 * This constructor can only be used by SCAPI's CommunicationSetup class.
+	 * Creates a new EncryptedChannel. The channel is connected between the local host and the IP address and port specified as arguments. The encryption scheme must be CPA-secure, otherwise
+	 * an exception is thrown. The encryption scheme does not need to be initialized with a key at this moment (even though it can), but before sending or receiving a message over this channel
+	 * the relevant secret key must be set.
+	 *   
+	 * @param socketAddress object containing other party's IP address and port
+	 * @param encScheme a symmetric encryption scheme that is CPA-secure.
+	 * @throws SecurityLevelException if the encryption scheme is not CPA-secure 
+	 */
 	EncryptedChannel(InetSocketAddress socketAddress, SymmetricEnc encScheme) throws SecurityLevelException{
 		super(new PlainTCPChannel(socketAddress));
 		doConstruct( encScheme);
 	}
 	
-	/** 
-	 * @param channel
-	 * @param encScheme
-	 * @throws SecurityLevelException 
+	/**
+	 * This public constructor can be used by anyone holding a channel that is connected. Such a channel can be obtained by running the prepareForCommunications function
+	 * of {@link CommunicationSetup} which returns a set of already connected channels. (Note that {@link PlainTCPChannel} does not have a public constructor, if you wish to establish
+	 * a connection without the CommunicationSetup you will need to write your own Channel class).<p>
+	 * Creates a new EncryptedChannel that wraps the already connected channel mentioned above. The encryption scheme must be CPA-secure, otherwise
+	 * an exception is thrown. The encryption scheme does not need to be initialized with a key at this moment (even though it can), but before sending or receiving a message over this channel
+	 * the relevant secret key must be set.
+	 *   
+	 * @param channel an already connected channel
+	 * @param encScheme a symmetric encryption scheme that is CPA-secure.
+	 * @throws SecurityLevelException if the encryption scheme is not CPA-secure 
 	 */
-	EncryptedChannel(Channel channel, SymmetricEnc encScheme) throws SecurityLevelException {
+	public EncryptedChannel(Channel channel, SymmetricEnc encScheme) throws SecurityLevelException {
 		super(channel);
 		doConstruct( encScheme);
 	}
@@ -83,41 +112,32 @@ class EncryptedChannel extends ChannelDecorator {
 		this.encScheme = encScheme;
 	}
 	
+	/**
+	 * Sets the key of the underlying encryption scheme. This function must be called before sending or receiving messages if the encryption scheme passed to this
+	 * channel had not been set with a key yet. The key can be set indefinite number of times depending on the needs of the application. 
+	 * @param key a suitable SecretKey
+	 * @throws InvalidKeyException if the given key does not match the underlying MAC algorithm.
+	 */
 	public void setKey( SecretKey key) throws InvalidKeyException{
 		this.encScheme.setKey(key);
 	}
 	
-	/** 
-	 * @param data
-	 */
-	/*private byte[] encrypt(byte[] data) {
-		//return encScheme.encrypt(new ByteArrayPlaintext(data)).getBytes();
-		
-	}
-    */
-	/** 
-	 * @param data
-	 */
-	/*
-	private byte[] decrypt(byte[] data) {
-		return encScheme.decrypt(new  )data;
-	}
-	*/
+	
 	/**
-	 * 
+	 * Receives an encrypted message sent by the other party. It then decrypts the message and returns the actual message object sent by the other party.
+	 * The caller of this function needs NOT to worry at all about the decryption of the message, all the work is performed inside the channel
+	 * @return the decrypted object sent by the other party
+	 * 		     
 	 */
 	public Serializable receive() throws ClassNotFoundException, IOException {
 		
-		//get the message from the channel
-		//SymmetricCiphertext cipher = (SymmetricCiphertext) channel.receive();
-		//IVCiphertext cipher = (IVCiphertext) channel.receive();
+		//Get the message from the channel
 		Serializable rcvMsg = (Serializable)  channel.receive(); 
 		SymmetricCiphertext cipher = (SymmetricCiphertext)rcvMsg;
-		//decrypt the encrypted message
+		//Decrypt the encrypted message
 		ByteArrayPlaintext msg = (ByteArrayPlaintext) encScheme.decrypt(cipher);
-		//return msg;
 		
-		//Deserialize the object. The caller of this function doesn't need to know anything about encryption, therfore he should get
+		//Deserialize the object. The caller of this function doesn't need to know anything about encryption, therefore he should get
 		//the plain object that was sent by the sender.
 		ByteArrayInputStream bStream = new ByteArrayInputStream(msg.getText());
 		ObjectInputStream ois = new ObjectInputStream(bStream);
@@ -126,14 +146,15 @@ class EncryptedChannel extends ChannelDecorator {
 	}
 
 	/**
-	 * 
+	 * Sends an encrypted message on the channel, using the underlying encryption scehme. The caller of this function needs NOT to worry at all about the encryption,
+	 * all the work is performed inside the channel
+	 * @param msg the object to send to the other party  AS IS, the only constraint is that it must be Serializable
 	 */
 	public void send(Serializable msg) throws IOException {
 		//The user of this channel should not need to worry about the details of how the encryption is performed and what elements
 		//are needed to encrypt. All this work is done here, hidden from the user. From the user's perspective, she's sending her message on an encrypted channel and that is all 
 		//she cares about.
 
-		
 		//Utilize the Serialization technique to obtain a stream of bytes representing the object that needs to be sent on the channel.
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		ObjectOutputStream oos = new ObjectOutputStream(bos);
@@ -141,30 +162,17 @@ class EncryptedChannel extends ChannelDecorator {
 		oos.writeObject(msg);
 		//Now retrieve "serialized" msg from bos:
 		byte[] serializedMsg = bos.toByteArray();
-		System.out.println("The serialized msg about to be sent is:");
-		for(int i = 0; i < serializedMsg.length; i++){
-			System.out.print(serializedMsg[i] + ", ");
-		}
-		System.out.println();
+		
 		//Generate a suitable Plaintext object from the "serialized" message to be sent.
 		ByteArrayPlaintext plainText = new ByteArrayPlaintext(serializedMsg);
 		//Encrypt the plaintext and send ciphertext obtained. (On the other side of the channel, an encrypted message or ciphertext will be received by the channel, 
 		//but what the caller of the function channel::receive will get is the correct decrypted and deserialized object).
 		SymmetricCiphertext cipher = encScheme.encrypt(plainText);
 		channel.send((Serializable)cipher);
-		
-		
-		/*
-		Message m = (Message)msg;
-		ByteArrayPlaintext plainText = new ByteArrayPlaintext(m.getData());
-		//Encrypt the plaintext and send ciphertext obtained. (On the other side of the channel, and encrypted message or ciphertext will be received by the channel, 
-		//but what the caller of the function channel::receive will get is the correct decrypted and deserialized object).
-		channel.send((Serializable) encScheme.encrypt(plainText));
-		*/
 	}
 
 	/**
-	 * Pass the close request to the attached channel.
+	 * Close the channel.
 	 */
 	public void close() {
 		
