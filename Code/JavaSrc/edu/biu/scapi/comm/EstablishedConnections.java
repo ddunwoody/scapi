bbcb863654e2b0eb8473541ffde1d9f461622866
Yael Ejgenberg <yaelnegje@yahoo.com>
2013-01-13 09:59:22 +0000

    1) Made this class package private. 2) Fixed comments for Javadoc.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1721 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/comm/EstablishedConnections.java
+++ Code/JavaSrc/edu/biu/scapi/comm/EstablishedConnections.java
@@ -24,19 +24,6 @@
 */
 
 
-
-
-
-/**
- * The CommunicationSetup class holds a container of type EstablishedConnections that keeps track of the connections (channels) 
- * as they are being established. This container has a number of channels that can be in different states.
- * EstablishedConnections has regular operations of containers such as add and remove. It also has logical operations such as areAllConnected.
- * At the end of the “prepare for communication” method, the calling application receives a map of connections in the EstablishedConnections 
- * object held by the CommunicationSetup. At this stage, all the channels in EstablishedConnections object need to be in READY state. 
- * It is possible that this object will be null if the “prepare for communication” did not succeed. 
- * The key to the map is an object of type InetSocketAddress that holds the IP and the port. Since the IP and port are unique, 
- * they define a unique InetSocketAddress that can serve as a key to the map.   
- */
 package edu.biu.scapi.comm;
 
 import java.net.InetSocketAddress;
@@ -52,17 +39,26 @@ import edu.biu.scapi.generals.Logging;
 
 
 /** 
- * @author LabTest
+ * 
+ * The CommunicationSetup class holds a container of type EstablishedConnections that keeps track of the connections (channels) 
+ * as they are being established. This container has a number of channels that can be in different states.
+ * EstablishedConnections has regular operations of containers such as add and remove. It also has logical operations such as areAllConnected.
+ * At the end of the “prepare for communication” method, the calling application receives a map of connections in the EstablishedConnections 
+ * object held by the CommunicationSetup. At this stage, all the channels in EstablishedConnections object need to be in READY state. 
+ * It is possible that this object will be null if the “prepare for communication” did not succeed. 
+ * The key to the map is an object of type InetSocketAddress that holds the IP and the port. Since the IP and port are unique, 
+ * they define a unique InetSocketAddress that can serve as a key to the map.   
+
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Meital Levy)
   */
-public class EstablishedConnections {
+class EstablishedConnections {
 	private Map<InetSocketAddress,Channel> connectionsMap;
-	//private Set<Channel> channels;
 
 	
 	/**
 	 * 
 	 */
-	public EstablishedConnections() {
+	EstablishedConnections() {
 		//initiate the map
 		connectionsMap = new HashMap<InetSocketAddress,Channel>();
 	}
@@ -72,7 +68,7 @@ public class EstablishedConnections {
 	/**
 	 * @return the connections
 	 */
-	public Map<InetSocketAddress,Channel> getConnections() {
+	Map<InetSocketAddress,Channel> getConnections() {
 		return connectionsMap;
 	}
 	
