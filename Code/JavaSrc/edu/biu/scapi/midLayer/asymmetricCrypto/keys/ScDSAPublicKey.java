2f5a0396122ac3cabc3b097f7fafe0b9442ec25d
Yael Ejgenberg <yaelnegje@yahoo.com>
2013-04-18 12:39:32 +0000

    Added and fixed Javadoc.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1874 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/keys/ScDSAPublicKey.java
+++ Code/JavaSrc/edu/biu/scapi/midLayer/asymmetricCrypto/keys/ScDSAPublicKey.java
@@ -31,20 +31,28 @@ import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ScCramerShoupPublicKey.ScCramerShoupPublicKeySendableData;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.GroupElement;
 import edu.biu.scapi.primitives.dlog.GroupElementSendableData;
 
+/**
+* This class represents a Public Key suitable for the Digital Signature Algorithm. Although the constructor is public, it should only be instantiated by the 
+* Digital Signature itself via the generateKey function. 
+* @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
+*
+*/
 public class ScDSAPublicKey implements DSAPublicKey{
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 7578867149669452105L;
 	private GroupElement y;
 
 	public ScDSAPublicKey(GroupElement y){
 		this.y = y;
+	}
+	
+	public ScDSAPublicKey(ScDSAPublicKeySendableData data, DlogGroup dlog){
+		this(dlog.generateElement(false, data.getY()));
 	}
 
 	@Override
@@ -67,15 +75,16 @@ public class ScDSAPublicKey implements DSAPublicKey{
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.keys.CramerShoupPublicKey#generateSendableData()
+	/**
+	 * (non-Javadoc)
+	 * @see edu.biu.scapi.midLayer.asymmetricCrypto.keys.DSAPublicKey#generateSendableData()
 	 */
 	@Override
 	public KeySendableData generateSendableData() {
 		return new ScDSAPublicKeySendableData(y.generateSendableData());
 	}
 
-
+	//Nested class that holds the sendable data of the outer class
 	static public class ScDSAPublicKeySendableData implements KeySendableData {
 
 		private static final long serialVersionUID = -3966023977520093223L;
@@ -88,8 +97,8 @@ public class ScDSAPublicKey implements DSAPublicKey{
 			return y;
 		}
 
-		//Even though ScCramerShoupPublicKey should be Serializable (it implements the Key interface which is Serializable), we need to stop the regular serialization mechanism.
-		//Cramer Shoup's public key contains GroupElements and cannot be serialized in the regular way, therefore, we stop the serialization here. 
+		//Even though ScDSAPublicKey should be Serializable (it implements the Key interface which is Serializable), we need to stop the regular serialization mechanism.
+		//DSA's public key contains GroupElements and cannot be serialized in the regular way, therefore, we stop the serialization here. 
 		//In order to serialize this object you need to call the generateSendableData() function which returns a KeySendableData object. This object can be serialized.
 		//In order to deserialize the public key in the other side the CramerShoup::generatePublicKey(KeySendableData) function needs to be called.
 		private void writeObject(ObjectOutputStream out) throws IOException
