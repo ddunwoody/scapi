060a969a12ed07b51906496e53e18c0953ffd0db
Moriya Farbstein <moriyaw@gmail.com>
2013-05-08 12:00:30 +0000

    initial version of OT receiver interface
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1905 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/OTReceiver.java
@@ -0,0 +1,58 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot;
+
+import edu.biu.scapi.exceptions.CheatAttemptException;
+
+/**
+ * Marker interface. Every class that implements it is signed as Oblivious Transfer receiver.
+ * 
+ * Oblivious Transfer is the situation where a sender has n messages, and the receiver has an 
+ * index i. The receiver wishes to receive the i-th among the sender's messages, 
+ * without the sender learning i, while the sender wants to ensure that the receiver receive 
+ * only one of the n messages.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public interface OTReceiver {
+	/**
+	 * Run the part of the protocol where the receiver input is not yet necessary.
+	 */
+	public void preProcess();
+	
+	/**
+	 * Sets the input for this OT receiver.
+	 * @param input
+	 */
+	public void setInput(OTRInput input);
+	
+	/**
+	 * Run the part of the protocol where the receiver input is necessary.
+	 * @return OTROutput, the output of the protocol.
+	 * @throws CheatAttemptException if there was a cheat attempt during the execution of the protocol.
+	 */
+	public OTROutput transfer() throws CheatAttemptException;
+}
