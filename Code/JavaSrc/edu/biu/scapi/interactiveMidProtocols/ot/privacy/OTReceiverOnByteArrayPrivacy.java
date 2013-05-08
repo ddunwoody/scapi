65762f891ef514efe9d05776c9939dfb17249290
Moriya Farbstein <moriyaw@gmail.com>
2013-05-08 12:01:05 +0000

    initial version of OT DDH Privacy.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@1907 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/privacy/OTReceiverOnByteArrayPrivacy.java
@@ -0,0 +1,145 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot.privacy;
+
+import java.security.SecureRandom;
+
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTROnByteArrayOutput;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTROutput;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSMessage;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.kdf.KeyDerivationFunction;
+
+/**
+ * Concrete class for OT Privacy assuming DDH receiver ON BYTE ARRAY.
+ * This class derived from OTReceiverDDHPrivacyAbs and implements the functionality 
+ * related to the byte array inputs.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class OTReceiverOnByteArrayPrivacy extends OTReceiverDDHPrivacyAbs{
+	
+	private KeyDerivationFunction kdf; //Used in the calculation.
+	private byte[] c0, c1;
+	
+	/**
+	 * Constructor that gets the channel and chooses default values of DlogGroup and SecureRandom.
+	 */
+	public OTReceiverOnByteArrayPrivacy(Channel channel){
+		super(channel);
+	}
+	
+	/**
+	 * Constructor that sets the given channel, dlogGroup, kdf and random.
+	 * @param channel
+	 * @param dlog must be DDH secure.
+	 * @param kdf
+	 * @param random
+	 */
+	public OTReceiverOnByteArrayPrivacy(Channel channel, DlogGroup dlog, KeyDerivationFunction kdf, SecureRandom random){
+		
+		super(channel, dlog, random);
+		this.kdf = kdf;
+	}
+	
+	/**
+	 * Run the following line from the protocol:
+	 * "IF NOT 
+	 *		•	w0, w1 in the DlogGroup, AND
+	 *		•	c0, c1 are binary strings of the same length
+	 *	   REPORT ERROR"
+	 * @param message received from the sender. must be OTSOnByteArrayPrivacyMessage.
+	 * @throws CheatAttemptException if there was a cheat attempt during the execution of the protocol.
+	 */
+	protected void checkReceivedTuple(OTSMessage message) throws CheatAttemptException{
+		//If message is not instance of OTSOnByteArrayPrivacyMessage, throw Exception.
+		if(!(message instanceof OTSOnByteArrayPrivacyMessage)){
+			throw new IllegalArgumentException("message should be instance of OTSOnByteArrayPrivacyMessage");
+		}
+		
+		OTSOnByteArrayPrivacyMessage msg = (OTSOnByteArrayPrivacyMessage)message;
+		
+		//Reconstruct the group elements from the given message.
+		w0 = dlog.reconstructElement(true, msg.getW0());
+		w1 = dlog.reconstructElement(true, msg.getW1());
+		
+		//Get the byte arrays from the given message.
+		c0 = msg.getC0();
+		c1 = msg.getC1();
+		
+		if (!(dlog.isMember(w0))){
+			throw new CheatAttemptException("w0 element is not a member in the current DlogGroup");
+		}
+		if (!(dlog.isMember(w1))){
+			throw new CheatAttemptException("w1 element is not a member in the current DlogGroup");
+		}
+		
+		if (c0.length != c1.length){
+			throw new CheatAttemptException("c0 and c1 is not in the same length");
+		}
+	}
+
+	/**
+	 * Run the following lines from the protocol:
+	 * "COMPUTE kσ = (wσ)^beta
+	 *	OUTPUT  xσ = cσ XOR KDF(|cσ|,kσ)"
+	 * @return OTROutput contains Xֿƒ
+	 */
+	protected OTROutput computeFinalXSigma() {
+		
+		GroupElement kSigma = null;
+		byte[] cSigma = null;
+		
+		//If σ = 0, compute w0^beta and set cσ to c0.
+		if (sigma == 0){
+			kSigma = dlog.exponentiate(w0, beta);
+			cSigma = c0;
+		} 
+		
+		//If σ = 0, compute w1^beta and set cσ to c1.
+		if (sigma == 1) {
+			kSigma = dlog.exponentiate(w1, beta);
+			cSigma = c1;
+		}
+		
+		//Compute kdf result:
+		int len = c0.length; // c0 and c1 have the same size.
+		byte[] kBytes = dlog.mapAnyGroupElementToByteArray(kSigma);
+		byte[] xSigma = kdf.deriveKey(kBytes, 0, kBytes.length, len).getEncoded();
+		
+		//Xores the result from the kdf with vSigma.
+		for(int i=0; i<len; i++){
+			xSigma[i] = (byte) (cSigma[i] ^ xSigma[i]);
+		}
+		
+		//Create and return the output containing xσ
+		return new OTROnByteArrayOutput(xSigma);
+	}
+
+}
---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/privacy/OTSenderDDHPrivacyAbs.java
@@ -0,0 +1,292 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot.privacy;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import org.bouncycastle.util.BigIntegers;
+
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSMessage;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSender;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
+import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
+import edu.biu.scapi.securityLevel.DDH;
+
+/**
+ * Abstract class for OT Privacy assuming DDH sender.
+ * Privacy OT have two modes: one is on ByteArray and the second is on GroupElement.
+ * The different is in the input and output types and the way to process them. 
+ * In spite that, there is a common behavior for both modes which this class is implementing.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public abstract class OTSenderDDHPrivacyAbs implements OTSender{
+
+	/*	
+	  This class runs the following protocol:
+			WAIT for message a from R
+			DENOTE the tuple a received by S by (x, y, z0, z1)
+			IF NOT
+			•	z0 != z1
+			•	x, y, z0, z1 in the DlogGroup
+			REPORT ERROR (cheat attempt)
+			SAMPLE random values u0,u1,v0,v1 in  {0, . . . , q-1} 
+			COMPUTE:
+			•	w0 = x^u0 • g^v0
+			•	k0 = (z0)^u0 • y^v0
+			•	w1 = x^u1 • g^v1
+			•	k1 = (z1)^u1 • y^v1 
+			in byteArray scenario:
+				•	c0 = x0 XOR KDF(|x0|,k0)
+				•	c1 = x1 XOR KDF(|x1|,k1) 
+			OR in GroupElement scenario:
+				•	c0 = x0 * k0
+				•	c1 = x1 * k1
+			SEND (w0, c0) and (w1, c1) to R
+			OUTPUT nothing
+	*/	 
+
+	private Channel channel;
+	protected DlogGroup dlog;
+	private SecureRandom random;
+	
+	//will be given in the receiver's message.
+	private GroupElement x, y, z0, z1;
+	
+	//Values required for calculations:
+	private BigInteger u0, u1, v0, v1;
+	protected GroupElement w0, w1, k0, k1;
+	
+	/**
+	 * Constructor that gets the channel and chooses default values of DlogGroup and SecureRandom.
+	 */
+	public OTSenderDDHPrivacyAbs(Channel channel){
+		try {
+			//Uses Miracl Koblitz 233 Elliptic curve.
+			setMembers(channel, new MiraclDlogECF2m("K-233"), new SecureRandom());
+		} catch (IOException e) {
+			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
+			setMembers(channel, new CryptoPpDlogZpSafePrime(), new SecureRandom());
+		}
+	}
+	
+	/**
+	 * Constructor that sets the given channel, dlogGroup and random.
+	 * @param channel
+	 * @param dlog must be DDH secure.
+	 * @param random
+	 */
+	public OTSenderDDHPrivacyAbs(Channel channel, DlogGroup dlog, SecureRandom random){
+		
+		setMembers(channel, dlog, random);
+	}
+	
+	/**
+	 * Sets the given members.
+	 * @param channel
+	 * @param dlog must be DDH secure.
+	 * @param random
+	 */
+	private void setMembers(Channel channel, DlogGroup dlog, SecureRandom random) {
+		//The underlying dlog group must be DDH secure.
+		if (!(dlog instanceof DDH)){
+			throw new IllegalArgumentException("DlogGroup should have DDH security level");
+		}
+		
+		this.channel = channel;
+		this.dlog = dlog;
+		this.random = random;
+		
+	}
+	
+	/**
+	 * Runs the part of the protocol where the sender input is not yet necessary.
+	 * @throws CheatAttemptException if there was a cheat attempt during the execution of the protocol.
+	 */
+	public void preProcess() throws CheatAttemptException{
+		/* Runs the following part of the protocol:
+				WAIT for message a from R
+				DENOTE the tuple a received by S by (x, y, z0, z1)
+				IF NOT
+				•	z0 != z1
+				•	x, y, z0, z1 in the DlogGroup
+				REPORT ERROR (cheat attempt)
+				SAMPLE random values u0,u1,v0,v1 in  {0, . . . , q-1} 
+				COMPUTE:
+				•	w0 = x^u0 • g^v0
+				•	k0 = (z0)^u0 • y^v0
+				•	w1 = x^u1 • g^v1
+				•	k1 = (z1)^u1 • y^v1 
+		*/
+		OTRPrivacyMessage message = waitForMessageFromReceiver();
+		checkReceivedTuple(message);
+		sampleRandomValues();
+		computePreProcessValues();
+	}
+
+	/**
+	 * Runs the part of the protocol where the sender input is necessary.
+	 */
+	public void transfer(){
+		/* Runs the following part of the protocol:
+				COMPUTE: in byteArray scenario:
+				•	c0 = x0 XOR KDF(|x0|,k0)
+				•	c1 = x1 XOR KDF(|x1|,k1) 
+				OR in GroupElement scenario:
+				•	c0 = x0 * k0
+				•	c1 = x1 * k1
+				SEND (w0, c0) and (w1, c1) to R
+				OUTPUT nothing
+		*/
+		OTSMessage message = computeTuple();
+		sendTupleToReceiver(message);
+	}
+
+	/**
+	 * Runs the following line from the protocol:
+	 * "WAIT for message (h0,h1) from R"
+	 * @return the received message.
+	 */
+	private OTRPrivacyMessage waitForMessageFromReceiver(){
+		try {
+			return (OTRPrivacyMessage) channel.receive();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Runs the following lines from the protocol:
+	 * "IF NOT
+	 *	•	z0 != z1
+	 *	•	x, y, z0, z1 in the DlogGroup
+	 *	REPORT ERROR (cheat attempt)"
+	 * @return the received message.
+	 * @throws CheatAttemptException 
+	 */
+	private void checkReceivedTuple(OTRPrivacyMessage message) throws CheatAttemptException {
+		//Reconstruct the group elements from the given message.
+		x = dlog.reconstructElement(true, message.getX());
+		y = dlog.reconstructElement(true, message.getY());
+		z0 = dlog.reconstructElement(true, message.getZ0());
+		z1 = dlog.reconstructElement(true, message.getZ1());
+		
+		if (!(dlog.isMember(x))){
+			throw new CheatAttemptException("x element is not a member in the current DlogGroup");
+		}
+		if (!(dlog.isMember(y))){
+			throw new CheatAttemptException("y element is not a member in the current DlogGroup");
+		}
+		if (!(dlog.isMember(z0))){
+			throw new CheatAttemptException("z0 element is not a member in the current DlogGroup");
+		}
+		if (!(dlog.isMember(z1))){
+			throw new CheatAttemptException("z1 element is not a member in the current DlogGroup");
+		}
+		
+		if (z0.equals(z1)){
+			throw new CheatAttemptException("z0 and z1 are equal");
+		}
+		
+	}
+	
+	/**
+	 * Runs the following line from the protocol:
+	 * "SAMPLE random values u0,u1,v0,v1 in  {0, . . . , q-1} "
+	 */
+	private void sampleRandomValues() {
+		BigInteger qMinusOne =  dlog.getOrder().subtract(BigInteger.ONE);
+		
+		//Save the random chosen values a s class members. 
+		u0 = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		u1 = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		v0 = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		v1 = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+	}
+	
+	/**
+	 * Runs the following lines from the protocol:
+	 * "COMPUTE:
+	 *	•	w0 = x^u0 • g^v0
+	 *	•	k0 = (z0)^u0 • y^v0
+	 *	•	w1 = x^u1 • g^v1
+	 *	•	k1 = (z1)^u1 • y^v1
+	 * 
+	 */
+	private void computePreProcessValues() {
+		GroupElement g = dlog.getGenerator(); //Get the group generator.
+		
+		//Calculates w0 = x^u0 • g^v0
+		w0 = dlog.multiplyGroupElements(dlog.exponentiate(x, u0), dlog.exponentiate(g, v0));
+		//Calculates k0 = (z0)^u0 • y^v0
+		k0 = dlog.multiplyGroupElements(dlog.exponentiate(z0, u0), dlog.exponentiate(y, v0));
+		
+		//Calculates w1 = x^u1 • g^v1
+		w1 = dlog.multiplyGroupElements(dlog.exponentiate(x, u1), dlog.exponentiate(g, v1));
+		//Calculates k1 = (z1)^u1 • y^v1
+		k1 = dlog.multiplyGroupElements(dlog.exponentiate(z1, u1), dlog.exponentiate(y, v1));
+		
+	}
+	
+	/**
+	 * Runs the following lines from the protocol:
+	 * "COMPUTE: in byteArray scenario:
+	 *	•	c0 = x0 XOR KDF(|x0|,k0)
+	 *	•	c1 = x1 XOR KDF(|x1|,k1) 
+	 *	OR in GroupElement scenario:
+	 *	•	c0 = x0 * k0
+	 *	•	c1 = x1 * k1"
+	 * @return tuple contains (w0, c0, w1, c1) to send to the receiver.
+	 */
+	protected abstract OTSMessage computeTuple();
+		
+	/**
+	 * Runs the following lines from the protocol:
+	 * "SEND (w0, c0) and (w1, c1) to R"
+	 * @param message to send to the receiver
+	 */
+	private void sendTupleToReceiver(OTSMessage message) {
+		
+		try {
+			//Send the message by the channel.
+			channel.send(message);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+}
