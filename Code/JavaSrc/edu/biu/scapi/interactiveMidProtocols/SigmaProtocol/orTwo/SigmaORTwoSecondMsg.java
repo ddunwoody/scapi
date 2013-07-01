d9b0ce84975164065a9898d0d8ddc729226a670b
Moriya Farbstein <moriyaw@gmail.com>
2013-07-01 12:06:58 +0000

    This message contains two SigmaProtocolMsg and two challenges and used when the SigmaORTwoProver sends the second message to the verifier.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2094 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/orTwo/SigmaORTwoSecondMsg.java
@@ -0,0 +1,69 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.orTwo;
+
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+
+/**
+ * Concrete implementation SigmaProtocol message. 
+ * 
+ * This message contains two SigmaProtocolMsg and two challenges and used when the SigmaORProver sends the second message to the verifier.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class SigmaORTwoSecondMsg implements SigmaProtocolMsg{
+	
+	
+	private static final long serialVersionUID = 2105516191595630990L;
+	
+	private SigmaProtocolMsg z0;
+	private byte[] e0;
+	private SigmaProtocolMsg z1;
+	private byte[] e1;
+	
+	public SigmaORTwoSecondMsg(SigmaProtocolMsg z0, byte[] e0, SigmaProtocolMsg z1, byte[] e1){
+		this.z0 = z0;
+		this.e0 = e0;
+		this.z1 = z1;
+		this.e1 = e1;
+	}
+	
+	public SigmaProtocolMsg getZ0(){
+		return z0;
+	}
+	
+	public byte[] getE0(){
+		return e0;
+	}
+	
+	public SigmaProtocolMsg getZ1(){
+		return z1;
+	}
+	
+	public byte[] getE1(){
+		return e1;
+	}
+}
