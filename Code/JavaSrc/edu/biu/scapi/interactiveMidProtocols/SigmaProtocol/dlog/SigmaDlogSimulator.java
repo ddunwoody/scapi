2cdfe937ea6d10e645dabb98af5283c14be3c04d
Moriya Farbstein <moriyaw@gmail.com>
2013-07-01 11:36:49 +0000

    added prover, verifier, simulator of Sigma Dlog protocol.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2051 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/dlog/SigmaDlogSimulator.java
@@ -0,0 +1,195 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import org.bouncycastle.util.BigIntegers;
+
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaSimulator;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaBIMsg;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaGroupElementMsg;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaSimulatorOutput;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
+import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
+
+/**
+ * Concrete implementation of Sigma Simulator.
+ * This implementation simulates the case that the prover convince a verifier that it knows the discrete log of the value h in G.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class SigmaDlogSimulator implements SigmaSimulator{
+
+	/*	
+	  This class computes the following calculations:
+		  	SAMPLE a random z <- Zq
+			COMPUTE a = g^z*h^(-e)  (where –e here means –e mod q)
+			OUTPUT (a,e,z).	
+	*/
+
+	private DlogGroup dlog; 		//Underlying DlogGroup.
+	private int t;					//Soundness parameter.
+	private SecureRandom random;
+	private BigInteger qMinusOne;
+	
+	/**
+	 * Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	 * @param dlog
+	 * @param t Soundness parameter in BITS.
+	 * @param random
+	 */
+	public SigmaDlogSimulator(DlogGroup dlog, int t, SecureRandom random){
+		// Sets the given parameters.
+		setParameters(dlog, t, random);
+	}
+	
+	/**
+	 * Default constructor that chooses default values for the parameters.
+	 */
+	public SigmaDlogSimulator() {
+		try {
+			//Calls the other constructor with Miracl Koblitz 233 Elliptic curve.
+			setParameters(new MiraclDlogECF2m("K-233"), 80, new SecureRandom());
+		} catch (IOException e) {
+			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
+			setParameters(new CryptoPpDlogZpSafePrime(), 80, new SecureRandom());
+		}
+	}
+
+	/**
+	 * If soundness parameter is valid, sets the parameters. Else, throw IllegalArgumentException.
+	 * @param dlog
+	 * @param t soundness parameter in BITS
+	 * @param random
+	 * @throws IllegalArgumentException if soundness parameter is invalid.
+	 */
+	private void setParameters(DlogGroup dlog, int t, SecureRandom random) {
+		
+		//Sets the parameters.
+		this.dlog = dlog;
+		this.t = t;
+		
+		//Check the soundness validity.
+		if (!checkSoundness()){
+			throw new IllegalArgumentException("soundness parameter t does not satisfy 2^t<q");
+		}
+		
+		this.random = random;
+		qMinusOne = dlog.getOrder().subtract(BigInteger.ONE);
+	}
+	
+	/**
+	 * Checks the validity of the given soundness parameter.
+	 * @return true if the soundness parameter is valid; false, otherwise.
+	 */
+	private boolean checkSoundness(){
+		//If soundness parameter does not satisfy 2^t<q, return false.
+		BigInteger soundness = new BigInteger("2").pow(t);
+		BigInteger q = dlog.getOrder();
+		if (soundness.compareTo(q) >= 0){
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Returns the soundness parameter for this Sigma protocol.
+	 * @return t soundness parameter
+	 */
+	public int getSoundness(){
+		return t;
+	}
+	
+	/**
+	 * Computes the simulator computation.
+	 * @param input MUST be an instance of SigmaDlogInput.
+	 * @param challenge
+	 * @return the output of the computation - (a, e, z).
+	 * @throws CheatAttemptException if the received challenge's length is not equal to the soundness parameter.
+	 * @throws IllegalArgumentException if the given input is not an instance of SigmaDlogInput.
+	 */
+	public SigmaSimulatorOutput simulate(SigmaProtocolInput input, byte[] challenge) throws CheatAttemptException{
+		//check the challenge validity.
+		if (!checkChallengeLength(challenge)){
+			throw new CheatAttemptException("the length of the given challenge is differ from the soundness parameter");
+		}
+		
+		if (!(input instanceof SigmaDlogInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaDlogInput");
+		}
+		SigmaDlogInput dlogInput = (SigmaDlogInput) input;
+		
+		//SAMPLE a random z <- Zq
+		BigInteger z = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		
+		//COMPUTE a = g^z*h^(-e)  (where –e here means –e mod q)
+		GroupElement gToZ = dlog.exponentiate(dlog.getGenerator(), z);
+		BigInteger e = new BigInteger(1, challenge);
+		BigInteger minusE = dlog.getOrder().subtract(e);
+		GroupElement hToE = dlog.exponentiate(dlogInput.getH(), minusE);
+		GroupElement a = dlog.multiplyGroupElements(gToZ, hToE);
+		
+		//OUTPUT (a,e,z).
+		return new SigmaDlogSimulatorOutput(new SigmaGroupElementMsg(a.generateSendableData()), challenge, new SigmaBIMsg(z));
+				
+	}
+	
+	/**
+	 * Computes the simulator computation.
+	 * @param input MUST be an instance of SigmaDlogInput.
+	 * @return the output of the computation - (a, e, z).
+	 * @throws IllegalArgumentException if the given input is not an instance of SigmaDlogInput.
+	 */
+	public SigmaSimulatorOutput simulate(SigmaProtocolInput input){
+		//Create a new byte array of size t/8, to get the required byte size.
+		byte[] e = new byte[t/8];
+		//Fill the byte array with random values.
+		random.nextBytes(e);
+		//Call the other simulate function with the given input and the samples e.
+		try {
+			return simulate(input, e);
+		} catch (CheatAttemptException e1) {
+			//will not occur since the challenge length is valid.
+		}
+		return null;
+	}
+	
+	/**
+	 * Checks if the given challenge length is equal to the soundness parameter.
+	 * @return true if the challenge length is t; false, otherwise. 
+	 */
+	private boolean checkChallengeLength(byte[] challenge){
+		//If the challenge's length is equal to t, return true. else, return false.
+		return (challenge.length == (t/8) ? true : false);
+	}
+}
---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/dlog/SigmaDlogVerifier.java
@@ -0,0 +1,231 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.DlogBasedSigma;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaVerifierComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaBIMsg;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaGroupElementMsg;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
+import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
+
+/**
+ * Concrete implementation of Sigma Protocol verifier computation. <p>
+ * 
+ * This implementation is based on Schnorr's sigma protocol for Dlog Group.
+ * This protocol is used for a prover to convince a verifier that it knows the discrete log of the value h in G.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class SigmaDlogVerifier implements SigmaVerifierComputation, DlogBasedSigma{
+
+	/*	
+	  This class computes the following calculations:
+		  	SAMPLE a random challenge  e <- {0, 1}^t 
+			ACC IFF VALID_PARAMS(G,q,g) = TRUE AND h in G AND g^z = ah^e          
+	*/	
+	
+	private DlogGroup dlog;			// Underlying DlogGroup.
+	private int t; 					//Soundness parameter in BITS.
+	private SigmaDlogInput input;	// Contains h.
+	private byte[] e;				//The challenge.
+	private SecureRandom random;
+	
+	/**
+	 * Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	 * @param dlog
+	 * @param t Soundness parameter in BITS.
+	 * @param random
+	 */
+	public SigmaDlogVerifier(DlogGroup dlog, int t, SecureRandom random) {
+		
+		// Sets the other parameter 
+		setParameters(dlog, t, random);
+	}
+	
+	/**
+	 * Default constructor that chooses default values for the parameters.
+	 */
+	public SigmaDlogVerifier() {
+		try {
+			//Calls the other constructor with Miracl Koblitz 233 Elliptic curve.
+			setParameters(new MiraclDlogECF2m("K-233"), 80, new SecureRandom());
+		} catch (IOException e) {
+			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
+			setParameters(new CryptoPpDlogZpSafePrime(), 80, new SecureRandom());
+		}
+	}
+
+	/**
+	 * If soundness parameter is valid, sets the parameters. Else, throw IllegalArgumentException.
+	 * @param dlog
+	 * @param t soundness parameter in BITS
+	 * @throws IllegalArgumentException if the given dlog is invalid.
+	 * @throws IllegalArgumentException if soundness parameter is invalid.
+	 */
+	private void setParameters(DlogGroup dlog, int t, SecureRandom random) {
+		if (!(dlog.validateGroup())){
+			throw new IllegalArgumentException("the given DlogGroup is not valid");
+		}
+		//Sets the parameters.
+		this.dlog = dlog;
+		this.t = t;
+		
+		//Check the soundness validity.
+		if (!checkSoundness()){
+			throw new IllegalArgumentException("soundness parameter t does not satisfy 2^t<q");
+		}
+		
+		this.random = random;
+	}
+	
+	/**
+	 * Checks the validity of the given soundness parameter.
+	 * @return true if the soundness parameter is valid; false, otherwise.
+	 */
+	private boolean checkSoundness(){
+		//If soundness parameter does not satisfy 2^t<q, throw IllegalArgumentException.
+		BigInteger soundness = new BigInteger("2").pow(t);
+		BigInteger q = dlog.getOrder();
+		if (soundness.compareTo(q) >= 0){
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Returns the soundness parameter for this Sigma protocol.
+	 * @return t soundness parameter
+	 */
+	public int getSoundness(){
+		return t;
+	}
+
+	/**
+	 * Sets the input for this Sigma protocol
+	 * @param input MUST be an instance of SigmaDlogInput.
+	 * @throws IllegalArgumentException if input is not an instance of SigmaDlogInput.
+	 */
+	public void setInput(SigmaProtocolInput input) {
+		if (!(input instanceof SigmaDlogInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaDlogInput");
+		}
+		this.input = (SigmaDlogInput) input;
+		
+	}
+	
+	/**
+	 * Computes the following line from the protocol:
+	 * 	"SAMPLE a random challenge e<-{0,1}^t".
+	 */
+	public void sampleChallenge(){
+		//Create a new byte array of size t/8, to get the required byte size.
+		e = new byte[t/8];
+		//fills the byte array with random values.
+		random.nextBytes(e);
+	}
+	
+	/**
+	 * Sets the given challenge.
+	 * @param challenge
+	 */
+	public void setChallenge(byte[] challenge){
+		e = challenge;
+	}
+	
+	/**
+	 * Returns the sampled challenge.
+	 * @return the challenge.
+	 */
+	public byte[] getChallenge(){
+		return e;
+	}
+
+	/**
+	 * Computes the following line from the protocol:
+	 * 	"ACC IFF VALID_PARAMS(G,q,g) = TRUE AND h in G AND g^z = ah^e".
+	 * @param a first message from prover
+	 * @param z second message from prover
+	 * @return true if the proof has been verified; false, otherwise.
+	 * @throws IllegalArgumentException if the first message of the prover is not an instance of SigmaGroupElementMsg
+	 * @throws IllegalArgumentException if the second message of the prover is not an instance of SigmaBIMsg
+	 */
+	public boolean verify(SigmaProtocolMsg a, SigmaProtocolMsg z) {
+		
+		boolean verified = true;
+		
+		//If one of the messages is illegal, throw exception.
+		if (!(a instanceof SigmaGroupElementMsg)){
+			throw new IllegalArgumentException("first message must be an instance of SigmaGroupElementMsg");
+		}
+		if (!(z instanceof SigmaBIMsg)){
+			throw new IllegalArgumentException("second message must be an instance of SigmaBIMsg");
+		}
+		
+		//Get the element of the first message from the prover.
+		SigmaGroupElementMsg firstMsg = (SigmaGroupElementMsg) a;
+		GroupElement aElement = dlog.reconstructElement(true, firstMsg.getElement());
+		
+		//Get the exponent in the second message from the prover.
+		SigmaBIMsg exponent = (SigmaBIMsg) z;
+		
+		//Get the h from the input and verify that it in the Dlog Group.
+		GroupElement h = input.getH();
+		
+		//If h is not member in the group, set verified to false.
+		verified = verified && dlog.isMember(h);
+		
+		//Compute g^z (left size of the verify equation).
+		GroupElement left = dlog.exponentiate(dlog.getGenerator(), exponent.getMsg());
+		
+		//Compute a*h^e (right side of the verify equation).
+		//Convert e to BigInteger.
+		BigInteger eBI = new BigInteger(1, e);
+		//Calculate h^e.
+		GroupElement hToe = dlog.exponentiate(h, eBI);
+		//Calculate a*h^e.
+		GroupElement right = dlog.multiplyGroupElements(aElement, hToe);
+		
+		//If left and right sides of the equation are not equal, set verified to false.
+		verified = verified && left.equals(right);
+		
+		e = null; //Delete the random value e.
+		
+		//Return true if all checks returned true; false, otherwise.
+		return verified;	
+	}
+	
+	
+
+}
