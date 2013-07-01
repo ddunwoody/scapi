e0cb60336ca713eadc6ab9c8ce8468c5f06c4c3d
Moriya Farbstein <moriyaw@gmail.com>
2013-07-01 11:56:33 +0000

    added the second message used by DamgardJurikProductProver to send to the verifier (z1, z2, z3).
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2079 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/damgardJurikProduct/SigmaDJProductSecondMsg.java
@@ -0,0 +1,64 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.damgardJurikProduct;
+
+import java.math.BigInteger;
+
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+
+/**
+ * Concrete implementation SigmaProtocol message. 
+ * This message contains three BigIntegers and used when the DamgardJurikProduct prover send the second message to the verifier.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+class SigmaDJProductSecondMsg implements SigmaProtocolMsg{
+	
+	private static final long serialVersionUID = -8437524435815994178L;
+	
+	private BigInteger z1;
+	private BigInteger z2;
+	private BigInteger z3;
+	
+	SigmaDJProductSecondMsg(BigInteger z1, BigInteger z2, BigInteger z3){
+		this.z1 = z1;
+		this.z2 = z2;
+		this.z3 = z3;
+	}
+	
+	BigInteger getZ1(){
+		return z1;
+	}
+	
+	BigInteger getZ2(){
+		return z2;
+	}
+	
+	BigInteger getZ3(){
+		return z3;
+	}
+
+}
