284f1e265128045f235aefc504e7df740020685f
Moriya Farbstein <moriyaw@gmail.com>
2013-07-01 11:35:33 +0000

    This interface manages the mathematical calculations of the prover side in the sigma protocol.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2049 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/SigmaProverComputation.java
@@ -0,0 +1,74 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol;
+
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+
+/**
+ * This interface manages the mathematical calculations of the prover side in the sigma protocol.
+ * It samples random values and computes the messages.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public interface SigmaProverComputation {
+	
+	/**
+	 * Returns the soundness parameter for this Sigma protocol.
+	 * @return t soundness parameter
+	 */
+	public int getSoundness();
+	
+	/**
+	 * Sets the input for this Sigma protocol.
+	 * @param input
+	 */
+	public void setInput(SigmaProtocolInput input);
+	
+	/**
+	 * Samples random values used in the concrete sigma protocol.
+	 */
+	public void sampleRandomValues();
+	
+	/**
+	 * Computes the first message of the sigma protocol.
+	 */
+	public SigmaProtocolMsg computeFirstMsg();
+	
+	/**
+	 * Computes the second message of the sigma protocol.
+	 * @throws CheatAttemptException if the received challenge's length is not equal to the soundness parameter.
+	 */
+	public SigmaProtocolMsg computeSecondMsg(byte[] challenge) throws CheatAttemptException;
+	
+	/**
+	 * Returns the simulator that matches this sigma protocol prover.
+	 * @return
+	 */
+	public SigmaSimulator getSimulator();
+
+}
