097fe23b976a5cff0ea2d1f07876e565ee3d7fb9
Moriya Farbstein <moriyaw@gmail.com>
2013-07-10 06:07:01 +0000

    added prover, verifier, simulator of SigmaElGamalCTKnowledge protocol.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2125 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/elGamalCTKnowledge/SigmaElGamalCTKnowledgeProver.java
@@ -0,0 +1,161 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCTKnowledge;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.DlogBasedSigma;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaProverComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaSimulator;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogProver;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogProverInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
+import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
+
+/**
+ * Concrete implementation of Sigma Protocol prover computation.
+ * 
+ * This protocol is used for a committer to prove that it knows the value committed to in the commitment (h,c1, c2).
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class SigmaElGamalCTKnowledgeProver implements SigmaProverComputation, DlogBasedSigma{
+
+	/*	
+	  This class uses an instance of SigmaDlogProver with:
+	  	•	Common parameters (G,q,g) and t
+		•	Common input: h (1st element of commitment)
+		•	P’s private input: a value w in Zq such that h = g^w (given w can decrypt and so this proves knowledge of committed value).	
+	*/	 
+	
+	private SigmaDlogProver sigmaDlog;	//underlying SigmaDlogProver to use.
+	private DlogGroup dlog;				//We need the DlogGroup instance in order to calculate the input for the underlying SigmaDlogProver
+	
+	/**
+	 * Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	 * @param dlog
+	 * @param t Soundness parameter in BITS.
+	 * @param random
+	 */
+	public SigmaElGamalCTKnowledgeProver(DlogGroup dlog, int t, SecureRandom random) {
+		
+		setParameters(dlog, t, random);
+	}
+	
+	/**
+	 * Default constructor that chooses default values for the parameters.
+	 */
+	public SigmaElGamalCTKnowledgeProver() {
+		try {
+			//Create Miracl Koblitz 233 Elliptic curve.
+			dlog = new MiraclDlogECF2m("K-233");
+		} catch (IOException e) {
+			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
+			dlog = new CryptoPpDlogZpSafePrime();
+		}
+		
+		setParameters(dlog, 80, new SecureRandom());
+	}
+	
+	
+	private void setParameters(DlogGroup dlog, int t, SecureRandom random) {
+		//Creates the underlying SigmaDlogProver object with default parameters.
+		sigmaDlog = new SigmaDlogProver(dlog, t, random);
+		this.dlog = dlog;
+	}
+	
+	/**
+	 * Returns the soundness parameter for this Sigma protocol.
+	 * @return t soundness parameter
+	 */
+	public int getSoundness(){
+		//Delegates the computation to the underlying Sigma Dlog prover.
+		return sigmaDlog.getSoundness();
+	}
+
+
+	/**
+	 * Sets the input for this Sigma protocol
+	 * @param input MUST be an instance of SigmaElGamalCTKnowledgeProverInput.
+	 * @throws IllegalArgumentException if input is not an instance of SigmaElGamalCTKnowledgeProverInput.
+	 */
+	public void setInput(SigmaProtocolInput in) {
+		if (!(in instanceof SigmaElGamalCTKnowledgeProverInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaElGamalCTKnowledgeProverInput");
+		}
+		SigmaElGamalCTKnowledgeProverInput input = (SigmaElGamalCTKnowledgeProverInput) in;
+		
+		//Create an input object to the underlying sigma dlog prover.
+		GroupElement h = dlog.reconstructElement(true, input.getCommitment().getPublicKey().getC());
+		SigmaDlogProverInput underlyingInput = new SigmaDlogProverInput(h, input.getW());
+		sigmaDlog.setInput(underlyingInput);
+		
+	}
+
+	/**
+	 * Samples random value r in Zq.
+	 */
+	public void sampleRandomValues() {
+		//Delegates to the underlying Sigma Dlog prover.
+		sigmaDlog.sampleRandomValues();
+	}
+
+	/**
+	 * Computes the first message of the protocol.
+	 * @return the computed message
+	 */
+	public SigmaProtocolMsg computeFirstMsg() {
+		//Delegates the computation to the underlying Sigma Dlog prover.
+		return sigmaDlog.computeFirstMsg();
+	}
+
+	/**
+	 * Computes the second message of the protocol.
+	 * @param challenge
+	 * @return the computed message.
+	 * @throws CheatAttemptException if the received challenge's length is not equal to the soundness parameter.
+	 */
+	public SigmaProtocolMsg computeSecondMsg(byte[] challenge) throws CheatAttemptException {
+		//Delegates the computation to the underlying Sigma Dlog prover.
+		return sigmaDlog.computeSecondMsg(challenge);
+		
+	}
+	
+	/**
+	 * Returns the simulator that matches this sigma protocol prover.
+	 * @return SigmaDlogSimulator
+	 */
+	public SigmaSimulator getSimulator(){
+		return new SigmaElGamalCTKnowledgeSimulator(sigmaDlog.getSimulator());
+	}
+
+}
---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/elGamalCTKnowledge/SigmaElGamalCTKnowledgeSimulator.java
@@ -0,0 +1,161 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCTKnowledge;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaSimulator;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogSimulator;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaSimulatorOutput;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
+import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
+
+/**
+ * Concrete implementation of Sigma Simulator.
+ * This implementation simulates the case that the prover convince a verifier that it knows the value committed to in the commitment (h,c1, c2).
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class SigmaElGamalCTKnowledgeSimulator implements SigmaSimulator{
+
+	/*	
+	  This class uses an instance of SigmaDlogSimulator with:
+	  	•	Common parameters (G,q,g) and t
+		•	Common input: h (1st element of commitment)
+	*/
+
+	private SigmaDlogSimulator dlogSim; //underlying SigmaDlogSimulator to use.
+	private DlogGroup dlog;				//We need the DlogGroup instance in order to calculate the input for the underlying SigmaDlogProver
+	
+	/**
+	 * Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	 * @param dlog
+	 * @param t Soundness parameter in BITS.
+	 * @param random
+	 */
+	public SigmaElGamalCTKnowledgeSimulator(DlogGroup dlog, int t, SecureRandom random){
+		
+		setParameters(dlog, t, random);
+	}
+	
+	/**
+	 * Default constructor that chooses default values for the parameters.
+	 */
+	public SigmaElGamalCTKnowledgeSimulator() {
+		try {
+			//Create Miracl Koblitz 233 Elliptic curve.
+			dlog = new MiraclDlogECF2m("K-233");
+		} catch (IOException e) {
+			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
+			dlog = new CryptoPpDlogZpSafePrime();
+		}
+		
+		setParameters(dlog, 80, new SecureRandom());
+	}
+	
+	private void setParameters(DlogGroup dlog, int t, SecureRandom random) {
+		//Creates the underlying SigmaDlogSimulator object with default parameters.
+		dlogSim = new SigmaDlogSimulator(dlog, t, random);
+		this.dlog = dlog;
+	}
+	
+	/**
+	 * Constructor that gets a simulator and sets it.
+	 * In getSimulator function in SigmaElGamalPrivateKeyProver, the prover needs to create an instance of this class.
+	 * The problem is that the prover does not know which Dlog, t and random to give, since they are values of the underlying 
+	 * SigmaDlogProver that the prover holds.
+	 * Using this constructor, the (ElGamal) prover can get the dlog simulator from the underlying (Dlog) prover and use it to create this object.
+	 * 
+	 * @param simulator MUST be an instance of SigmaDlogSimulator.
+	 * @throws IllegalArgumentException if the given simulator is not an instance of SigmaDlogSimulator.
+	 */
+	SigmaElGamalCTKnowledgeSimulator(SigmaSimulator simulator) {
+		
+		if (!(simulator instanceof SigmaDlogSimulator)){
+			throw new IllegalArgumentException("The given simulator is not an instance of SigmaDlogSimulator");
+		}
+		//Sets the given object to the underlying SigmaDlogSimulator.
+		dlogSim = (SigmaDlogSimulator) simulator;
+	}
+	
+	/**
+	 * Returns the soundness parameter for this Sigma protocol.
+	 * @return t soundness parameter
+	 */
+	public int getSoundness(){
+		return dlogSim.getSoundness();
+	}
+	
+	/**
+	 * Computes the simulator computation.
+	 * @param input MUST be an instance of SigmaElGamalCTKnowledgeInput.
+	 * @param challenge
+	 * @return the output of the computation - (a, e, z).
+	 * @throws CheatAttemptException if the received challenge's length is not equal to the soundness parameter.
+	 * @throws IllegalArgumentException if the given input is not an instance of SigmaElGamalCTKnowledgeInput.
+	 */
+	public SigmaSimulatorOutput simulate(SigmaProtocolInput input, byte[] challenge) throws CheatAttemptException{
+		if (!(input instanceof SigmaElGamalCTKnowledgeInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaElGamalCTKnowledgeInput");
+		}
+		SigmaElGamalCTKnowledgeInput elGamalInput = (SigmaElGamalCTKnowledgeInput) input;
+		
+		//Convert the input to match the required SigmaDlogSimulator's input.
+		GroupElement h = dlog.reconstructElement(true, elGamalInput.getCommitment().getPublicKey().getC());
+		SigmaDlogInput dlogInput = new SigmaDlogInput(h);
+		
+		//Delegates the computation to the underlying Sigma Dlog prover.
+		return dlogSim.simulate(dlogInput, challenge); 
+				
+	}
+	
+	/**
+	 * Computes the simulator computation.
+	 * @param input MUST be an instance of SigmaElGamalCTKnowledgeInput.
+	 * @return the output of the computation - (a, e, z).
+	 * @throws IllegalArgumentException if the given input is not an instance of SigmaElGamalCTKnowledgeInput.
+	 */
+	public SigmaSimulatorOutput simulate(SigmaProtocolInput input){
+		if (!(input instanceof SigmaElGamalCTKnowledgeInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaElGamalCTKnowledgeInput");
+		}
+		SigmaElGamalCTKnowledgeInput elGamalInput = (SigmaElGamalCTKnowledgeInput) input;
+		
+		//Convert the input to match the required SigmaDlogSimulator's input.
+		GroupElement h = dlog.reconstructElement(true, elGamalInput.getCommitment().getPublicKey().getC());
+		SigmaDlogInput dlogInput = new SigmaDlogInput(h);
+		
+		//Delegates the computation to the underlying Sigma Dlog simulator.
+		return dlogSim.simulate(dlogInput); 
+	}
+
+}
