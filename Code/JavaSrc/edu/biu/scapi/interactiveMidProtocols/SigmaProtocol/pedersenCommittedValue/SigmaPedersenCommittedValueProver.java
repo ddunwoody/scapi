98a4244e718d7e1dcdc2a55e4c29910a2835351e
Moriya Farbstein <moriyaw@gmail.com>
2013-07-10 05:55:50 +0000

    added prover, verifier, simulator of Sigma PedersenCommittedValue protocol.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2117 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/pedersenCommittedValue/SigmaPedersenCommittedValueProver.java
@@ -0,0 +1,168 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.pedersenCommittedValue;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.DlogBasedSigma;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaProverComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaSimulator;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogProver;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogProverInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
+import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
+
+/**
+ * Concrete implementation of Sigma Protocol prover computation. <p>
+ * 
+ * This protocol is used for a committer to prove that the value committed to in the commitment (h, c) is x.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class SigmaPedersenCommittedValueProver implements SigmaProverComputation, DlogBasedSigma {
+	
+	/*	
+	  Since c = g^r*h^x, it suffices to prove knowledge of r s.t. g^r = c*h^(-x). This is just a DLOG Sigma protocol.
+	  
+	  This class uses an instance of SigmaDlogProver with:
+	  	•	Common parameters (G,q,g) and t
+		•	Common input: h’ = c*h^(-x) 
+		•	P’s private input: a value r in Zq such that h’ = g^r
+	*/	
+
+	private SigmaDlogProver sigmaDlog;	//underlying SigmaDlogProver to use.
+	private DlogGroup dlog;				//We need the DlogGroup instance in order to calculate the input for the underlying SigmaDlogProver
+	
+	/**
+	 * Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	 * @param dlog
+	 * @param t Soundness parameter in BITS.
+	 * @param random
+	 */
+	public SigmaPedersenCommittedValueProver(DlogGroup dlog, int t, SecureRandom random) {
+		
+		setParameters(dlog, t, random);
+	}
+	
+	/**
+	 * Default constructor that chooses default values for the parameters.
+	 */
+	public SigmaPedersenCommittedValueProver() {
+		
+		try {
+			//Create Miracl Koblitz 233 Elliptic curve.
+			dlog = new MiraclDlogECF2m("K-233");
+		} catch (IOException e) {
+			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
+			dlog = new CryptoPpDlogZpSafePrime();
+		}
+		
+		setParameters(dlog, 80, new SecureRandom());
+		
+	}
+	
+	private void setParameters(DlogGroup dlog, int t, SecureRandom random) {
+		//Creates the underlying SigmaDlogProver object with default parameters.
+		sigmaDlog = new SigmaDlogProver(dlog, t, random);
+		this.dlog = dlog;
+	}
+
+	/**
+	 * Returns the soundness parameter for this Sigma protocol.
+	 * @return t soundness parameter
+	 */
+	public int getSoundness(){
+		//Delegates the computation to the underlying Sigma Dlog prover.
+		return sigmaDlog.getSoundness();
+	}
+
+
+	/**
+	 * Sets the input for this Sigma protocol
+	 * @param input MUST be an instance of SigmaPedersenCommittedValueProverInput.
+	 * @throws IllegalArgumentException if input is not an instance of SigmaPedersenCommittedValueProverInput.
+	 */
+	public void setInput(SigmaProtocolInput in) {
+		if (!(in instanceof SigmaPedersenCommittedValueProverInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaPedersenCommittedValueProverInput");
+		}
+		SigmaPedersenCommittedValueProverInput input = (SigmaPedersenCommittedValueProverInput) in;
+		
+		//convert the input to the underlying Dlog prover. h’ = c*h^(-x).
+		BigInteger minusX = dlog.getOrder().subtract(input.getX());
+		GroupElement hToX = dlog.exponentiate(input.getH(), minusX);
+		GroupElement c = dlog.reconstructElement(true, input.getCommitment().getC());
+		GroupElement hTag = dlog.multiplyGroupElements(c, hToX);
+		
+		SigmaDlogProverInput underlyingInput = new SigmaDlogProverInput(hTag, input.getR());
+		sigmaDlog.setInput(underlyingInput);
+		
+	}
+
+	/**
+	 * Samples random value r in Zq.
+	 */
+	public void sampleRandomValues() {
+		//Delegates to the underlying Sigma Dlog prover.
+		sigmaDlog.sampleRandomValues();
+	}
+
+	/**
+	 * Computes the first message of the protocol.
+	 * @return the computed message
+	 */
+	public SigmaProtocolMsg computeFirstMsg() {
+		//Delegates the computation to the underlying Sigma Dlog prover.
+		return sigmaDlog.computeFirstMsg();
+	}
+
+	/**
+	 * Computes the second message of the protocol.
+	 * @param challenge
+	 * @return the computed message.
+	 * @throws CheatAttemptException if the received challenge's length is not equal to the soundness parameter.
+	 */
+	public SigmaProtocolMsg computeSecondMsg(byte[] challenge) throws CheatAttemptException {
+		//Delegates the computation to the underlying Sigma Dlog prover.
+		return sigmaDlog.computeSecondMsg(challenge);
+		
+	}
+	
+	/**
+	 * Returns the simulator that matches this sigma protocol prover.
+	 * @return SigmaDlogSimulator
+	 */
+	public SigmaSimulator getSimulator(){
+		return new SigmaPedersenCommittedValueSimulator(sigmaDlog.getSimulator());
+	}
+}
---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/pedersenCommittedValue/SigmaPedersenCommittedValueVerifier.java
@@ -0,0 +1,167 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.pedersenCommittedValue;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.DlogBasedSigma;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaVerifierComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogVerifier;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
+import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
+
+/**
+ * Concrete implementation of Sigma Protocol verifier computation. <p>
+ * 
+ * This protocol is used for a committer to prove that the value committed to in the commitment (h, c) is x.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class SigmaPedersenCommittedValueVerifier implements SigmaVerifierComputation, DlogBasedSigma{
+	/*	
+	  Since c = g^r*h^x, it suffices to prove knowledge of r s.t. g^r = c*h^(-x). This is just a DLOG Sigma protocol.
+	  
+	  This class uses an instance of SigmaDlogProver with:
+	  	•	Common parameters (G,q,g) and t
+		•	Common input: h’ = c*h^(-x) 
+	*/
+	
+	private SigmaDlogVerifier sigmaDlog;	//underlying SigmaDlogVerifier to use.
+	private DlogGroup dlog;					//We need the DlogGroup instance in order to calculate the input for the underlying SigmaDlogVerifier.
+	
+	/**
+	 * Constructor that gets the underlying DlogGroup, soundness parameter and SecureRandom.
+	 * @param dlog
+	 * @param t Soundness parameter in BITS.
+	 * @param random
+	 */
+	public SigmaPedersenCommittedValueVerifier(DlogGroup dlog, int t, SecureRandom random) {
+		
+		setParameters(dlog, t, random);
+	}
+	
+	/**
+	 * Default constructor that chooses default values for the parameters.
+	 */
+	public SigmaPedersenCommittedValueVerifier() {
+		
+		try {
+			//Create Miracl Koblitz 233 Elliptic curve.
+			dlog = new MiraclDlogECF2m("K-233");
+		} catch (IOException e) {
+			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
+			dlog = new CryptoPpDlogZpSafePrime();
+		}
+		
+		setParameters(dlog, 80, new SecureRandom());
+		
+	}
+	
+	private void setParameters(DlogGroup dlog, int t, SecureRandom random) {
+		
+		//Creates the underlying SigmaDlogVerifier object with default parameters.
+		sigmaDlog = new SigmaDlogVerifier(dlog, t, random);
+		this.dlog = dlog;
+	}
+	
+	/**
+	 * Returns the soundness parameter for this Sigma protocol.
+	 * @return t soundness parameter
+	 */
+	public int getSoundness(){
+		//Delegates to the underlying Sigma Dlog verifier.
+		return sigmaDlog.getSoundness();
+	}
+
+	/**
+	 * Sets the input for this Sigma protocol
+	 * @param input MUST be an instance of SigmaPedersenCommittedValueInput.
+	 * @throws IllegalArgumentException if input is not an instance of SigmaPedersenCommittedValueInput.
+	 */
+	public void setInput(SigmaProtocolInput in) {
+		
+		if (!(in instanceof SigmaPedersenCommittedValueInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaPedersenCommittedValueInput");
+		}
+		SigmaPedersenCommittedValueInput input = (SigmaPedersenCommittedValueInput) in;
+		
+		//convert the input to the underlying Dlog prover. h’ = c*h^(-x).
+		BigInteger minusX = dlog.getOrder().subtract(input.getX());
+		GroupElement hToX = dlog.exponentiate(input.getH(), minusX);
+		GroupElement c = dlog.reconstructElement(true, input.getCommitment().getC());
+		GroupElement hTag = dlog.multiplyGroupElements(c, hToX);
+		
+		SigmaDlogInput underlyingInput = new SigmaDlogInput(hTag);
+		sigmaDlog.setInput(underlyingInput);
+				
+	}
+	
+	/**
+	 * Samples the challenge e <- {0,1}^t
+	 */
+	public void sampleChallenge(){
+		//Delegates to the underlying Sigma Dlog verifier.
+		sigmaDlog.sampleChallenge();
+	}
+	
+	/**
+	 * Sets the given challenge.
+	 * @param challenge
+	 */
+	public void setChallenge(byte[] challenge){
+		//Delegates to the underlying Sigma Dlog verifier.
+		sigmaDlog.setChallenge(challenge);
+	}
+	
+	/**
+	 * Returns the sampled challenge.
+	 * @return the challenge.
+	 */
+	public byte[] getChallenge(){
+		//Delegates to the underlying Sigma Dlog verifier.
+		return sigmaDlog.getChallenge();
+	}
+
+	/**
+	 * Verifies the proof.
+	 * @param z second message from prover
+	 * @return true if the proof has been verified; false, otherwise.
+	 * @throws IllegalArgumentException if the first message of the prover is not an instance of SigmaGroupElementMsg
+	 * @throws IllegalArgumentException if the second message of the prover is not an instance of SigmaBIMsg
+	 */
+	public boolean verify(SigmaProtocolMsg a, SigmaProtocolMsg z) {
+		
+		return sigmaDlog.verify(a, z);
+	}
+
+}
