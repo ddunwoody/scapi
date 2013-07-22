31539c46ac73b0570bbdd57bad6eb9a6b7dd0206
Moriya Farbstein <moriyaw@gmail.com>
2013-07-22 05:18:42 +0000

    Fixed bug - in setInput function there is a check that the length of x0 and x1 is the same. The bug was that the check compared between x0 to x0. Changed it to x0 and x1.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2187 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/semiHonest/OTSenderOnByteArraySemiHonest.java
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/semiHonest/OTSenderOnByteArraySemiHonest.java
@@ -82,7 +82,7 @@ public class OTSenderOnByteArraySemiHonest extends OTSenderDDHSemiHonestAbs impl
 		OTSOnByteArrayInput inputStrings = (OTSOnByteArrayInput)input;
 		
 		//If x0, x1 are not of the same length, throw Exception.
-		if (inputStrings.getX0().length != inputStrings.getX0().length){
+		if (inputStrings.getX0().length != inputStrings.getX1().length){
 			throw new IllegalArgumentException("x0 and x1 should be of the same length.");
 		}
 		
