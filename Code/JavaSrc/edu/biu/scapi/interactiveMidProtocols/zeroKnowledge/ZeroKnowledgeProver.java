2ec8409b50b7b2a4512a9dae62e0252a626610cb
Moriya Farbstein <moriyaw@gmail.com>
2013-08-01 07:23:54 +0000

    added interfaces for Zero Knowledge prover and verifier.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2221 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/zeroKnowledge/ZeroKnowledgeProver.java
@@ -0,0 +1,58 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.zeroKnowledge;
+
+import java.io.IOException;
+
+import edu.biu.scapi.exceptions.CheatAttemptException;
+
+/**
+ * A zero-knowledge proof or zero-knowledge protocol is a method by which one party (the prover) can prove
+ * to another party (the verifier) that a given statement is true, without conveying any additional 
+ * information apart from the fact that the statement is indeed true.
+ * 
+ * This interface is a general interface that simulates the prover side of the Zero Knowledge proof. 
+ * Every class that implements it is signed as Zero Knowledge prover.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public interface ZeroKnowledgeProver {
+
+	/**
+	 * Sets the input for this Zero Knowledge protocol.
+	 * @param input
+	 */
+	public void setInput(ZeroKnowledgeInput input);
+	
+	/**
+	 * Runs the prover side of the Zero Knowledge proof.
+	 * @throws IOException if failed to send the message.
+	 * @throws CheatAttemptException if the challenge's length is not as expected. 
+	 * @throws ClassNotFoundException 
+	 */
+	public void prove() throws IOException, CheatAttemptException, ClassNotFoundException;
+	
+}
