ec25bf98cd6de222a825c09c93e9f296872aa7bf
Moriya Farbstein <moriyaw@gmail.com>
2013-09-30 10:49:08 +0000

    1. Deleted default constructor and setParameters function. Now there is one constructor that sets the parameters. 2. Changed the name of getSoundness to getSoundnessParam. 3. Deleted setInput function. This implementations moved to verify function. 4. Added parameter input to verify function.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2320 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/cramerShoupEncryptedValue/SigmaCramerShoupEncryptedValueVerifier.java
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/cramerShoupEncryptedValue/SigmaCramerShoupEncryptedValueVerifier.java
@@ -24,7 +24,6 @@
 */
 package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.cramerShoupEncryptedValue;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.ArrayList;
@@ -32,18 +31,15 @@ import java.util.ArrayList;
 import edu.biu.scapi.exceptions.InvalidDlogGroupException;
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.DlogBasedSigma;
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaVerifierComputation;
-import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dhExtended.SigmaDHExtendedInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dhExtended.SigmaDHExtendedCommonInput;
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dhExtended.SigmaDHExtendedVerifier;
-import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaCommonInput;
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
 import edu.biu.scapi.midLayer.asymmetricCrypto.keys.CramerShoupPublicKey;
 import edu.biu.scapi.midLayer.ciphertext.CramerShoupOnGroupElementCiphertext;
 import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.GroupElement;
-import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
-import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
 import edu.biu.scapi.primitives.hash.CryptographicHash;
-import edu.biu.scapi.primitives.hash.cryptopp.CryptoPpSHA1;
 
 /**
  * Concrete implementation of Sigma Protocol verifier computation. <p>
@@ -78,39 +74,6 @@ public class SigmaCramerShoupEncryptedValueVerifier implements SigmaVerifierComp
 	 */
 	public SigmaCramerShoupEncryptedValueVerifier(DlogGroup dlog, CryptographicHash hash, int t, SecureRandom random) throws InvalidDlogGroupException {
 		
-		setParameters(dlog, hash, t, random);
-	}
-	
-	/**
-	 * Default constructor that chooses default values for the parameters.
-	 */
-	public SigmaCramerShoupEncryptedValueVerifier() {
-	
-		DlogGroup dlogTemp;
-		try {
-			//Create Miracl Koblitz 233 Elliptic curve.
-			dlogTemp = new MiraclDlogECF2m("K-233");
-		} catch (IOException e) {
-			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
-			dlogTemp = new CryptoPpDlogZpSafePrime();
-		}
-		
-		try {
-			setParameters(dlogTemp, new CryptoPpSHA1(), 80, new SecureRandom());
-		} catch (InvalidDlogGroupException e) {
-			// Can not occur since the DlogGroup is valid.
-		}
-	}
-	
-	/**
-	 * Sets the given parameters.
-	 * @param dlog DlogGroup used in CramerShoup encryption scheme.
-	 * @param hash CryptographicHash used in CramerShoup encryption scheme.
-	 * @param t Soundness parameter in BITS.
-	 * @param random
-	 * @throws InvalidDlogGroupException if the given DlogGroup is not valid.
-	 */
-	private void setParameters(DlogGroup dlog, CryptographicHash hash, int t, SecureRandom random) throws InvalidDlogGroupException {
 		//Creates the underlying SigmaDHExtendedVerifier object with the given parameters.
 		sigmaDH = new SigmaDHExtendedVerifier(dlog, t, random);
 		this.dlog = dlog;
@@ -121,60 +84,9 @@ public class SigmaCramerShoupEncryptedValueVerifier implements SigmaVerifierComp
 	 * Returns the soundness parameter for this Sigma protocol.
 	 * @return t soundness parameter
 	 */
-	public int getSoundness(){
+	public int getSoundnessParam(){
 		//Delegates to the underlying sigmaDHExtended verifier.
-		return sigmaDH.getSoundness();
-	}
-
-	/**
-	 * Sets the input for this Sigma protocol.
-	 * @param input MUST be an instance of SigmaCramerShoupEncryptedValueInput.
-	 * @throws IllegalArgumentException if input is not the expected.
-	 */
-	public void setInput(SigmaProtocolInput in) {
-		if (!(in instanceof SigmaCramerShoupEncryptedValueInput)){
-			throw new IllegalArgumentException("the given input must be an instance of SigmaCramerShoupEncryptedValueInput");
-		}
-		
-		//Gets the input values.
-		SigmaCramerShoupEncryptedValueInput input = (SigmaCramerShoupEncryptedValueInput) in;
-		CramerShoupPublicKey publicKey = input.getPublicKey();
-		CramerShoupOnGroupElementCiphertext cipher = input.getCipher();
-		GroupElement x = input.getX();
-		
-		//Prepare the input for the underlying SigmaDHExtendedVerifier.
-		ArrayList<GroupElement> gArray = new ArrayList<GroupElement>();
-		ArrayList<GroupElement> hArray = new ArrayList<GroupElement>();
-		
-		//Converts the given input to the necessary input to the underlying SigmaDHExtendedVerifier.
-		//(g1,g2,g3,g4,h1,h2,h3,h4) = (g1,g2,h,cd^w,u1,u2,e/x,v)
-		
-		//add the input for the gArray:
-		gArray.add(publicKey.getGenerator1()); //add g1 = g1.
-		gArray.add(publicKey.getGenerator2()); //add g2 = g2.
-		gArray.add(publicKey.getH());		   //add g3 = h.
-		
-		//Compute w = H(u1,u2,e).
-		BigInteger q = dlog.getOrder();
-		BigInteger w = calcW(cipher.getU1(), cipher.getU2(), cipher.getE()).mod(q);
-		//Compute cd^w. such that w = H(u1,u2,e).
-		GroupElement dToW = dlog.exponentiate(publicKey.getD(), w);
-		GroupElement g4 = dlog.multiplyGroupElements(publicKey.getC(), dToW);
-		gArray.add(g4);		   				   //add g4 = cd^w.
-		
-		//add the input for the hArray:
-		hArray.add(cipher.getU1());			   //add h1 = u1.
-		hArray.add(cipher.getU2());			   //add h2 = u2.
-		//Compute e/x = e*x^(-1)
-		GroupElement xInverse = dlog.getInverse(x);
-		GroupElement h3 = dlog.multiplyGroupElements(cipher.getE(), xInverse);
-		hArray.add(h3);			   			   //add h3 = e/x.
-		hArray.add(cipher.getV());			   //add h4 = v.
-		
-		//Create an input object to the underlying sigma DHExtended verifier.
-		SigmaDHExtendedInput underlyingInput = new SigmaDHExtendedInput(gArray, hArray);
-		sigmaDH.setInput(underlyingInput);
-		
+		return sigmaDH.getSoundnessParam();
 	}
 	
 	/**
@@ -184,7 +96,7 @@ public class SigmaCramerShoupEncryptedValueVerifier implements SigmaVerifierComp
 	 * @param eToByteArray
 	 * @return the result of hash(u1ToByteArray+u2ToByteArray+eToByteArray) as BigInteger.
 	 */
-	protected BigInteger calcW(GroupElement u1, GroupElement u2, GroupElement e) {
+	private BigInteger calcW(GroupElement u1, GroupElement u2, GroupElement e) {
 		
 		byte[] u1ToByteArray = dlog.mapAnyGroupElementToByteArray(u1);
 		byte[] u2ToByteArray = dlog.mapAnyGroupElementToByteArray(u2);
@@ -237,14 +149,58 @@ public class SigmaCramerShoupEncryptedValueVerifier implements SigmaVerifierComp
 
 	/**
 	 * Verifies the proof.
+	 * @param input MUST be an instance of SigmaCramerShoupEncryptedValueCommonInput.
 	 * @param z second message from prover
 	 * @return true if the proof has been verified; false, otherwise.
+	 * @throws IllegalArgumentException if input is not the expected.
 	 * @throws IllegalArgumentException if the first message of the prover is not an instance of SigmaDHExtendedMsg
 	 * @throws IllegalArgumentException if the second message of the prover is not an instance of SigmaBIMsg
 	 */
-	public boolean verify(SigmaProtocolMsg a, SigmaProtocolMsg z) {
+	public boolean verify(SigmaCommonInput in, SigmaProtocolMsg a, SigmaProtocolMsg z) {
+		if (!(in instanceof SigmaCramerShoupEncryptedValueCommonInput)){
+			throw new IllegalArgumentException("the given input must be an instance of SigmaCramerShoupEncryptedValueCommonInput");
+		}
+		
+		//Gets the input values.
+		SigmaCramerShoupEncryptedValueCommonInput input = (SigmaCramerShoupEncryptedValueCommonInput) in;
+		CramerShoupPublicKey publicKey = input.getPublicKey();
+		CramerShoupOnGroupElementCiphertext cipher = input.getCipher();
+		GroupElement x = input.getX();
+		
+		//Prepare the input for the underlying SigmaDHExtendedVerifier.
+		ArrayList<GroupElement> gArray = new ArrayList<GroupElement>();
+		ArrayList<GroupElement> hArray = new ArrayList<GroupElement>();
+		
+		//Converts the given input to the necessary input to the underlying SigmaDHExtendedVerifier.
+		//(g1,g2,g3,g4,h1,h2,h3,h4) = (g1,g2,h,cd^w,u1,u2,e/x,v)
+		
+		//add the input for the gArray:
+		gArray.add(publicKey.getGenerator1()); //add g1 = g1.
+		gArray.add(publicKey.getGenerator2()); //add g2 = g2.
+		gArray.add(publicKey.getH());		   //add g3 = h.
+		
+		//Compute w = H(u1,u2,e).
+		BigInteger q = dlog.getOrder();
+		BigInteger w = calcW(cipher.getU1(), cipher.getU2(), cipher.getE()).mod(q);
+		//Compute cd^w. such that w = H(u1,u2,e).
+		GroupElement dToW = dlog.exponentiate(publicKey.getD(), w);
+		GroupElement g4 = dlog.multiplyGroupElements(publicKey.getC(), dToW);
+		gArray.add(g4);		   				   //add g4 = cd^w.
+		
+		//add the input for the hArray:
+		hArray.add(cipher.getU1());			   //add h1 = u1.
+		hArray.add(cipher.getU2());			   //add h2 = u2.
+		//Compute e/x = e*x^(-1)
+		GroupElement xInverse = dlog.getInverse(x);
+		GroupElement h3 = dlog.multiplyGroupElements(cipher.getE(), xInverse);
+		hArray.add(h3);			   			   //add h3 = e/x.
+		hArray.add(cipher.getV());			   //add h4 = v.
+		
+		//Create an input object to the underlying sigma DHExtended verifier.
+		SigmaDHExtendedCommonInput underlyingInput = new SigmaDHExtendedCommonInput(gArray, hArray);
+		
 		//Delegates to the underlying Sigma DHExtended verifier.
-		return sigmaDH.verify(a, z);
+		return sigmaDH.verify(underlyingInput, a, z);
 	}
 
 }
