57320cf788a78aaad181ba003e8c96a9b08dac45
Moriya Farbstein <moriyaw@gmail.com>
2013-10-01 07:49:10 +0000

    1. Deleted default constructor and setParameters function. 2. Changed the name of getSoundness to getSoundnessParam. 3. Deleted setInput and sampleRandomValues functions. 4. Added parameter input to computeFirstMsg.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2384 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/pedersenCommittedValue/SigmaPedersenCommittedValueProver.java
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/SigmaProtocol/pedersenCommittedValue/SigmaPedersenCommittedValueProver.java
@@ -24,7 +24,6 @@
 */
 package edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.pedersenCommittedValue;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
@@ -34,12 +33,10 @@ import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaProverComputatio
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaSimulator;
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogProver;
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.dlog.SigmaDlogProverInput;
-import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProverInput;
 import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
 import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.GroupElement;
-import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
-import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
 
 /**
  * Concrete implementation of Sigma Protocol prover computation. <p>
@@ -71,33 +68,6 @@ public class SigmaPedersenCommittedValueProver implements SigmaProverComputation
 	 */
 	public SigmaPedersenCommittedValueProver(DlogGroup dlog, int t, SecureRandom random) {
 		
-		setParameters(dlog, t, random);
-	}
-	
-	/**
-	 * Default constructor that chooses default values for the parameters.
-	 */
-	public SigmaPedersenCommittedValueProver() {
-		
-		try {
-			//Create Miracl Koblitz 233 Elliptic curve.
-			dlog = new MiraclDlogECF2m("K-233");
-		} catch (IOException e) {
-			//If there is a problem with the elliptic curves file, create Zp DlogGroup.
-			dlog = new CryptoPpDlogZpSafePrime();
-		}
-		
-		setParameters(dlog, 80, new SecureRandom());
-		
-	}
-	
-	/**
-	 * Sets the given parameters.
-	 * @param dlog
-	 * @param t Soundness parameter in BITS.
-	 * @param random
-	 */
-	private void setParameters(DlogGroup dlog, int t, SecureRandom random) {
 		//Create the underlying SigmaDlogProver object with the given parameters.
 		sigmaDlog = new SigmaDlogProver(dlog, t, random);
 		this.dlog = dlog;
@@ -107,49 +77,46 @@ public class SigmaPedersenCommittedValueProver implements SigmaProverComputation
 	 * Returns the soundness parameter for this Sigma protocol.
 	 * @return t soundness parameter
 	 */
-	public int getSoundness(){
+	public int getSoundnessParam(){
 		//Delegates the computation to the underlying Sigma Dlog prover.
-		return sigmaDlog.getSoundness();
+		return sigmaDlog.getSoundnessParam();
 	}
 
 
 	/**
-	 * Sets the input for this Sigma protocol
+	 * Converts the input for the underlying prover computation.
 	 * @param input MUST be an instance of SigmaPedersenCommittedValueProverInput.
 	 * @throws IllegalArgumentException if input is not an instance of SigmaPedersenCommittedValueProverInput.
 	 */
-	public void setInput(SigmaProtocolInput in) {
+	private SigmaDlogProverInput convertInput(SigmaProverInput in) {
 		if (!(in instanceof SigmaPedersenCommittedValueProverInput)){
 			throw new IllegalArgumentException("the given input must be an instance of SigmaPedersenCommittedValueProverInput");
 		}
 		SigmaPedersenCommittedValueProverInput input = (SigmaPedersenCommittedValueProverInput) in;
+		SigmaPedersenCommittedValueCommonInput params = input.getCommonParams();
 		
 		//Convert the input to the underlying Dlog prover. h’ = c*h^(-x).
-		BigInteger minusX = dlog.getOrder().subtract(input.getX());
-		GroupElement hToX = dlog.exponentiate(input.getH(), minusX);
-		GroupElement c = dlog.reconstructElement(true, input.getCommitment().getC());
+		BigInteger minusX = dlog.getOrder().subtract(params.getX());
+		GroupElement hToX = dlog.exponentiate(params.getH(), minusX);
+		GroupElement c = params.getCommitment();
 		GroupElement hTag = dlog.multiplyGroupElements(c, hToX);
 		
-		SigmaDlogProverInput underlyingInput = new SigmaDlogProverInput(hTag, input.getR());
-		sigmaDlog.setInput(underlyingInput);
+		return new SigmaDlogProverInput(hTag, input.getR());
 		
-	}
-
-	/**
-	 * Samples random value r in Zq.
-	 */
-	public void sampleRandomValues() {
-		//Delegates to the underlying Sigma Dlog prover.
-		sigmaDlog.sampleRandomValues();
 	}
 
 	/**
 	 * Computes the first message of the protocol.
+	 * @param input MUST be an instance of SigmaPedersenCommittedValueProverInput.
 	 * @return the computed message
+	 * @throws IllegalArgumentException if input is not an instance of SigmaPedersenCommittedValueProverInput.
 	 */
-	public SigmaProtocolMsg computeFirstMsg() {
+	public SigmaProtocolMsg computeFirstMsg(SigmaProverInput in) {
+		//Converts the input to the underlying prover.
+		SigmaDlogProverInput input = convertInput(in);
+		
 		//Delegates the computation to the underlying Sigma Dlog prover.
-		return sigmaDlog.computeFirstMsg();
+		return sigmaDlog.computeFirstMsg(input);
 	}
 
 	/**
