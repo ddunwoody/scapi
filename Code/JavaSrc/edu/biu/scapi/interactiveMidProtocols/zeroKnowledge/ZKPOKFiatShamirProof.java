5e89648c0a6991ebcae98284fa8e58eb8e66eb89
Moriya Farbstein <moriyaw@gmail.com>
2013-10-01 10:19:45 +0000

    Added message contains two SigmaProtocolMessage and a challenge. The message is sent by the prover of ZKPOK Fiat Shamir protocol to the verifier.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2426 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/zeroKnowledge/ZKPOKFiatShamirProof.java
@@ -0,0 +1,63 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.zeroKnowledge;
+
+import java.io.Serializable;
+
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.utility.SigmaProtocolMsg;
+
+/** 
+ * This message contains two SigmaProtocolMessage and a challenge. 
+ * The message is sent by the prover of ZKPOK Fiat Shamir protocol to the verifier.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class ZKPOKFiatShamirProof implements Serializable{
+
+	
+	private static final long serialVersionUID = -3337598431871087383L;
+	private SigmaProtocolMsg a;
+	private byte[] e;
+	private SigmaProtocolMsg z;
+	
+	public ZKPOKFiatShamirProof(SigmaProtocolMsg a, byte[] e, SigmaProtocolMsg z){
+		this.a = a;
+		this.e = e;
+		this.z = z;
+	}
+	
+	public SigmaProtocolMsg getA(){
+		return a;
+	}
+	
+	public byte[] getE(){
+		return e;
+	}
+	
+	public SigmaProtocolMsg getZ(){
+		return z;
+	}
+}
