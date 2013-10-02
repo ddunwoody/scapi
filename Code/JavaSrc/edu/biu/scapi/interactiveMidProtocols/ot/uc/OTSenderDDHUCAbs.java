57444ecc5d61bcb04a9628fa904d66518d5ce107
Moriya Farbstein <moriyaw@gmail.com>
2013-10-02 09:38:35 +0000

    1. Abstract class tuned to package private instead of public. 2. Deleted class members that used for transfer calculations. These values are now part of the transfer function. 3. Deleted setMembers function. 4. Deleted preProcess function. 5. Transfer function now gets a channel as an argument.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2469 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/uc/OTSenderDDHUCAbs.java
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/uc/OTSenderDDHUCAbs.java
@@ -31,14 +31,13 @@ import java.security.SecureRandom;
 import edu.biu.scapi.comm.Channel;
 import edu.biu.scapi.exceptions.SecurityLevelException;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTRMessage;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSInput;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTSMessage;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTSender;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTUtil;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTUtil.RandOutput;
 import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.GroupElement;
-import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
-import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
 import edu.biu.scapi.securityLevel.DDH;
 import edu.biu.scapi.securityLevel.UC;
 
@@ -52,7 +51,7 @@ import edu.biu.scapi.securityLevel.UC;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
-public abstract class OTSenderDDHUCAbs implements OTSender, UC{
+abstract class OTSenderDDHUCAbs implements OTSender, UC{
 
 	/*	
 	  This class runs the following protocol:
@@ -65,99 +64,64 @@ public abstract class OTSenderDDHUCAbs implements OTSender, UC{
 			OUTPUT nothing
 	 */	 
 
-	private Channel channel;
 	protected DlogGroup dlog;
 	private SecureRandom random;
-	private GroupElement g0, g1, h0, h1;
-
-	//Values required for the tuple calculation:
-	protected GroupElement u0, v0, u1, v1;
-
-	/**
-	 * Constructor that sets the given channel and choose default values to the other parameters.
-	 * @param channel
-	 */
-	public OTSenderDDHUCAbs(Channel channel){
-		
-		
-			DlogGroup dlog;
-			try {
-				//Uses Miracl Koblitz 233 Elliptic curve.
-				dlog = new MiraclDlogECF2m("K-233");
-			} catch (IOException e) {
-				dlog = new CryptoPpDlogZpSafePrime();
-			} 
-		
-		GroupElement g0 = dlog.getGenerator();
-		GroupElement g1 = dlog.createRandomElement();
-		GroupElement h0 = dlog.createRandomElement();
-		GroupElement h1 = dlog.createRandomElement();
-		try {
-			setMembers(channel, dlog, g0, g1, h0, h1, new SecureRandom());
-		} catch (SecurityLevelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	private GroupElement g0, g1, h0, h1; //Common reference string
 	
 	/**
-	 * Constructor that sets the given channel, common reference string composed of a DLOG 
-	 * description (G,q,g0) and (g0,g1,h0,h1) which is a randomly chosen non-DDH tuple, 
-	 * kdf and random.
-	 * @param channel
+	 * Constructor that sets the given common reference string composed of a DLOG 
+	 * description (G,q,g0) and (g0,g1,h0,h1) which is a randomly chosen non-DDH tuple and random.
 	 * @param dlog must be DDH secure.
 	 * @param g0 
 	 * @param g1 
 	 * @param h0 
 	 * @param h1 
-	 * @param kdf
 	 * @param random
 	 * @throws SecurityLevelException if the given dlog is not DDH secure
 	 */
-	public OTSenderDDHUCAbs(Channel channel, DlogGroup dlog, GroupElement g0, GroupElement g1, 
+	OTSenderDDHUCAbs(DlogGroup dlog, GroupElement g0, GroupElement g1, 
 			GroupElement h0, GroupElement h1, SecureRandom random) throws SecurityLevelException{
-		setMembers(channel, dlog, g0, g1, h0, h1, random);
-		
-	}
-	
-	/**
-	 * Sets the given parameters.
-	 * @param channel
-	 * @param dlog must be DDH secure.
-	 * @param g0
-	 * @param g1
-	 * @param h0
-	 * @param h1
-	 * @param random
-	 * @throws SecurityLevelException if the given dlog is not DDH secure.
-	 */
-	private void setMembers(Channel channel, DlogGroup dlog, GroupElement g0, 
-			GroupElement g1, GroupElement h0, GroupElement h1, SecureRandom random) throws SecurityLevelException{
 		//The underlying dlog group must be DDH secure.
 		if (!(dlog instanceof DDH)){
 			throw new SecurityLevelException("DlogGroup should have DDH security level");
 		}
 		
-		this.channel = channel;
 		this.dlog = dlog;
 		this.random = random;
 		this.g0 = g0;
 		this.g1 = g1;
 		this.h0 = h0;
 		this.h1 = h1;
+		
+		// This protocol has no pre process stage.
 	}
 
 	/**
-	 * Runs the part of the protocol where the sender input is not yet necessary as follows:
-	 * "WAIT for message (g,h) from R
+	 * Runs the part of the protocol where the sender's input is necessary as follows:<p>
+	 *	WAIT for message (g,h) from R
 	 *	COMPUTE (u0,v0) = RAND(g0,g,h0,h)
-	 *	COMPUTE (u1,v1) = RAND(g1,g,h1,h)".
-	 * @throws IOException if failed to receive a message.
+	 *	COMPUTE (u1,v1) = RAND(g1,g,h1,h)		
+	 *	COMPUTE:<p> 
+	 *		in the byte array scenario<p>
+	 *			•   c0 = x0 XOR KDF(|x0|,v0)
+	 *			•   c1 = x1 XOR KDF(|x1|,v1)
+	 *		OR in the GroupElement scenario:<p>
+	 *			•	c0 = x0 * v0<p>
+	 *			•	c1 = x1 * v1<p>
+	 *	SEND (u0,c0) and (u1,c1) to R<p>
+	 *	OUTPUT nothing<p>
+	 * The transfer stage of OT protocol which can be called several times in parallel.
+	 * In order to enable the parallel calls, each transfer call should use a different channel to send and receive messages.
+	 * This way the parallel executions of the function will not block each other.
+	 * The parameters given in the input must match the DlogGroup member of this class, which given in the constructor.
+	 * @param channel 
+	 * @param input
+	 * @throws IOException if failed to send the message.
 	 * @throws ClassNotFoundException 
 	 */
-	public void preProcess() throws ClassNotFoundException, IOException{
+	public void transfer(Channel channel, OTSInput input) throws IOException, NullPointerException, ClassNotFoundException{
 		//Wait for message (g,h) from R
-		OTRMessage message = waitForMessageFromReceiver();
+		OTRMessage message = waitForMessageFromReceiver(channel);
 		GroupElement g = dlog.reconstructElement(true, message.getFirstGE());
 		GroupElement h = dlog.reconstructElement(true, message.getSecondGE());
 		
@@ -165,49 +129,27 @@ public abstract class OTSenderDDHUCAbs implements OTSender, UC{
 		//Compute (u1,v1) = RAND(g1,g,h1,h)
 		RandOutput tuple0 = OTUtil.rand(dlog, g0, g, h0, h, random);
 		RandOutput tuple1 = OTUtil.rand(dlog, g1, g, h1, h, random);
-		u0 = tuple0.getU();
-		v0 = tuple0.getV();
-		u1 = tuple1.getU();
-		v1 = tuple1.getV();
+		GroupElement u0 = tuple0.getU();
+		GroupElement v0 = tuple0.getV();
+		GroupElement u1 = tuple1.getU();
+		GroupElement v1 = tuple1.getV();
+		
+		//Compute c0, c1.
+		OTSMessage messageToSend = computeTuple(input, u0, u1, v0, v1);
+		
+		sendTupleToReceiver(channel, messageToSend);
+	
 	}
-
-	/**
-	 * Runs the part of the protocol where the sender's input is necessary as follows:<p>
-	 *		COMPUTE:<p> 
-	 *			in the byte array scenario<p>
-	 *				•   c0 = x0 XOR KDF(|x0|,v0)
-	 *				•   c1 = x1 XOR KDF(|x1|,v1)
-	 *			OR in the GroupElement scenario:<p>
-	 *				•	c0 = x0 * v0<p>
-	 *				•	c1 = x1 * v1<p>
-	 *		SEND (u0,c0) and (u1,c1) to R<p>
-	 *		OUTPUT nothing<p>
-	 * @throws IOException if failed to send the message.
-	 * @throws NullPointerException if the function {@link #preProcess()} has not been called at least once
-	 */
-	public void transfer() throws IOException, NullPointerException{
-		//This function can be called only after preProcess() function has been called at least once. 
-		//The caller application can choose to call preProcess for every new OT it needs to perform or to use the pre-processed values 
-		//calculated the first time for all or many upcoming transfers. It depends on the application's needs.
-		//In any case, we could check here if the necessary pre-processed values are null and if so not to transfer but since in most cases the values will not be null(assuming the user of the functionality
-		//understands what she is doing) it is enough to catch the NullPointerException and throw instead of it a IllegalStateException with an explanation.
-		try{
-			OTSMessage message = computeTuple();
-			sendTupleToReceiver(message);
-		}catch(NullPointerException e){
-			throw new IllegalStateException("preProcess function should be called before transfer at least once");
-		}
-
-	}
-
+	
 	/**
 	 * Runs the following line from the protocol:
 	 * "WAIT for message (h0,h1) from R"
+	 * @param channel 
 	 * @return the received message.
 	 * @throws ClassNotFoundException 
 	 * @throws IOException if failed to receive a message.
 	 */
-	private OTRMessage waitForMessageFromReceiver() throws ClassNotFoundException, IOException{
+	private OTRMessage waitForMessageFromReceiver(Channel channel) throws ClassNotFoundException, IOException{
 		Serializable message = null;
 		try {
 			message = channel.receive();
@@ -228,17 +170,23 @@ public abstract class OTSenderDDHUCAbs implements OTSender, UC{
 	 *		OR in the GroupElement scenario:<p>
 	 *			•	c0 = x0 * v0<p>
 	 *			•	c1 = x1 * v1<p>
+	 * @param v1 
+	 * @param v0 
+	 * @param u1 
+	 * @param u0 
+	 * @param input 
 	 * @return tuple contains (u, v0, v1) to send to the receiver.
 	 */
-	protected abstract OTSMessage computeTuple();
+	protected abstract OTSMessage computeTuple(OTSInput input, GroupElement u0, GroupElement u1, GroupElement v0, GroupElement v1);
 
 	/**
 	 * Runs the following lines from the protocol:
 	 * "SEND (u,v0,v1) to R"
+	 * @param channel 
 	 * @param message to send to the receiver
 	 * @throws IOException if failed to send the message.
 	 */
-	private void sendTupleToReceiver(OTSMessage message) throws IOException {
+	private void sendTupleToReceiver(Channel channel, OTSMessage message) throws IOException {
 
 		try {
 			//Send the message by the channel.
