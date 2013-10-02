7c46656592ce153ec087ad93f30696072d19c714
Moriya Farbstein <moriyaw@gmail.com>
2013-10-02 07:43:50 +0000

    1. Abstract class tuned to package private instead of public. 2. Deleted class members that used for calculations. These values are now part of the transfer function. 3. In default constructor we now read the default values from a configuration file. 4. changed setMembers function to doConstruct. 5. deleted preProcess function. This protocol has no pre process stage. 6. Deleted setInput function. The input is given now in the transfer function. 7. Transfer function now gets a channel as an argument.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2450 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/semiHonest/OTReceiverDDHSemiHonestAbs.java
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/semiHonest/OTReceiverDDHSemiHonestAbs.java
@@ -32,7 +32,9 @@ import java.security.SecureRandom;
 import org.bouncycastle.util.BigIntegers;
 
 import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.FactoriesException;
 import edu.biu.scapi.exceptions.SecurityLevelException;
+import edu.biu.scapi.generals.ScapiDefaultConfiguration;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTRBasicInput;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTRMessage;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTRInput;
@@ -41,9 +43,8 @@ import edu.biu.scapi.interactiveMidProtocols.ot.OTReceiver;
 import edu.biu.scapi.interactiveMidProtocols.ot.OTSMessage;
 import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.GroupElement;
-import edu.biu.scapi.primitives.dlog.cryptopp.CryptoPpDlogZpSafePrime;
-import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
 import edu.biu.scapi.securityLevel.DDH;
+import edu.biu.scapi.tools.Factories.DlogGroupFactory;
 
 /**
  * Abstract class for Semi-Honest OT assuming DDH receiver.
@@ -54,7 +55,7 @@ import edu.biu.scapi.securityLevel.DDH;
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
  *
  */
-public abstract class OTReceiverDDHSemiHonestAbs implements OTReceiver{
+abstract class OTReceiverDDHSemiHonestAbs implements OTReceiver{
 	/*	
 	 	This class runs the following protocol:
 		 	SAMPLE random values alpha in Zq and h in the DlogGroup 
@@ -69,109 +70,89 @@ public abstract class OTReceiverDDHSemiHonestAbs implements OTReceiver{
 				 OR xSigma = vSigma * (kSigma)^(-1) 			- in GroupElement scenario
 	*/	
 	
-	private Channel channel;
 	protected DlogGroup dlog;
 	private SecureRandom random;
 	private BigInteger qMinusOne;
 	
-	//Values required for calculations:
-	protected short sigma;
-	BigInteger alpha;
-	private GroupElement gAlpha;
-	private GroupElement h;
-	
 	/**
-	 * Constructor that gets the channel and chooses default values of DlogGroup and SecureRandom.
+	 * Constructor that chooses default values of DlogGroup and SecureRandom.
 	 */
-	public OTReceiverDDHSemiHonestAbs(Channel channel){
-		try{
-			try {
-				//Uses Miracl Koblitz 233 Elliptic curve.
-				setMembers(channel, new MiraclDlogECF2m("K-233"), new SecureRandom());
-			} catch (IOException e) {
-				//If there is a problem with the elliptic curves file, create Zp DlogGroup.
-				setMembers(channel, new CryptoPpDlogZpSafePrime(), new SecureRandom());
-			}
-		} catch (SecurityLevelException e) {
-			// Can not occur since the DlogGroup is DDH secure.
+	OTReceiverDDHSemiHonestAbs(){
+		//Read the default DlogGroup name from a configuration file.
+		String dlogName = ScapiDefaultConfiguration.getInstance().getProperty("DDHDlogGroup");
+		DlogGroup dlog = null;
+		try {
+			//Create the default DlogGroup by the factory.
+			dlog = DlogGroupFactory.getInstance().getObject(dlogName);
+			
+		} catch (FactoriesException e1) {
+			// Should not occur since the dlog name in the configuration file is valid.
+		}
+		
+		try {
+			doConstruct(dlog, new SecureRandom());
+		} catch (SecurityLevelException e1) {
+			// Should not occur since the dlog in the configuration file is as secure as needed.
 		}
 	}
 
 	/**
-	 * Constructor that sets the given channel, dlogGroup and random.
-	 * @param channel
+	 * Constructor that sets the given dlogGroup and random.
 	 * @param dlog must be DDH secure.
 	 * @param random
 	 * @throws SecurityLevelException if the given dlog is not DDH secure.
 	 */
-	public OTReceiverDDHSemiHonestAbs(Channel channel, DlogGroup dlog, SecureRandom random) throws SecurityLevelException{
+	OTReceiverDDHSemiHonestAbs(DlogGroup dlog, SecureRandom random) throws SecurityLevelException{
 		
-		setMembers(channel, dlog, random);
+		doConstruct(dlog, random);
 	}
 	
 	/**
 	 * Sets the given members.
-	 * @param channel
 	 * @param dlog must be DDH secure.
 	 * @param random
 	 * @throws SecurityLevelException if the given dlog is not DDH secure.
 	 */
-	private void setMembers(Channel channel, DlogGroup dlog, SecureRandom random) throws SecurityLevelException {
+	private void doConstruct(DlogGroup dlog, SecureRandom random) throws SecurityLevelException {
 		//The underlying dlog group must be DDH secure.
 		if (!(dlog instanceof DDH)){
 			throw new SecurityLevelException("DlogGroup should have DDH security level");
 		}
 		
-		this.channel = channel;
 		this.dlog = dlog;
 		this.random = random;
 		qMinusOne =  dlog.getOrder().subtract(BigInteger.ONE);
 		
+		// This protocol has no pre process stage.
+		
 	}
 	
 	/**
-	 * Runs the part of the protocol where the receiver input is not yet necessary.
-	 */
-	public void preProcess(){
-		/* Run the following part of the protocol:
-				SAMPLE random values alpha in  [0, . . . , q-1]  and h in the DlogGroup. 
-				COMPUTE g^alpha 
-		*/
-		
-		//Sample random values.
-		sampleRandomValues();
-		
-		//Calculate g^alpha.
-		GroupElement g = dlog.getGenerator();
-		gAlpha = dlog.exponentiate(g, alpha);
-	}
-
-	/**
-	 * Sets the input for this OT receiver.
+	 * Runs the part of the protocol where the receiver input is necessary.
+	 * The transfer stage of OT protocol which can be called several times in parallel.
+	 * In order to enable the parallel calls, each transfer call should use a different channel to send and receive messages.
+	 * This way the parallel executions of the function will not block each other.
+	 * The parameters given in the input must match the DlogGroup member of this class, which given in the constructor.
 	 * @param input MUST be OTRBasicInput.
+	 * @return OTROutput, the output of the protocol.
+	 * @throws IOException if failed to send or receive a message.
+	 * @throws ClassNotFoundException
 	 */
-	public void setInput(OTRInput input) {
+	public OTROutput transfer(Channel channel, OTRInput input) throws IOException, ClassNotFoundException{
+		//check if the input is valid.
 		//If input is not instance of OTRBasicInput, throw Exception.
 		if (!(input instanceof OTRBasicInput)){
 			throw new IllegalArgumentException("input shoud contain sigma.");
 		}
 		
+		byte sigma = ((OTRBasicInput) input).getSigma();
 		//The given sigma should be 0 or 1.
 		if ((sigma != 0) && (sigma!= 1)){
 			throw new IllegalArgumentException("Sigma should be 0 or 1");
 		}
-		//Set sigma.
-		this.sigma = ((OTRBasicInput) input).getSigma();
-	}
-	
-	/**
-	 * Runs the part of the protocol where the receiver input is necessary.
-	 * @return OTROutput, the output of the protocol.
-	 * @throws IOException if failed to send or receive a message.
-	 * @throws ClassNotFoundException
-	 */
-	public OTROutput transfer() throws IOException, ClassNotFoundException{
-		/* Run the following part of the protocol:
+		
+		/* Run the protocol:
+				SAMPLE random values alpha in  [0, . . . , q-1]  and h in the DlogGroup
 				COMPUTE h0,h1 as follows:
 					1.	If sigma = 0 then h0 = g^alpha  and h1 = h
 					2.	If sigma = 1 then h0 = h and h1 = g^alpha 
@@ -182,38 +163,42 @@ public abstract class OTReceiverDDHSemiHonestAbs implements OTReceiver{
 				OUTPUT  xSigma = vSigma XOR KDF(|cSigma|,kSigma)	- in byte array scenario
 					 OR xSigma = vSigma * (kSigma)^(-1) 			- in GroupElement scenario
 		*/
-		try{
-			OTRMessage tuple = computeTuple();
-			sendTupleToSender(tuple);
-			OTSMessage message = waitForMessageFromSender();
-			return computeFinalXSigma(message);
-			
-		}catch(NullPointerException e){
-			throw new IllegalStateException("preProcess function should be called before transfer atleast once");
-		}
 		
-	}
+		//Sample random alpha
+		BigInteger alpha = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		
+		//Compute h0, h1
+		OTRMessage tuple = computeTuple(alpha, sigma);
+		
+		//Send the tuple to sender
+		sendTupleToSender(channel, tuple);
+		
+		//Wait for message from sender
+		OTSMessage message = waitForMessageFromSender(channel);
+		
+		//Compute xSigma
+		return computeFinalXSigma(sigma, alpha, message);
 	
-	/**
-	 * Runs the following lines from the protocol:
-	 * "SAMPLE random values alpha in  [0, . . . , q-1]  and h in the DlogGroup". 
-	 */
-	private void sampleRandomValues() {
-		//Sample random alpha.
-		alpha = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
-		
-		//Sample random h.
-		h = dlog.createRandomElement();
 	}
 
 	/**
 	 * Runs the following lines from the protocol:
-	 * "COMPUTE h0,h1 as follows:
+	 *  COMPUTE h0,h1 as follows:
 	 *		1.	If sigma = 0 then h0 = g^alpha  and h1 = h
 	 *		2.	If sigma = 1 then h0 = h and h1 = g^alpha"
+	 * @param alpha random value sampled by the protocol
+	 * @param sigma input for the protocol
 	 * @return OTRSemiHonestMessage contains the tuple (h0, h1).
 	 */
-	private OTRMessage computeTuple() {
+	private OTRMessage computeTuple(BigInteger alpha, byte sigma) {
+		
+		//Sample random h.
+		GroupElement h = dlog.createRandomElement();
+		
+		//Calculate g^alpha.
+		GroupElement g = dlog.getGenerator();
+		GroupElement gAlpha = dlog.exponentiate(g, alpha);
+				
 		GroupElement h0 = null;
 		GroupElement h1 = null;
 		if (sigma == 0){
@@ -230,10 +215,11 @@ public abstract class OTReceiverDDHSemiHonestAbs implements OTReceiver{
 	/**
 	 * Runs the following line from the protocol:
 	 * "SEND (h0,h1) to S"
-	 * @param tuple to send to the sender
+	 * @param channel 
+	 * @param tuple contains (h0,h1)
 	 * @throws IOException if failed to send the message.
 	 */
-	private void sendTupleToSender(OTRMessage tuple) throws IOException {
+	private void sendTupleToSender(Channel channel, OTRMessage tuple) throws IOException {
 		try {
 			channel.send(tuple);
 		} catch (IOException e) {
@@ -245,11 +231,12 @@ public abstract class OTReceiverDDHSemiHonestAbs implements OTReceiver{
 	/**
 	 * Runs the following line from the protocol:
 	 * "WAIT for the message (u, v0,v1) from S"
+	 * @param channel 
 	 * @return OTSMessage contains (u, v0,v1)
 	 * @throws ClassNotFoundException
 	 * @throws IOException if failed to receive a message.
 	 */
-	private OTSMessage waitForMessageFromSender() throws ClassNotFoundException, IOException {
+	private OTSMessage waitForMessageFromSender(Channel channel) throws ClassNotFoundException, IOException {
 		Serializable message;
 		try {
 			message = channel.receive();
@@ -264,12 +251,14 @@ public abstract class OTReceiverDDHSemiHonestAbs implements OTReceiver{
 	
 	/**
 	 * Runs the following lines from the protocol:
-	 * "COMPUTE kσ = (u)^alpha					- in byte array scenario
-	 *		OR (kσ)^(-1) = u^(-alpha)			- in GroupElement scenario
-	 *		OUTPUT  xσ = vσ XOR KDF(|cσ|,kσ)	- in byte array scenario
-	 *		 	 OR xσ = vσ * (kσ)^(-1)" 		- in GroupElement scenario
+	 * "COMPUTE kSigma = (u)^alpha							- in byte array scenario
+			OR (kSigma)^(-1) = u^(-alpha)					- in GroupElement scenario
+		OUTPUT  xSigma = vSigma XOR KDF(|cSigma|,kSigma)	- in byte array scenario
+			 OR xSigma = vSigma * (kSigma)^(-1) 			- in GroupElement scenario
+	 * @param sigma input for the protocol
+	 * @param alpha random value sampled by the protocol
 	 * @param message received from the sender
-	 * @return OTROutput contains Xσ
+	 * @return OTROutput contains XSigma
 	 */
-	protected abstract OTROutput computeFinalXSigma(OTSMessage message);
+	protected abstract OTROutput computeFinalXSigma(byte sigma, BigInteger alpha, OTSMessage message);
 }
