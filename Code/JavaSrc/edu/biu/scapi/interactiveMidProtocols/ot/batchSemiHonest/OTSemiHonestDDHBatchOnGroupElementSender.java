46eed0f235e920d03214e523821dbe87368d2f76
Moriya Farbstein <moriyaw@gmail.com>
2013-10-07 09:35:56 +0000

    Added implementation of sender side in batch OT Semi-Honest. There is an abstract class and concrete classes for byte array and group element inputs.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2577 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/batchSemiHonest/OTSemiHonestDDHBatchOnGroupElementSender.java
@@ -0,0 +1,105 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot.batchSemiHonest;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+import edu.biu.scapi.exceptions.SecurityLevelException;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSBatchOnGroupElementInput;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSInput;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSMsg;
+import edu.biu.scapi.interactiveMidProtocols.ot.semiHonest.OTSemiHonestDDHOnGroupElementSenderMsg;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.securityLevel.SemiHonest;
+
+/**
+ * Concrete class for Batch Semi-Honest OT assuming DDH sender ON GROUP ELEMENT.
+ * This class derived from OTSemiHonestDDHBatchAbs and implements the functionality 
+ * related to the GroupElement inputs.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class OTSemiHonestDDHBatchOnGroupElementSender extends OTSemiHonestDDHBatchSenderAbs implements SemiHonest{
+	
+	/**
+	 * Constructor that chooses default values of DlogGroup and SecureRandom.
+	 */
+	public OTSemiHonestDDHBatchOnGroupElementSender(){
+		super();
+	}
+	
+	/**
+	 * Constructor that sets the given dlogGroup and random.
+	 * @param dlog must be DDH secure.
+	 * @param random
+	 * @throws SecurityLevelException if the given DlogGroup is not DDH secure.
+	 */
+	public OTSemiHonestDDHBatchOnGroupElementSender(DlogGroup dlog, SecureRandom random) throws SecurityLevelException{
+		super(dlog, random);
+	}
+	
+	/**
+	 * Runs the following lines from the protocol:
+	 * "COMPUTE:
+	 *	•   vi0 = xi0 * ki0
+	 *	•	vi1 = xi1 * ki1""
+	 * @param input MUST be an instance of OTSBatchOnGroupElementInput
+	 * @param k1Arr 
+	 * @param k0Arr 
+	 * @param u 
+	 * @return tuple contains (ui, vi0, vi1) to send to the receiver.
+	 */
+	protected OTSMsg computeMsg(OTSInput input, GroupElement u, ArrayList<GroupElement> k0Arr, ArrayList<GroupElement> k1Arr) {
+		//If input is not instance of OTSBatchOnGroupElementInput, throw Exception.
+		if (!(input instanceof OTSBatchOnGroupElementInput)){
+			throw new IllegalArgumentException("input should be an instance of OTSBatchOnGroupElementInput.");
+		}
+				
+		//Set x0, x1.
+		ArrayList<GroupElement> x0Arr = ((OTSBatchOnGroupElementInput) input).getX0Arr();
+		ArrayList<GroupElement> x1Arr = ((OTSBatchOnGroupElementInput) input).getX1Arr();
+		int size = x0Arr.size();
+		
+		ArrayList<OTSemiHonestDDHOnGroupElementSenderMsg> tuples = new ArrayList<OTSemiHonestDDHOnGroupElementSenderMsg>();
+		
+		for (int i=0; i<size; i++){
+			//Calculate v0:
+			GroupElement v0 = dlog.multiplyGroupElements(x0Arr.get(i), k0Arr.get(i));
+			
+			//Calculate v1:
+			GroupElement v1 = dlog.multiplyGroupElements(x1Arr.get(i), k1Arr.get(i));
+			
+			tuples.add(i, new OTSemiHonestDDHOnGroupElementSenderMsg(u.generateSendableData(), 
+								v0.generateSendableData(), v1.generateSendableData()));
+		}
+		
+		//Create and return sender message.
+		return new OTSemiHonestDDHBatchOnGroupElementSenderMsg(tuples);
+	}
+
+}
---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/batchSemiHonest/OTSemiHonestDDHBatchSenderAbs.java
@@ -0,0 +1,247 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot.batchSemiHonest;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+import org.bouncycastle.util.BigIntegers;
+
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.FactoriesException;
+import edu.biu.scapi.exceptions.SecurityLevelException;
+import edu.biu.scapi.generals.ScapiDefaultConfiguration;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTRGroupElementPairMsg;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSInput;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSMsg;
+import edu.biu.scapi.interactiveMidProtocols.ot.OTSender;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.securityLevel.DDH;
+import edu.biu.scapi.tools.Factories.DlogGroupFactory;
+
+/**
+ * Abstract class for batch Semi-Honest OT assuming DDH sender.
+ * Batch Semi-Honest OT has two modes: one is on ByteArray and the second is on GroupElement.
+ * The difference is in the input and output types and the way to process them. 
+ * In spite that, there is a common behavior for both modes which this class implements.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+abstract class OTSemiHonestDDHBatchSenderAbs implements OTSender{
+
+	/*	
+	  This class runs the following protocol:
+		 	WAIT for message (h0i,h1i) from R
+			SAMPLE a single random value r <-  {0, . . . , q-1} and COMPUTE u=g^r
+			For every i=1,...,m, COMPUTE:
+			•	ki0 = (hi0)^r
+			•	ki1 = (hi1)^r
+			In the byte array scenario:
+			•	vi0 = xi0 XOR KDF(|xi0|,ki0)
+			•	vi1 = xi1 XOR KDF(|xi1|,ki1)
+			In the group element scenario:
+			•   vi0 = xi0 * ki0
+			•	vi1 = xi1 * ki1
+			For every i=1,...,m, SEND (u,vi0,vi1) to R
+			OUTPUT nothing
+
+	 */	 
+
+	protected DlogGroup dlog;
+	private SecureRandom random;
+	private BigInteger qMinusOne;
+
+	/**
+	 * Constructor that chooses default values of DlogGroup and SecureRandom.
+	 */
+	OTSemiHonestDDHBatchSenderAbs() {
+		//Read the default DlogGroup name from a configuration file.
+		String dlogName = ScapiDefaultConfiguration.getInstance().getProperty("DDHDlogGroup");
+		DlogGroup dlog = null;
+		try {
+			//Create the default DlogGroup by the factory.
+			dlog = DlogGroupFactory.getInstance().getObject(dlogName);
+			System.out.println(dlog.getGroupType());
+		} catch (FactoriesException e1) {
+			// Should not occur since the dlog name in the configuration file is valid.
+		}
+		
+		try {
+			doConstruct(dlog, new SecureRandom());
+		} catch (SecurityLevelException e1) {
+			// Should not occur since the dlog in the configuration file is as secure as needed.
+		}
+	}
+
+	/**
+	 * Constructor that sets the given dlogGroup and random.
+	 * @param dlog must be DDH secure.
+	 * @param random
+	 * @throws SecurityLevelException if the given dlog is not DDH secure
+	 */
+	OTSemiHonestDDHBatchSenderAbs(DlogGroup dlog, SecureRandom random) throws SecurityLevelException{
+
+		doConstruct(dlog, random);
+	}
+
+	/**
+	 * Sets the given members.
+	 * @param dlog must be DDH secure.
+	 * @param random
+	 * @throws SecurityLevelException if the given dlog is not DDH secure.
+	 */
+	private void doConstruct(DlogGroup dlog, SecureRandom random) throws SecurityLevelException {
+		//The underlying dlog group must be DDH secure.
+		if (!(dlog instanceof DDH)){
+			throw new SecurityLevelException("DlogGroup should have DDH security level");
+		}
+
+		this.dlog = dlog;
+		this.random = random;
+		qMinusOne =  dlog.getOrder().subtract(BigInteger.ONE);
+
+		// This protocol has no pre process stage.
+	}
+
+	/**
+	 * Runs the part of the protocol where the input is necessary as follows:<p>
+	 *	WAIT for message (h0i,h1i) from R
+	 *	SAMPLE a single random value r <-  {0, . . . , q-1} and COMPUTE u=g^r
+	 *	For every i=1,...,m, COMPUTE:
+	 *		•	ki0 = (hi0)^r
+	 *		•	ki1 = (hi1)^r
+	 *		In the byte array scenario:
+	 *		•	vi0 = xi0 XOR KDF(|xi0|,ki0)
+	 *		•	vi1 = xi1 XOR KDF(|xi1|,ki1)
+	 *		In the group element scenario:
+	 *		•   vi0 = xi0 * ki0
+	 *		•	vi1 = xi1 * ki1
+	 *	For every i=1,...,m, SEND (u,vi0,vi1) to R
+	 *	OUTPUT nothing<p>
+	 * The transfer stage of OT protocol which can be called several times in parallel.
+	 * In order to enable the parallel calls, each transfer call should use a different channel to send and receive messages.
+	 * This way the parallel executions of the function will not block each other.
+	 * The parameters given in the input must match the DlogGroup member of this class, which given in the constructor.
+	 * @param channel
+	 * @param input
+	 * @throws IOException if failed to send the message.
+	 * @throws ClassNotFoundException 
+	 */
+	public void transfer(Channel channel, OTSInput input) throws IOException, NullPointerException, ClassNotFoundException{
+		//WAIT for message (hi0,hi1) from R
+		OTRGroupElementBatchMsg message = waitForMessageFromReceiver(channel);
+		
+		//SAMPLE a random value r in  [0, . . . , q-1] 
+		BigInteger r = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		GroupElement g = dlog.getGenerator(); //Get the group generator.
+		
+		//Calculate u = g^r.
+		GroupElement u = dlog.exponentiate(g, r);
+		
+		ArrayList<OTRGroupElementPairMsg> tuples = message.getTuples();
+		int size = tuples.size();
+		ArrayList<GroupElement> k0Array = new ArrayList<GroupElement>();
+		ArrayList<GroupElement> k1Array = new ArrayList<GroupElement>();
+		GroupElement h0, h1;
+		OTRGroupElementPairMsg tuple;
+		
+		//For every i=1,...,m, COMPUTE:
+		//	ki0 = (hi0)^r
+		//	ki1 = (hi1)^r
+		for (int i=0; i<size; i++){
+			tuple = tuples.get(i);
+			//Recreate h0 from the data in the received message.
+			h0 = dlog.reconstructElement(true, tuple.getFirstGE());
+			h1 = dlog.reconstructElement(true, tuple.getSecondGE());
+			
+			//Calculate k0 = h0^r.
+			k0Array.add(i, dlog.exponentiate(h0, r));
+			k1Array.add(i, dlog.exponentiate(h1, r));
+		}
+		
+		OTSMsg messageToSend = computeMsg(input, u, k0Array, k1Array);
+		sendTupleToReceiver(channel, messageToSend);
+	}
+
+	/**
+	 * Runs the following line from the protocol:
+	 * "WAIT for message (h0,h1) from R"
+	 * @param channel 
+	 * @return the received message.
+	 * @throws ClassNotFoundException 
+	 * @throws IOException if failed to receive a message.
+	 */
+	private OTRGroupElementBatchMsg waitForMessageFromReceiver(Channel channel) throws ClassNotFoundException, IOException{
+		Serializable message = null;
+		try {
+			message = channel.receive();
+		} catch (IOException e) {
+			throw new IOException("Failed to receive message. The thrown message is: " + e.getMessage());
+		}
+		if (!(message instanceof OTRGroupElementBatchMsg)){
+			throw new IllegalArgumentException("The received message should be an instance of OTRGroupElementBatchMsg");
+		}
+		return (OTRGroupElementBatchMsg) message;
+	}
+
+	/**
+	 * Runs the following lines from the protocol:
+	 * "In the byte array scenario:
+	 *	•	vi0 = xi0 XOR KDF(|xi0|,ki0)
+	 *	•	vi1 = xi1 XOR KDF(|xi1|,ki1)
+	 *	In the group element scenario:
+	 *	•   vi0 = xi0 * ki0
+	 *	•	vi1 = xi1 * ki1"
+	 * @param input
+	 * @param k1Array
+	 * @param k0Array
+	 * @param u 
+	 * @return tuple contains (u, v0, v1) to send to the receiver.
+	 */
+	protected abstract OTSMsg computeMsg(OTSInput input, GroupElement u, ArrayList<GroupElement> k0Array, ArrayList<GroupElement> k1Array);
+
+	/**
+	 * Runs the following lines from the protocol:
+	 * "For every i=1,...,m, SEND (u,vi0,vi1) to R"
+	 * @param channel 
+	 * @param message to send to the receiver
+	 * @throws IOException if failed to send the message.
+	 */
+	private void sendTupleToReceiver(Channel channel, OTSMsg message) throws IOException {
+
+		try {
+			//Send the message by the channel.
+			channel.send(message);
+		} catch (IOException e) {
+			throw new IOException("failed to send the message. The thrown message is: " + e.getMessage());
+		}	
+	}
+
+}
