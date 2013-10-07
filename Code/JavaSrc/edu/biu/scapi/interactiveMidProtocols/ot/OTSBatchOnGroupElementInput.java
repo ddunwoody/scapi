d607bbb667e3b8f078a02fee995d40d6e6f56c28
Moriya Farbstein <moriyaw@gmail.com>
2013-10-07 09:39:17 +0000

    Utility classes in Batch OT Semi-Honest. These are inputs and output of the sender and receiver.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2579 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/OTSBatchOnGroupElementInput.java
@@ -0,0 +1,54 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot;
+
+import java.util.ArrayList;
+
+import edu.biu.scapi.primitives.dlog.GroupElement;
+
+/**
+ * Concrete implementation of batch OT sender (on GroupElement) input.
+ * In the GroupElement scenario, the sender gets for each i=1,...,m, two DlogGroup elements x0, x1.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class OTSBatchOnGroupElementInput implements OTSInput{
+	private ArrayList<GroupElement> x0Arr;
+	private ArrayList<GroupElement> x1Arr;
+	
+	public OTSBatchOnGroupElementInput(ArrayList<GroupElement> x0Arr, ArrayList<GroupElement> x1Arr){
+		this.x0Arr = x0Arr;
+		this.x1Arr = x1Arr;
+	}
+	
+	public ArrayList<GroupElement> getX0Arr(){
+		return x0Arr;
+	}
+	
+	public ArrayList<GroupElement> getX1Arr(){
+		return x1Arr;
+	}
+}
