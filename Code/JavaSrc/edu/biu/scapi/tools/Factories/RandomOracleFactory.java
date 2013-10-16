0a786154d6c55ec129edc8367ffb733fb91989d1
Moriya Farbstein <moriyaw@gmail.com>
2013-10-16 10:54:18 +0000

    Added implementation of RandomOracle factory.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2592 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/tools/Factories/RandomOracleFactory.java
@@ -0,0 +1,85 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.tools.Factories;
+
+import edu.biu.scapi.exceptions.FactoriesException;
+import edu.biu.scapi.primitives.randomOracle.RandomOracle;
+
+/**
+ * RandomOracleFactory has a member of type FactoriesUtility to which it delegates the actual creation of the object. 
+ * This ensures proper code re-use.
+ * The factories have two getObject methods to retrieve an algorithm compatible with the type; 
+ * one specifies the provider and the other one relies on a default provider.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class RandomOracleFactory {
+	private FactoriesUtility factoriesUtility;
+	private static RandomOracleFactory instance = new RandomOracleFactory();
+
+	/**
+	 * Private constructor since this class is of the singleton pattern. 
+	 * It creates an instance of FactoriesUtility and passes a predefined file names to the constructor
+	 * of FactoriesUtility.
+	 * 
+	 */
+	private RandomOracleFactory() {
+	
+		//create an instance of FactoriesUtility with the predefined file names.
+		factoriesUtility = new FactoriesUtility("RandomOracleDefault.properties", "RandomOracle.properties");
+		
+	}
+	
+	/** 
+	 * @param provider the required provider name
+	 * @param algName the required algorithm name
+	 * @return an object of type RandomOracle class that was determined by the algName + provider
+	 * @throws FactoriesException 
+	 */
+	public RandomOracle getObject(String algName, String provider) throws FactoriesException {
+		
+		return (RandomOracle) factoriesUtility.getObject(provider, algName);
+	}
+
+	/** 
+	 * 
+	 * @param algName the required algorithm name
+	 * @return an object of type TargetCollisionResistant class that was determined by the algName + the default provider for that algorithm
+	 * @throws FactoriesException 
+	 */
+	public RandomOracle getObject(String algName) throws FactoriesException {
+		
+		return (RandomOracle) factoriesUtility.getObject(algName);
+	}
+
+	/** 
+	 * @return the singleton instance.
+	 */
+	public static RandomOracleFactory getInstance() {
+		return instance;
+
+	}
+}
