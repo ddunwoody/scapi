3f1fa1f3a184a397132f40535b4498df2f0be037
Moriya Farbstein <moriyaw@gmail.com>
2013-11-04 08:39:32 +0000

    Added interface of commitment message that the committer sends to the receiver. Each commitment protocol has to implement this interface.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2656 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/commitmentScheme/CmtCCommitmentMsg.java
@@ -0,0 +1,46 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.commitmentScheme;
+
+/**
+ * This interface represents the commitment message sent from the committer to the receiver during the commitment phase.
+ * Every commitment has an id needed to identify the specific commitment in the case that many commitments are performed
+ * by the committer without decommiting in between the commitments. Each commitment has an id attached to it used lated for decommitment.
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
+ *
+ */
+public interface CmtCCommitmentMsg {
+	/**
+	 * 
+	 * @return the commitment id
+	 */
+	long getId();
+	
+	/**
+	 * The commitment objects can vary, therefore returns an Object instance.
+	 * @return the commitment object.
+	 */
+	Object getCommitment();
+}
