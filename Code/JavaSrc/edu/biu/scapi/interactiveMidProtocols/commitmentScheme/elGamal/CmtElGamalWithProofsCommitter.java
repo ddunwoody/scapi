4b15258dbb25ef65b120f1d373762bcbd3bf3940
Moriya Farbstein <moriyaw@gmail.com>
2013-11-04 09:09:52 +0000

    Added implementation of ElGamal commitment with proofs. This class is used to prove ElGamal knowledge and ElGamalCommittedValue knowledge, using ZK based on SigmaElGamalKnowledge and SigmaElGamalCommittedValue.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2672 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/commitmentScheme/elGamal/CmtElGamalWithProofsCommitter.java
@@ -0,0 +1,128 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.commitmentScheme.elGamal;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.exceptions.CommitValueException;
+import edu.biu.scapi.exceptions.InvalidDlogGroupException;
+import edu.biu.scapi.exceptions.SecurityLevelException;
+import edu.biu.scapi.generals.ScapiDefaultConfiguration;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaProverComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCTKnowledge.SigmaElGamalCTKnowledgeProverComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCTKnowledge.SigmaElGamalCTKnowledgeProverInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCommittedValue.SigmaElGamalCommittedValueProverComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCommittedValue.SigmaElGamalCommittedValueProverInput;
+import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CmtWithProofsCommitter;
+import edu.biu.scapi.interactiveMidProtocols.zeroKnowledge.ZKFromSigmaProver;
+import edu.biu.scapi.interactiveMidProtocols.zeroKnowledge.ZKPOKFromSigmaCommitPedersenProver;
+import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ElGamalPublicKey;
+import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ScElGamalPrivateKey;
+import edu.biu.scapi.midLayer.ciphertext.ElGamalCiphertextSendableData;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+
+/**
+ * Concrete implementation of committer with proofs.
+ * This implementation uses ZK based on SigmaElGamalKnowledge and SIgmaElGamalCommittedValue.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class CmtElGamalWithProofsCommitter extends CmtElGamalOnGroupElementCommitter implements CmtWithProofsCommitter{
+
+	//Proves that the committer knows the committed value.
+	private ZKPOKFromSigmaCommitPedersenProver knowledgeProver;
+	//Proves that the committed value is x.
+	private ZKFromSigmaProver committedValProver;
+	
+	/**
+	 * Default constructor that gets the channel and creates the ZK provers with default Dlog group.
+	 * @param channel
+	 * @throws IOException
+	 */
+	public CmtElGamalWithProofsCommitter(Channel channel) throws IOException {
+		super(channel);
+		String statisticalParameter = ScapiDefaultConfiguration.getInstance().getProperty("StatisticalParameter");
+		int t = Integer.parseInt(statisticalParameter);
+		doConstruct(t, new SecureRandom());
+	}
+	
+	/**
+	 * Constructor that gets the channel, dlog, statistical parameter and random and uses them to create the ZK provers.
+	 * @param channel
+	 * @param dlog
+	 * @param t statistical parameter
+	 * @param random
+	 * @throws IllegalArgumentException
+	 * @throws SecurityLevelException if the given dlog is not DDH secure
+	 * @throws InvalidDlogGroupException if the given dlog is not valid.
+	 * @throws IOException if there was a problem in the communication
+	 */
+	public CmtElGamalWithProofsCommitter(Channel channel, DlogGroup dlog, int t, SecureRandom random) throws IllegalArgumentException, SecurityLevelException, InvalidDlogGroupException, IOException {
+		super(channel, dlog, random);	
+		doConstruct(t, random);
+	}
+	
+	/**
+	 * Creates the ZK provers using sigma protocols that prove Pedersen's proofs.
+	 * @param t statistical parameter
+	 * @throws IOException if there was a problem in the communication
+	 */
+	private void doConstruct(int t, SecureRandom random) throws IOException {
+		SigmaProverComputation elGamalCommittedValProver = new SigmaElGamalCommittedValueProverComputation(dlog, t, random);
+		SigmaProverComputation elGamalCTKnowledgeProver = new SigmaElGamalCTKnowledgeProverComputation(dlog, t, random);
+		knowledgeProver = new  ZKPOKFromSigmaCommitPedersenProver(channel, elGamalCTKnowledgeProver);
+		committedValProver = new ZKFromSigmaProver(channel, elGamalCommittedValProver);
+		
+	}
+
+	@Override
+	public void proveKnowledge(long id) throws IOException, CheatAttemptException, ClassNotFoundException {
+		Object[] keys = getPreProcessValues();
+		SigmaElGamalCTKnowledgeProverInput input =  new SigmaElGamalCTKnowledgeProverInput
+				((ElGamalPublicKey) keys[0], ((ScElGamalPrivateKey) keys[1]).getX());
+		knowledgeProver.prove(input);
+	}
+
+	@Override
+	public void proveCommittedValue(long id) throws IOException, CheatAttemptException, ClassNotFoundException, CommitValueException  {
+		//Send s1 to P2
+		CmtElGamalCommitmentPhaseValues val = getCommitmentPhaseValues(id);
+		try {
+			channel.send(val.getX().generateSendableData());
+		} catch (IOException e) {
+			throw new IOException("failed to send the message. The thrown message is: " + e.getMessage());
+		}
+		SigmaElGamalCommittedValueProverInput input =  new SigmaElGamalCommittedValueProverInput(
+				(ElGamalPublicKey)getPreProcessValues()[0], 
+				(ElGamalCiphertextSendableData) val.getComputedCommitment().generateSendableData(), 
+				(GroupElement)val.getX().getX(), val.getR().getR());
+		committedValProver.prove(input);
+	}
+}
---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/commitmentScheme/elGamal/CmtElGamalWithProofsReceiver.java
@@ -0,0 +1,151 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.commitmentScheme.elGamal;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.SecureRandom;
+
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.exceptions.CommitValueException;
+import edu.biu.scapi.exceptions.InvalidDlogGroupException;
+import edu.biu.scapi.exceptions.SecurityLevelException;
+import edu.biu.scapi.generals.ScapiDefaultConfiguration;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.SigmaVerifierComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCTKnowledge.SigmaElGamalCTKnowledgeCommonInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCTKnowledge.SigmaElGamalCTKnowledgeVerifierComputation;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCommittedValue.SigmaElGamalCommittedValueCommonInput;
+import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCommittedValue.SigmaElGamalCommittedValueVerifierComputation;
+import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CmtWithProofsReceiver;
+import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CmtGroupElementCommitValue;
+import edu.biu.scapi.interactiveMidProtocols.zeroKnowledge.ZKFromSigmaVerifier;
+import edu.biu.scapi.interactiveMidProtocols.zeroKnowledge.ZKPOKFromSigmaCommitPedersenVerifier;
+import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ElGamalPublicKey;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+import edu.biu.scapi.primitives.dlog.GroupElementSendableData;
+
+/**
+ * Concrete implementation of receiver with proofs.
+ * 
+ * This implementation uses ZK based on SigmaElGamalKnowledge and SIgmaElGamalCommittedValue.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein)
+ *
+ */
+public class CmtElGamalWithProofsReceiver extends CmtElGamalOnGroupElementReceiver implements CmtWithProofsReceiver{
+	
+	//Verifies that the committer knows the committed value.
+	private ZKPOKFromSigmaCommitPedersenVerifier knowledgeVerifier;
+	//Proves that the committed value is x.
+	private ZKFromSigmaVerifier committedValVerifier;
+	
+	/**
+	 * Default constructor that gets the channel and creates the ZK verifiers with default Dlog group.
+	 * @param channel
+	 * @throws ClassNotFoundException if there was a problem in the serialization.
+	 * @throws IOException if there was a problem in the communication level.
+	 * @throws CheatAttemptException if the receiver suspects the committer try to cheat.
+	 */
+	public CmtElGamalWithProofsReceiver(Channel channel) throws IOException, ClassNotFoundException, CheatAttemptException {
+		super(channel);
+		String statisticalParameter = ScapiDefaultConfiguration.getInstance().getProperty("StatisticalParameter");
+		int t = Integer.parseInt(statisticalParameter);
+		try {
+			doConstruct(t, new SecureRandom());
+		} catch (InvalidDlogGroupException e) {
+			//Should not occur since the default dlog is valid.
+		}
+	}
+	
+	/**
+	 * Constructor that gets the channel, dlog, statistical parameter and random and uses
+	 * them to create the ZK verifiers.
+	 * @param channel
+	 * @param dlog
+	 * @param t statistical parameter
+	 * @param random
+	 * @throws SecurityLevelException if the given dlog is not DDH secure
+	 * @throws InvalidDlogGroupException if the given dlog is not valid.
+	 * @throws ClassNotFoundException if there was a problem in the serialization
+	 * @throws IOException if there was a problem in the communication
+	 * @throws CheatAttemptException if the receiver h is not in the DlogGroup.
+	 */
+	public CmtElGamalWithProofsReceiver(Channel channel, DlogGroup dlog, int t, SecureRandom random) throws IllegalArgumentException, SecurityLevelException, InvalidDlogGroupException, IOException, ClassNotFoundException, CheatAttemptException {
+		super(channel, dlog);	
+		doConstruct(t, random);
+	}
+	
+	/**
+	 * Creates the ZK verifiers using sigma protocols that verifies ElGamal's proofs.
+	 * @param t
+	 * @throws IOException Creates the ZK provers using sigma protocols that prove Pedersen's proofs.
+	 * @throws InvalidDlogGroupException if the given dlog is not valid.
+	 * @throws CheatAttemptException if the receiver h is not in the DlogGroup.
+	 * @throws ClassNotFoundException if there was a problem in the serialization
+	 */
+	private void doConstruct(int t, SecureRandom random) throws IOException, ClassNotFoundException, CheatAttemptException, InvalidDlogGroupException {
+		SigmaVerifierComputation elGamalCommittedValVerifier = new SigmaElGamalCommittedValueVerifierComputation(dlog, t, random);
+		SigmaVerifierComputation elGamalCTKnowledgeVerifier = new SigmaElGamalCTKnowledgeVerifierComputation(dlog, t, random);
+		knowledgeVerifier = new  ZKPOKFromSigmaCommitPedersenVerifier(channel, elGamalCTKnowledgeVerifier, random);
+		committedValVerifier = new ZKFromSigmaVerifier(channel, elGamalCommittedValVerifier, random);
+		
+	}
+
+	@Override
+	public boolean verifyKnowledge(long id) throws IOException, CheatAttemptException, ClassNotFoundException {
+		SigmaElGamalCTKnowledgeCommonInput input =  new SigmaElGamalCTKnowledgeCommonInput
+				((ElGamalPublicKey)getPreProcessedValues()[0]);
+		return knowledgeVerifier.verify(input);
+	}
+
+	@Override
+	public CmtGroupElementCommitValue verifyCommittedValue(long id) throws IOException, CheatAttemptException, ClassNotFoundException, CommitValueException  {
+		//Receive the committed value from the committer.
+		Serializable x;
+		try {
+			x = channel.receive();
+		} catch (IOException e) {
+			throw new IOException("Failed to receive x. The thrown message is: " + e.getMessage());
+		}
+		if (!(x instanceof GroupElementSendableData)){
+			throw new IllegalArgumentException("The received x is not an instance of GroupElementSendableData");
+		}
+		GroupElement committedVal = dlog.reconstructElement(true, (GroupElementSendableData) x);
+		
+		//Creates input for the ZK verifier
+		CmtElGamalCommitmentMessage commitmentVal = getCommitmentPhaseValues(id);
+		SigmaElGamalCommittedValueCommonInput input =  new SigmaElGamalCommittedValueCommonInput
+				((ElGamalPublicKey)getPreProcessedValues()[0], commitmentVal.getCommitment(), committedVal);
+		//Computes the verification.
+		boolean verified = committedValVerifier.verify(input);
+		if (verified){
+			return new CmtGroupElementCommitValue(committedVal);
+		} else{
+			return null;
+		}
+	}
+}
