acee80364ef2d67f924919b0723ba423c47b4798
Moriya Farbstein <moriyaw@gmail.com>
2013-11-04 09:06:27 +0000

    1. Changed the name according to the new convention. 2. added generateBytesFromCommitValue function. 3. the implementation of the constructor that gets the channel changed to read default dlog from the defaults file. 4. Added comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2669 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/commitmentScheme/elGamal/CmtElGamalOnGroupElementReceiver.java
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/commitmentScheme/elGamal/CmtElGamalOnGroupElementReceiver.java
@@ -24,53 +24,97 @@ Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
  */
 package edu.biu.scapi.interactiveMidProtocols.commitmentScheme.elGamal;
 
+import java.io.IOException;
+
 import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.exceptions.FactoriesException;
 import edu.biu.scapi.exceptions.InvalidDlogGroupException;
 import edu.biu.scapi.exceptions.SecurityLevelException;
-import edu.biu.scapi.interactiveMidProtocols.SigmaProtocol.elGamalCommittedValue.SigmaElGamalCommittedValueInput;
-import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CTReceiver;
-import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CommitValue;
-import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.GroupElementCommitValue;
+import edu.biu.scapi.generals.ScapiDefaultConfiguration;
+import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CmtReceiver;
+import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CmtCommitValue;
+import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CmtGroupElementCommitValue;
+import edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CmtOnGroupElement;
 import edu.biu.scapi.midLayer.asymmetricCrypto.encryption.ScElGamalOnGroupElement;
-import edu.biu.scapi.midLayer.asymmetricCrypto.keys.ScElGamalPublicKey;
 import edu.biu.scapi.midLayer.ciphertext.ElGamalOnGroupElementCiphertext.ElGamalOnGrElSendableData;
 import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.GroupElement;
 import edu.biu.scapi.primitives.dlog.GroupElementSendableData;
 import edu.biu.scapi.securityLevel.PerfectlyBindingCT;
+import edu.biu.scapi.tools.Factories.DlogGroupFactory;
 
 /**
+ * This class implements the receiver side of the ElGamal commitment. 
+ * It uses El Gamal encryption for  group elements, that is, the encryption class used is 
+ * ScElGamalOnGroupElement. This default cannot be changed.
+ * 
  * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Yael Ejgenberg)
  *
  */
-public class ElGamalCTReceiver extends ElGamalCTRCore implements CTReceiver, PerfectlyBindingCT {
+public class CmtElGamalOnGroupElementReceiver extends CmtElGamalReceiverCore implements CmtReceiver, PerfectlyBindingCT, CmtOnGroupElement {
 
 	/**
-	 * @param channel
-	 * @throws IllegalArgumentException
-	 * @throws SecurityLevelException
-	 * @throws InvalidDlogGroupException
+	 * This constructor lets the caller pass the channel and the dlog group to work with.
+	 * The El Gamal option (ScElGamalOnGroupElement)is set by default by the constructor 
+	 * and cannot be changed.
+	 * @param channel used for the communication
+	 * @param dlog Dlog group
+	 * @throws CheatAttemptException if the receiver suspects the committer trying to cheat.
+	 * @throws ClassNotFoundException if there was a problem during serialization.
+	 * @throws SecurityLevelException if the given dlog is not DDH - secure
+	 * @throws InvalidDlogGroupException if the given dlog is not valid
+	 * @throws IOException if there was a problem during communication
 	 */
-	/*
-	public ElGamalCTReceiver(Channel channel) throws IllegalArgumentException, SecurityLevelException, InvalidDlogGroupException {
-		super(channel);
-	}
-	 */
-	public ElGamalCTReceiver(Channel channel, DlogGroup dlog) throws IllegalArgumentException, SecurityLevelException, InvalidDlogGroupException {
+	public CmtElGamalOnGroupElementReceiver(Channel channel, DlogGroup dlog) throws SecurityLevelException, InvalidDlogGroupException, ClassNotFoundException, IOException, CheatAttemptException {
 		super(channel, dlog, new ScElGamalOnGroupElement(dlog));	
 	}
 	
-	public ElGamalCTReceiver(Channel channel) {
-		super(channel);
+	/**
+	 * This constructor uses default Dlog group and El Gamal.
+	 * @param channel
+	 * @throws CheatAttemptException if the receiver suspects the committer trying to cheat.
+	 * @throws IOException if there was a problem during communication
+	 * @throws ClassNotFoundException if there was a problem during serialization.
+	 */
+	public CmtElGamalOnGroupElementReceiver(Channel channel) throws ClassNotFoundException, IOException, CheatAttemptException {
+		String dlogGroupName = ScapiDefaultConfiguration.getInstance().getProperty("DDHDlogGroup ");
+		DlogGroup dlog =  null;
+		//Create the dlog group 
+		try {
+			dlog = DlogGroupFactory.getInstance().getObject(dlogGroupName);
+		} catch (FactoriesException e1) {
+			e1.printStackTrace();
+		}
+		//Proceed with construction of ElGamalCTRCore instance
+		try {
+			doConstruct(channel, dlog , new ScElGamalOnGroupElement(dlog));
+		} catch (SecurityLevelException e) {
+			e.printStackTrace();
+		} catch (InvalidDlogGroupException e) {
+			e.printStackTrace();
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.biu.scapi.interactiveMidProtocols.commitmentScheme.ElGamalCTRCore#processDecommitment(int, edu.biu.scapi.interactiveMidProtocols.commitmentScheme.CTCElGamalDecommitmentMessage)
+	/**
+	 * Proccesses the decommitment phase.
+	 * "IF NOT
+	 *	•	u=g^r 
+	 *	•	v = h^r * x
+	 *	•	x in G
+	 *		OUTPUT REJ
+	 *	ELSE
+	 *	    OUTPUT ACC and value x"
+	 * @param id the id of the commitment.
+	 * @param msg the receiver message from the committer
+	 * @return the committed value if the decommit succeeded; null, otherwise.
 	 */
-	@Override
-	protected CommitValue processDecommitment(int id, CTCElGamalDecommitmentMessage msg) {
+	protected CmtCommitValue processDecommitment(long id, CmtElGamalDecommitmentMessage msg) {
 		GroupElement xEl = null;
-
+		
+		if (!(msg.getX() instanceof GroupElementSendableData))
+			throw new IllegalArgumentException("x value is not an instance of GroupElementSendableData");
+		
 		try{
 			xEl = dlog.reconstructElement(true, (GroupElementSendableData) msg.getX());
 		}catch (IllegalArgumentException e){
@@ -82,28 +126,29 @@ public class ElGamalCTReceiver extends ElGamalCTRCore implements CTReceiver, Per
 			return null;
 
 		//Fetch received commitment according to ID
-		CTCElGamalCommitmentMessage receivedCommitment = commitmentMap.get(Integer.valueOf(id));
+		CmtElGamalCommitmentMessage receivedCommitment = commitmentMap.get(Long.valueOf(id));
+		if (!(receivedCommitment.getCommitment() instanceof ElGamalOnGrElSendableData))
+			throw new IllegalArgumentException("commitment value is not an instance of ElGamalOnGrElSendableData");
 
-		GroupElement h = ((ScElGamalPublicKey)elGamal.reconstructPublicKey(receivedCommitment.getPublicKey())).getH();
-		if(!dlog.isMember(h))
-			return null;
-
-		GroupElement u = dlog.reconstructElement(true,((ElGamalOnGrElSendableData)receivedCommitment.getCipherData()).getCipher1());
-		GroupElement v = dlog.reconstructElement(true,((ElGamalOnGrElSendableData)receivedCommitment.getCipherData()).getCipher2());
-		GroupElement gToR = dlog.exponentiate(dlog.getGenerator(), msg.getR());	
-		GroupElement hToR = dlog.exponentiate(h, msg.getR());
-		System.out.println("u = " + u.toString());
-		System.out.println("v = " + v.toString());
-		System.out.println("gToR = " + gToR.toString());
-		System.out.println("hToR = " + hToR.toString());
-		System.out.println("hTor * x = " + dlog.multiplyGroupElements(hToR, xEl));
-
+		GroupElement u = dlog.reconstructElement(true,((ElGamalOnGrElSendableData)receivedCommitment.getCommitment()).getCipher1());
+		GroupElement v = dlog.reconstructElement(true,((ElGamalOnGrElSendableData)receivedCommitment.getCommitment()).getCipher2());
+		GroupElement gToR = dlog.exponentiate(dlog.getGenerator(), msg.getR().getR());	
+		GroupElement hToR = dlog.exponentiate(publicKey.getH(), msg.getR().getR());
+		
 		if( u.equals(gToR) && v.equals(dlog.multiplyGroupElements(hToR, xEl)) )
-			return new GroupElementCommitValue(xEl);
+			return new CmtGroupElementCommitValue(xEl);
 		return null;
 	}
 	
-	public SigmaElGamalCommittedValueInput getInputForZK(GroupElement x){
-		return new SigmaElGamalCommittedValueInput(msg, x);
+	/**
+	 * This function converts the given commit value to a byte array. 
+	 * @param value
+	 * @return the generated bytes.
+	 */
+	public byte[] generateBytesFromCommitValue(CmtCommitValue value){
+		if (!(value instanceof  CmtGroupElementCommitValue))
+			throw new IllegalArgumentException("The given value must be of type  GroupElementCommitValue");
+		return dlog.mapAnyGroupElementToByteArray((GroupElement) value.getX());
 	}
+	
 }
