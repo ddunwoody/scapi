880759519b1981e80fdeb4c8a682ee6f1922f194
Moriya Farbstein <moriyaw@gmail.com>
2013-11-10 08:35:02 +0000

    Added utility classes that execute the preprocess phase of the OT. The preprocess is common to FullSimulation, FullSimulationROM and UC OT protocols.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2806 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/fullSimulation/OTFullSimReceiverPreprocessUtil.java
@@ -0,0 +1,108 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot.fullSimulation;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import org.bouncycastle.util.BigIntegers;
+
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.exceptions.CommitValueException;
+import edu.biu.scapi.interactiveMidProtocols.sigmaProtocol.dh.SigmaDHProverInput;
+import edu.biu.scapi.interactiveMidProtocols.zeroKnowledge.ZKPOKProver;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+
+public class OTFullSimReceiverPreprocessUtil {
+
+	/**
+	 * Runs the part of the protocol where the receiver input is not yet necessary:
+	 * 	"SAMPLE random values y, alpha0 <- {0, . . . , q-1} 
+	 *	SET alpha1 = alpha0 + 1 
+	 *	COMPUTE 
+	 *    1. g1 = (g0)^y
+	 *	  2. h0 = (g0)^(alpha0)
+	 *	  3. h1 = (g1)^(alpha1)
+	 *	SEND (g1,h0,h1) to S
+	 *  Run the prover in ZKPOK_FROM_SIGMA with Sigma protocol SIGMA_DH. Use common input (g0,g1,h0,h1/g1) and private input alpha0."
+	 * @param channel
+	 * @return 
+	 * @throws ClassNotFoundException 
+	 * @throws CheatAttemptException 
+	 * @throws IOException 
+	 * @throws CommitValueException 
+	 */
+	public static OTFullSimPreprocessPhaseValues preProcess(DlogGroup dlog, ZKPOKProver zkProver, Channel channel, SecureRandom random) throws IOException, CheatAttemptException, ClassNotFoundException, CommitValueException{
+		BigInteger qMinusOne = dlog.getOrder().subtract(BigInteger.ONE);
+		
+		//Sample random values 
+		BigInteger y = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		BigInteger alpha0 = BigIntegers.createRandomInRange(BigInteger.ZERO, qMinusOne, random);
+		
+		//Set alpha1 = alpha0 + 1 
+		BigInteger alpha1 = alpha0.add(BigInteger.ONE);
+		
+		//Calculate tuple elements
+		GroupElement g0 = dlog.getGenerator();
+		GroupElement g1 = dlog.exponentiate(g0, y);
+		GroupElement h0 = dlog.exponentiate(g0, alpha0);
+		GroupElement h1 = dlog.exponentiate(g1, alpha1);
+		
+		OTFullSimDDHReceiverMsg tuple = new OTFullSimDDHReceiverMsg(g1.generateSendableData(), h0.generateSendableData(), h1.generateSendableData());
+				
+		//Send tuple to sender.
+		sendTupleToSender(channel, tuple);
+		
+		//Run the prover in ZKPOK_FROM_SIGMA with Sigma protocol SIGMA_DH.
+		GroupElement g1Inv = dlog.getInverse(g1);
+		GroupElement h1DivG1 = dlog.multiplyGroupElements(h1, g1Inv);
+		
+		zkProver.prove(new SigmaDHProverInput(g1, h0, h1DivG1, alpha0));
+		
+		return new OTFullSimPreprocessPhaseValues(g0, g1, h0, h1);
+	}
+	
+	
+	/**
+	 * Runs the following line from the protocol:
+	 * "SEND tuple to S"
+	 * @param channel
+	 * @param a the tuple to send to the sender.
+	 * @throws IOException 
+	 */
+	private static void sendTupleToSender(Channel channel, Serializable a) throws IOException {
+		try {
+			channel.send(a);
+		} catch (IOException e) {
+			throw new IOException("failed to send the message. The thrown message is: " + e.getMessage());
+		}
+		
+	}
+	
+}
---
+++ Code/JavaSrc/edu/biu/scapi/interactiveMidProtocols/ot/fullSimulation/OTFullSimSenderPreprocessUtil.java
@@ -0,0 +1,95 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.interactiveMidProtocols.ot.fullSimulation;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.exceptions.CheatAttemptException;
+import edu.biu.scapi.exceptions.CommitValueException;
+import edu.biu.scapi.interactiveMidProtocols.sigmaProtocol.dh.SigmaDHCommonInput;
+import edu.biu.scapi.interactiveMidProtocols.zeroKnowledge.ZKPOKVerifier;
+import edu.biu.scapi.primitives.dlog.DlogGroup;
+import edu.biu.scapi.primitives.dlog.GroupElement;
+
+public class OTFullSimSenderPreprocessUtil {
+
+	/**
+	 * Runs the part of the protocol where the sender input is not yet necessary:
+	 * WAIT for message from R
+	 * DENOTE the values received by (g1,h0,h1) 
+	 * Run the verifier in ZKPOK_FROM_SIGMA with Sigma protocol SIGMA_DH. Use common input (g0,g1,h0,h1/g1).
+	 * If output is REJ, REPORT ERROR (cheat attempt) and HALT.
+	 * @param channel
+	 * @return 
+	 * @throws IOException if failed to receive a message.
+	 * @throws ClassNotFoundException 
+	 * @throws CheatAttemptException 
+	 * @throws CommitValueException 
+	 */
+	public static OTFullSimPreprocessPhaseValues preProcess(Channel channel, DlogGroup dlog, ZKPOKVerifier zkVerifier) throws ClassNotFoundException, IOException, CheatAttemptException, CommitValueException{
+		
+		//Wait for message from R
+		OTFullSimDDHReceiverMsg message = waitForFullSimMessageFromReceiver(channel);
+		
+		GroupElement g1 = dlog.reconstructElement(true, message.getG1());
+		GroupElement h0 = dlog.reconstructElement(true, message.getH0());
+		GroupElement h1 = dlog.reconstructElement(true, message.getH1());
+		
+		//Run the verifier in ZKPOK_FROM_SIGMA with Sigma protocol SIGMA_DH.
+		GroupElement g1Inv = dlog.getInverse(g1);
+		GroupElement h1DivG1 = dlog.multiplyGroupElements(h1, g1Inv);
+		
+		//If the output of the Zero Knowledge Proof Of Knowledge is REJ, throw CheatAttempException.
+		if (!zkVerifier.verify(new SigmaDHCommonInput(g1, h0, h1DivG1))){
+			throw new CheatAttemptException("ZKPOK verifier outputed REJECT");
+		}
+		
+		return new OTFullSimPreprocessPhaseValues(dlog.getGenerator(), g1, h0, h1);
+	}
+	
+	/**
+	 * Runs the following line from the protocol:
+	 * "WAIT for message (h0,h1) from R"
+	 * @param channel
+	 * @return the received message.
+	 * @throws ClassNotFoundException 
+	 * @throws IOException if failed to receive a message.
+	 */
+	private static OTFullSimDDHReceiverMsg waitForFullSimMessageFromReceiver(Channel channel) throws ClassNotFoundException, IOException{
+		Serializable message = null;
+		try {
+			message = channel.receive();
+		} catch (IOException e) {
+			throw new IOException("Failed to receive message. The thrown message is: " + e.getMessage());
+		}
+		if (!(message instanceof OTFullSimDDHReceiverMsg)){
+			throw new IllegalArgumentException("The received message should be an instance of OTRFullSimMessage");
+		}
+		return (OTFullSimDDHReceiverMsg) message;
+	}
+	
+}
