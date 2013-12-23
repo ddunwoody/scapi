642bcf5ae61231980e8283edcde00e92f95a214a
Moriya Farbstein <moriyaw@gmail.com>
2013-12-23 08:02:31 +0000

    1. deleted the serialization of the encryption scheme. 2. added the private visibility to some fields that were package private. 3. added and updated comments and indentations. 4. Deleted the exception that were thrown from the default constructor. 5. Added to the constructor with the keySize argument also a CryptographicHash and SecureRandom arguments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2891 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/encryption/HashingMultiKeyEncryption.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/encryption/HashingMultiKeyEncryption.java
@@ -24,8 +24,6 @@
 */
 package edu.biu.scapi.circuits.encryption;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.security.SecureRandom;
 
 import javax.crypto.SecretKey;
@@ -40,11 +38,10 @@ import edu.biu.scapi.primitives.hash.CryptographicHash;
 import edu.biu.scapi.tools.Factories.CryptographicHashFactory;
 
 /**
- * We implement this double encryption scheme using Hashing. We use SHA-1. In
- * this encryption scheme the garbled values of each wire are appended to each
- * other and the result is hashed. We XOR the plaintext with the output of the
- * hash function. See <i>Garbling Schemes </i> by Mihir Bellare, Viet Tung
- * Hoang, and Phillip Rogaway for a full discussion on the many different types
+ * We implement this double encryption scheme using Hashing. <p>
+ * In this encryption scheme the garbled values of each wire are appended to each other and the result is hashed. 
+ * We XOR the plaintext with the output of the hash function. 
+ * See <i>Garbling Schemes </i> by Mihir Bellare, Viet Tung Hoang, and Phillip Rogaway for a full discussion on the many different types
  * of garbling schemes and how efficient they are.
  * 
  * @author Steven Goldfeder
@@ -53,89 +50,74 @@ import edu.biu.scapi.tools.Factories.CryptographicHashFactory;
 public class HashingMultiKeyEncryption implements MultiKeyEncryptionScheme {
 
 	/**
-	 * 
+	 * Key size in bits. This is the size of the individual keys. 
+	 * One or more individual keys of this size will be combined into a {@code MultiSecretKey}.
 	 */
-	private static final long serialVersionUID = 3026420970664946138L;
+	private int keySize; 
+
+	//The {@code MultiSecretKey} for this encryption scheme.
+	private MultiSecretKey key;
+	
+	//A cryptographically strong random number generator.
+	private SecureRandom random;
+	
+	// Boolean flag set to {@code true} if the key is set and {@code false} otherwise.
+	private boolean isKeySet;
+
+	private CryptographicHash hash;
 
 	/**
-	 * Key size in bits. This is the size of the individual keys. One or more
-	 * individual keys of this size will be combined into a {@code MultiSecretKey}
-	 */
-	int keySize; 
-
-	/**
-	 * The {@code MultiSecretKey} for this encryption scheme
-	 */
-	MultiSecretKey key;
-	/**
-	 * a cryptographically strong random number generator
-	 */
-	SecureRandom random;
-	/**
-	 * boolean flag set to {@code true} if the key is set and {@code false}
-	 * otherwise
-	 */
-	boolean isKeySet;
-	/**
-	 * 
-	 */
-	private transient CryptographicHash hash;
-
-	/**
-	 * @throws FactoriesException
+	 * Constructor that sets the given values.
 	 * @throws InvalidKeySizeException 
 	 */
-	public HashingMultiKeyEncryption(int keySize) throws FactoriesException, InvalidKeySizeException {
+	public HashingMultiKeyEncryption(int keySize, CryptographicHash hash, SecureRandom random) throws InvalidKeySizeException {
+		doConstruct(keySize, hash, random);
+	}
+
+	/**
+	 * Constructor that sets default values.
+	 */
+	public HashingMultiKeyEncryption()  {
+		
+		try {
+			doConstruct(80, CryptographicHashFactory.getInstance().getObject("SHA-1","CryptoPP"), new SecureRandom());
+		} catch (InvalidKeySizeException e) {
+			// shouldn't occur since the given key size is correct.
+		} catch (FactoriesException e) {
+			// Should not occur since the arguments to the factory are correct.
+		}
+	}
+
+	private void doConstruct(int keySize, CryptographicHash hash, SecureRandom random) throws InvalidKeySizeException {
 		if(keySize% 8 != 0){
 			throw new InvalidKeySizeException();
 		}
 		this.keySize = keySize;
-		hash = CryptographicHashFactory.getInstance().getObject("SHA-1","CryptoPP");
-		random = new SecureRandom();
-		isKeySet = false;
+		this.hash = hash;
+		this.random = random;
 	}
-
-	/**
-	 * @throws FactoriesException 
-	 * 
-	 */
-	public HashingMultiKeyEncryption() throws FactoriesException {
-		
-		hash = CryptographicHashFactory.getInstance().getObject("SHA-1","CryptoPP");
-	}
-
+	
 	@Override
-	public byte[] encrypt(byte[] plaintext)
-			throws KeyNotSetException, PlaintextTooLongException {
+	public byte[] encrypt(byte[] plaintext) throws KeyNotSetException, PlaintextTooLongException {
 		
 		
 		if (!isKeySet) {
 			throw new KeyNotSetException();
 		}
-		// we append all the keys together and hash the result
+		// We append all the keys together and hash the result.
 		for (SecretKey k : key.getKeys()) {
-			/*
-			 * divide by 8 since the key size is specified in bits and we are using a
-			 * byte array
-			 */
+			//Divide by 8 since the key size is specified in bits and we are using a byte array.
 			hash.update(k.getEncoded(), 0, keySize / 8);
 		}
-		byte[] output = new byte[20];
+		byte[] output = new byte[hash.getHashedMsgSize()];
 		hash.hashFinal(output, 0);
 		int offset = output.length - plaintext.length;
 		if (offset < 0) {
-			/*
-			 * the plaintext is longer than the output of the hashed and cannot be
-			 * XOR'd to it
-			 */
+			// The plaintext is longer than the output of the hashed and cannot be XOR'd to it.
 			throw new PlaintextTooLongException();
 		}
 		if (offset > 0) {
-			/*
-			 * we truncate the output to the size of the plaintext by using only the
-			 * last n bytes where n is the size of the plaintext
-			 */
-
+			//We truncate the output to the size of the plaintext by using only the last n bytes where n is the size of the plaintext.
 			byte[] temp = new byte[plaintext.length];
 			for (int i = 0; i < temp.length; i++) {
 				temp[i] = output[i + offset];
@@ -152,16 +134,14 @@ public class HashingMultiKeyEncryption implements MultiKeyEncryptionScheme {
 	public byte[] decrypt(byte[] ciphertext)
 			throws KeyNotSetException, CiphertextTooLongException {
 		
-		
-		
 		if (!isKeySet()) {
 			throw new KeyNotSetException();
 		}
-		// see comments to encrypt method to understand the encryption/decryption
+		// See comments to encrypt method to understand the encryption/decryption.
 		for (SecretKey k : key.getKeys()) {
 			hash.update(k.getEncoded(), 0, keySize / 8);
 		}
-		byte[] output = new byte[20];
+		byte[] output = new byte[hash.getHashedMsgSize()];
 		hash.hashFinal(output, 0);
 		int offset = output.length - ciphertext.length;
 		if (offset < 0) {
@@ -182,10 +162,7 @@ public class HashingMultiKeyEncryption implements MultiKeyEncryptionScheme {
 
 	@Override
 	public SecretKey generateKey() {
-		/*
-		 * divide by 8 since the key size is specified in bits and we are using a
-		 * byte array
-		 */
+		//Divide by 8 since the key size is specified in bits and we are using a byte array
 		byte[] key = new byte[keySize / 8];
 		random.nextBytes(key);
 		return new SecretKeySpec(key, "");
@@ -211,18 +188,10 @@ public class HashingMultiKeyEncryption implements MultiKeyEncryptionScheme {
 	public void setTweak(byte[] tweak) {
 		// this encryption scheme ignores the tweak
 	}
-	
-	
-	private void readObject(ObjectInputStream inputStream)
-            throws IOException, ClassNotFoundException, FactoriesException
-    {
-		inputStream.defaultReadObject();
-		hash = CryptographicHashFactory.getInstance().getObject("SHA-1", "CryptoPP");
-    }
 
 	@Override
 	/**
-	 * Returns the size of the ciphertext
+	 * Returns the size of the ciphertext.
 	 */
 	public int getCipherSize() {
 
