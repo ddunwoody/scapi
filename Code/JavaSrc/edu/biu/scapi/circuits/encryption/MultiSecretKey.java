cbb74f1719ac2f0a2679dd9db1a72723a566c1e5
Moriya Farbstein <moriyaw@gmail.com>
2013-12-23 08:05:52 +0000

    1. Added and updated comments and indentations. 2. Changed the name of the numberOfKeys function to getNumberOfKeys.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2893 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/encryption/MultiSecretKey.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/encryption/MultiSecretKey.java
@@ -31,55 +31,45 @@ import java.io.Serializable;
 import javax.crypto.SecretKey;
 
 /**
- * This class is the Key to be used when encrypting with any @link
- * MultiKeyEncryption} scheme. It generalizes the notion of a dual key cipher
- * that is generally associated with Yao's protocol. A dial key cipher uses two
- * keys and is thus only appropriate for encrypting 2-input {@code GarbledGate}
- * s. A MultiKey cipher can be used for any number of inputs. When it is used to
- * encrypt a 2-input {@code GarbledGate}, it is a dial key cipher.
- * {@link MultiKeyEncryptionScheme}. It contains an array of {@link SecretKey}s
- * that the {@link MultiKeyEncryptionScheme} will use to encrypt.
+ * This class is the Key to be used when encrypting with any @link MultiKeyEncryption} scheme. <P>
+ * It generalizes the notion of a dual key cipher that is generally associated with Yao's protocol. 
+ * A dial key cipher uses two keys and is thus only appropriate for encrypting 2-input {@code GarbledGate}s. 
+ * A MultiKey cipher can be used for any number of inputs. When it is used to encrypt a 2-input {@code GarbledGate}, it is a dial key 
+ * cipher {@link MultiKeyEncryptionScheme}. 
+ * It contains an array of {@link SecretKey}s that the {@link MultiKeyEncryptionScheme} will use to encrypt.
  * 
  * @author Steven Goldfeder
  * 
  */
 public class MultiSecretKey implements Serializable{
-  /**
-	 * 
-	 */
+  
 	private static final long serialVersionUID = 1543950325054479100L;
-private SecretKey[] keys;
+	private SecretKey[] keys;
 
-  /**
-   * A constructor that constructs a {@code MultiSecretKey} from any number of
-   * {@code SecretKey} objects. The {@code SecretKey}s can be passed to the
-   * constructor either in an array or as separate parameters.
-   * 
-   * @param keys
-   *          The {@code SecretKey}s that will be used to construct the
-   *          {@code MultiSecretKey}. The {@code SecretKey}s can be passed to
-   *          the constructor either in an array or as separate parameters.
-   */
-  public MultiSecretKey(SecretKey... keys) {
-    this.keys = keys;
-  }
+	/**
+	 * A constructor that constructs a {@code MultiSecretKey} from any number of {@code SecretKey} objects. 
+	 * The {@code SecretKey}s can be passed to the constructor either in an array or as separate parameters.
+	 * 
+	 * @param keys The {@code SecretKey}s that will be used to construct the {@code MultiSecretKey}. 
+	 * The {@code SecretKey}s can be passed to the constructor either in an array or as separate parameters.
+	 */
+	 public MultiSecretKey(SecretKey... keys) {
+		 this.keys = keys;
+	 }
 
-  /**
-   * An accessor method for getting an array of the {@code SecretKey}s from this
-   * {@code MultiSecretKey}.
-   * 
-   * @return an array containing the individual {@code SecretKey} objects that
-   *         make up this {@code MultiSecretKey}
-   */
-  public SecretKey[] getKeys() {
-    return keys;
-  }
+	 /**
+	  * Returns an array of the {@code SecretKey}s from this {@code MultiSecretKey}.
+	  * @return an array containing the individual {@code SecretKey} objects that make up this {@code MultiSecretKey}
+	  */
+	  public SecretKey[] getKeys() {
+		  return keys;
+	  }
 
-  /**
-   * @return the number of keys in this {@code MultiSecretKey}
-   */
-  public int numberOfKeys() {
-    return keys.length;
-  }
+	  /**
+	   * @return the number of keys in this {@code MultiSecretKey}.
+	   */
+	  public int getNumberOfKeys() {
+	    return keys.length;
+	  }
 
 }
