24177be96f3c63170021a3b813db75a9893d11a6
Moriya Farbstein <moriyaw@gmail.com>
2013-12-23 07:58:23 +0000

    1. deleted the serialization of the encryption scheme. 2. added the private visibility to some fields that were package private. 3. added and updated comments and indentations. 4. Added constructor that receive arguments from the user.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2889 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/encryption/AES128MultiKeyEncryption.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/encryption/AES128MultiKeyEncryption.java
@@ -25,38 +25,31 @@
 package edu.biu.scapi.circuits.encryption;
 
 import java.security.InvalidKeyException;
-import java.security.SecureRandom;
 
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 
-import edu.biu.scapi.circuits.garbledCircuit.MinimizeAESSetKeyGarbledBooleanCircuit;
 import edu.biu.scapi.exceptions.CiphertextTooLongException;
 import edu.biu.scapi.exceptions.KeyNotSetException;
 import edu.biu.scapi.exceptions.PlaintextTooLongException;
 import edu.biu.scapi.exceptions.TweakNotSetException;
+import edu.biu.scapi.primitives.prf.AES;
 import edu.biu.scapi.primitives.prf.PseudorandomFunction;
 import edu.biu.scapi.primitives.prf.cryptopp.CryptoPpAES;
 
 /**
- * This is a semi-classical double encryption scheme in which we use a PRF(
- * here, we use AES) twice on each value with different keys to double encrypt.
- * In this implementation though, we deviate slightly from the classical method
- * by using the PRF on a tweak rather than on the value itself. See <i>Garbling
- * Schemes </i> by Mihir Bellare, Viet Tung Hoang, and Phillip Rogaway Section
- * 5.6. This implementation follows their second suggestion in section .6(the
- * second paragraph starting with a dash '-' ).
- * <p>
- * Note that this encryption scheme does not minimize the number of AES set key
- * operations. See {@link MinimizeAESSetKeyGarbledBooleanCircuit} and
- * {@code MinimizeAESSetKeyGarbledGate} for a circuit and gate that do that as
- * well as a discussion of why a separate circuit was necessary to accomplish
- * this.
- * <p>
- * This encryption scheme encrypts by setting each key from the
- * {@code MultiSecretKey} as the AES key and then calling AES on the tweak. We
- * XOR these to each other and then XOR the result to the plaintext.
+ * This is a semi-classical double encryption scheme in which we use a PRF (here, we use AES) twice on each value with different keys
+ * to double encrypt. <p>
+ * In this implementation though, we deviate slightly from the classical method by using the PRF on a tweak rather than on the value 
+ * itself. See <i>Garbling Schemes </i> by Mihir Bellare, Viet Tung Hoang, and Phillip Rogaway Section 5.6. 
+ * This implementation follows their second suggestion in section .6 (the second paragraph starting with a dash '-' ). <p>
  * 
+ * Note that this encryption scheme does not minimize the number of AES set key operations. 
+ * See {@link MinimizeAESSetKeyGarbledBooleanCircuit} and {@code MinimizeAESSetKeyGarbledGate} for a circuit and gate that do that as
+ * well as a discussion of why a separate circuit was necessary to accomplish this. <p>
+ * 
+ * This encryption scheme encrypts by setting each key from the {@code MultiSecretKey} as the AES key and then calling AES on the tweak.
+ * We XOR these to each other and then XOR the result to the plaintext.
  * 
  * @author Steven Goldfeder
  * 
@@ -64,28 +57,26 @@ import edu.biu.scapi.primitives.prf.cryptopp.CryptoPpAES;
 public class AES128MultiKeyEncryption implements MultiKeyEncryptionScheme {
 
 	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7955510990507430705L;
-	/**
 	 * Key size in bits
 	 */
 	static final int KEY_SIZE = 128;
+	
 	/**
-	 * The key to encrypt on. In this scheme each of the individual
-	 * {@code SecretKey}s that make up the {@code MultiSecretKey} will be set as
-	 * the key to AES
+	 * The key to encrypt on. 
+	 * In this scheme each of the individual {@code SecretKey}s that make up the {@code MultiSecretKey} will be set as the key to AES.
 	 */
-	MultiSecretKey key;
-	SecureRandom random;
-	boolean isKeySet = false;
-	PseudorandomFunction aes;
-	byte[] tweak;
-	boolean isTweakSet = false;
+	private MultiSecretKey key;
+	private boolean isKeySet;
+	private PseudorandomFunction aes;
+	private byte[] tweak;
+	private boolean isTweakSet;
 
+	public AES128MultiKeyEncryption(AES aes) {
+		this.aes = aes;
+	}
+	
 	public AES128MultiKeyEncryption() {
-		aes = new CryptoPpAES();
-		random = new SecureRandom();
+		this(new CryptoPpAES());
 	}
 
 	@Override
@@ -105,37 +96,34 @@ public class AES128MultiKeyEncryption implements MultiKeyEncryptionScheme {
 	}
 
 	@Override
-	public byte[] encrypt(byte[] plaintext) throws KeyNotSetException, TweakNotSetException, InvalidKeyException, IllegalBlockSizeException, PlaintextTooLongException {
+	public byte[] encrypt(byte[] plaintext) throws KeyNotSetException, TweakNotSetException, InvalidKeyException, PlaintextTooLongException, IllegalBlockSizeException {
 		if (!isKeySet) {
 			throw new KeyNotSetException();
 		}
 		if (!isTweakSet) {
 			throw new TweakNotSetException();
 		}
-		/*
-		 * divide by 8 since the key size is specified in bits and we are using a
-		 * byte array
-		 */
+
+		//Divide by 8 since the key size is specified in bits and we are using a byte array
 		if (plaintext.length > KEY_SIZE / 8) {
 			throw new PlaintextTooLongException();
 		}
-		int numberOfKeys = key.numberOfKeys();
+		int numberOfKeys = key.getNumberOfKeys();
 		SecretKey[] keys = key.getKeys();
 		byte[] outBytes = new byte[KEY_SIZE / 8];
 		byte[] temp = new byte[KEY_SIZE / 8];
+		
 		/*
-		 * we set each key from the MultiSecretKey as the aes key and use it to
-		 * encrypt the tweak. We XOR these to each other and then XOR the result to
-		 * the plaintext.
+		 * We set each key from the MultiSecretKey as the aes key and use it to encrypt the tweak. 
+		 * We XOR these to each other and then XOR the result to the plaintext.
 		 */
 		for (int i = 0; i < numberOfKeys; i++) {
 			aes.setKey(keys[i]);
 			aes.computeBlock(tweak, 0, temp, 0);
 			for (int currentByte = 0; currentByte < outBytes.length; currentByte++) {
 				/*
-				 * the array is initially populated with 0's an thus on the first
-				 * iteration, the XOR has the effect of just setting it to the value it
-				 * is being XORd to since n XOR 0 = n
+				 * The array is initially populated with 0's an thus on the first iteration, 
+				 * the XOR has the effect of just setting it to the value it is being XORd to since n XOR 0 = n
 				 */
 				outBytes[currentByte] ^= temp[currentByte];
 			}
@@ -156,33 +144,30 @@ public class AES128MultiKeyEncryption implements MultiKeyEncryptionScheme {
 		if (!isTweakSet) {
 			throw new TweakNotSetException();
 		}
-		/*
-		 * divide by 8 since the key size is specified in bits and we are using a
-		 * byte array
-		 */
+		
+		//Divide by 8 since the key size is specified in bits and we are using a byte array
 		if (ciphertext.length > KEY_SIZE / 8) {
 			throw new CiphertextTooLongException();
 		}
-		int numberOfKeys = key.numberOfKeys();
+		int numberOfKeys = key.getNumberOfKeys();
 		SecretKey[] keys = key.getKeys();
 		aes.setKey(keys[0]);
 		byte[] outBytes = new byte[KEY_SIZE / 8];
 		/*
-		 * We encrypt with the 0th key first(outside of the loop) so that outBytes
-		 * is given an initial value. Then, for the remaining keys, we use aes in
-		 * the loop and XOR the result to outBytes
+		 * We encrypt with the 0th key first(outside of the loop) so that outBytes is given an initial value. 
+		 * Then, for the remaining keys, we use aes in the loop and XOR the result to outBytes
 		 */
 		byte[] temp = new byte[KEY_SIZE / 8];
 		aes.computeBlock(tweak, 0, outBytes, 0);
 		for (int i = 1; i < numberOfKeys; i++) {
 			aes.setKey(keys[i]);
 			aes.computeBlock(tweak, 0, temp, 0);
-			// XORing the result to outBytes
+			// XORing the result to outBytes.
 			for (int currentByte = 0; currentByte < outBytes.length; currentByte++) {
 				outBytes[currentByte] ^= temp[currentByte];
 			}
 		}
-		// we now XOR outbytes to the ciphertext to get the plaintext
+		// We now XOR outbytes to the ciphertext to get the plaintext.
 		for (int currentByte = 0; currentByte < outBytes.length; currentByte++) {
 			outBytes[currentByte] ^= ciphertext[currentByte];
 		}
@@ -200,7 +185,6 @@ public class AES128MultiKeyEncryption implements MultiKeyEncryptionScheme {
 		isTweakSet = true;
 	}
 
-	
 	/**
 	 * return the block size of aes
 	 */
