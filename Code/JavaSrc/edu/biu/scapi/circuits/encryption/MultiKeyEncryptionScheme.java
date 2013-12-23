f7b46e69861d8537fe0e5189a9408865d1aa9017
Moriya Farbstein <moriyaw@gmail.com>
2013-12-23 08:04:03 +0000

    1. deleted the serialization of the encryption scheme. 2. added the public visibility to some ,ethods that were package private. 3. added and updated comments and indentations.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2892 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/encryption/MultiKeyEncryptionScheme.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/encryption/MultiKeyEncryptionScheme.java
@@ -24,7 +24,6 @@
 */
 package edu.biu.scapi.circuits.encryption;
 
-import java.io.Serializable;
 import java.security.InvalidKeyException;
 
 import javax.crypto.IllegalBlockSizeException;
@@ -36,88 +35,77 @@ import edu.biu.scapi.exceptions.PlaintextTooLongException;
 import edu.biu.scapi.exceptions.TweakNotSetException;
 
 /**
- * A general interface for creating multiple key encryption schemes. When
- * constructing a garbled circuit, an encryption scheme must be specified. The
- * Garbled circuit will use the specified scheme to encrypt each gate.
- * <p>
- * See <i> Garbling Schemes</i> by Mihir Bellare, Viet Tung Hoang, and Phillip
- * Rogaway. In this paper, the authors present the idea of a Garbling
- * Scheme--i.e. the notion to treat Garbling Schemes as cryptographic primitive.
- * In the paper, they present a number of different efficient garbling schemes.
- * We have implemented a number of the ones that they mentioned, and any other
- * garbling schemes can easily be implemented and used anywhere in our code as
- * long as they implement this interface. 
+ * A general interface for creating multiple key encryption schemes. <p>
+ * When constructing a garbled circuit, an encryption scheme must be specified. 
+ * The Garbled circuit will use the specified scheme to encrypt each gate. <p>
+ * 
+ * See <i> Garbling Schemes</i> by Mihir Bellare, Viet Tung Hoang, and Phillip Rogaway. In this paper, the authors present the idea of a
+ * Garbling Scheme--i.e. the notion to treat Garbling Schemes as cryptographic primitive. In the paper, they present a number of 
+ * different efficient garbling schemes.
+ * 
+ * We have implemented a number of the ones that they mentioned, and any other garbling schemes can easily be implemented and used 
+ * anywhere in our code as long as they implement this interface. 
  * 
  * @author Steven Goldfeder
  * 
  */
-public interface MultiKeyEncryptionScheme  extends Serializable{
+public interface MultiKeyEncryptionScheme {
 
 	/**
-	 * This method generates a <b>single</b> {@codeSecretKey} NOT a
-	 * {@code MultiSecretKey}. This is necessary since the user will often need
-	 * to generate single keys first and then combine them to a single
-	 * {@code MultiSecretKey}.
-	 * <p>
-	 * Consider the following problem: Say we want to garble and compute a gate
-	 * in Yao's protocol. Consider a 2 input gate. Each input Wire will have two
-	 * possible garbled values--corresponding to a 0 value and a 1 value. These
-	 * values are <@code SecretKey}s <b>NOT</b? {@code MultiSecretKey}s. When we
-	 * encrypt the truth table, we combine the single keys to create
-	 * {@code MultiSecretKey}s. So, if we want to encrypt the 0-0 entry of the
-	 * truth table, we will take the 0 key from each {@code GarbledWire} and
-	 * combine them to a {@code MultiSecretKey} (using the
-	 * {@code generateMultiKey()} method. Then if we want to encrypt the 0-1
-	 * entry, we will use the 0-key from the first wire and the 1-key from the
-	 * second wire. We will combine these 2 keys into a single
-	 * {@code MultiSecretKey} and use this to encrypt. Note that in this example,
-	 * the 0-key from the first Wire is part of 2 different {@code MultiSecretKey}
-	 * s. First we combined it with the 0-key of the second wire and then we
-	 * combined it with the 1-key.
-	 * <p>
-	 * Thus, it is necessary to have a method to generate individual keys and a
-	 * separate method to combine different single keys into
-	 * {@code MultiSecretKey}s. are single keys, not MultiKeys
+	 * This method generates a <b>single</b> {@codeSecretKey} NOT a {@code MultiSecretKey}. <p>
+	 * This is necessary since the user will often need to generate single keys first and then combine them to a single 
+	 * {@code MultiSecretKey}.<p>
+	 * Consider the following problem: Say we want to garble and compute a gate in Yao's protocol. 
+	 * Consider a 2 input gate. Each input Wire will have two possible garbled values--corresponding to a 0 value and a 1 value. 
+	 * These values are <@code SecretKey}s <b>NOT</b? {@code MultiSecretKey}s. When we encrypt the truth table, we combine the single
+	 * keys to create {@code MultiSecretKey}s. So, if we want to encrypt the 0-0 entry of the truth table, we will take the 0 key from 
+	 * each {@code GarbledWire} and combine them to a {@code MultiSecretKey} (using the {@code generateMultiKey()} method. 
+	 * Then if we want to encrypt the 0-1 entry, we will use the 0-key from the first wire and the 1-key from the second wire. 
+	 * We will combine these 2 keys into a single {@code MultiSecretKey} and use this to encrypt. Note that in this example,
+	 * the 0-key from the first Wire is part of 2 different {@code MultiSecretKey}s. First we combined it with the 0-key of the second
+	 * wire and then we combined it with the 1-key. <p>
+	 * Thus, it is necessary to have a method to generate individual keys and a separate method to combine different single keys into
+	 * {@code MultiSecretKey}s. 
 	 * 
-	 * @return generates a {@link SecretKey} of the specified size. One or more of
-	 *         these keys will be combined into a {@code MultiSecretKey} to
-	 *         encrypt and decrypt on.
+	 * @return {@link SecretKey} of the specified size. One or more of these keys will be combined into a 
+	 * 					{@code MultiSecretKey} to encrypt and decrypt on.
 	 */
-	SecretKey generateKey();
+	public SecretKey generateKey();
 
 	/**
-	 * This method is provided with individual {@code SecretKey}s and combines
-	 * them into a {@code MultiSecretKey} that can be used for encryption and
-	 * decryption with the {@code MultiKeyEncryptionScheme}.
+	 * This method is provided with individual {@code SecretKey}s and combines them into a {@code MultiSecretKey} that can be used 
+	 * for encryption and decryption with the {@code MultiKeyEncryptionScheme}.
 	 * 
-	 * @param keys
-	 *          the individual {@link SecretKey}s that make up the
-	 *          {@code MultiSecretKey}. The {@code SecretKey} objects can be
-	 *          passed in an array or as individual parameters.
-	 * @return a {@code MultiSecretKey} made up of the {@code SecretKey}s that
-	 *         were passed as parameters
+	 * @param keys the individual {@link SecretKey}s that make up the {@code MultiSecretKey}. 
+	 * The {@code SecretKey} objects can be passed in an array or as individual parameters.
+	 * @return a {@code MultiSecretKey} made up of the {@code SecretKey}s that were passed as parameters.
 	 */
-	MultiSecretKey generateMultiKey(SecretKey... keys);
+	public MultiSecretKey generateMultiKey(SecretKey... keys);
 
 	/**
-	 * Sets the key to the specified {@code MultiKeyEncryptionScheme}. The key
-	 * that it is currently set to will be used for encryption and decryption
-	 * until {@code setKey()} is called again.
+	 * Sets the key to the specified {@code MultiKeyEncryptionScheme}.<p>
+	 * The key that it is currently set to will be used for encryption and decryption until {@code setKey()} is called again.
 	 * 
-	 * @param key
-	 *          the {@code MultiSecretKey} to be used for encryption and
-	 *          decryption.
+	 * @param key the {@code MultiSecretKey} to be used for encryption and decryption.
 	 */
-	void setKey(MultiSecretKey key);
+	public void setKey(MultiSecretKey key);
 
 	/**
-	 * This method used the individual {@code SecretKey}s that make up the
-	 * {@code MultiSecretKey} to encrypt the plaintext
+	 * See <i> Garbling Schemes</i> by Mihir Bellare, Viet Tung Hoang, and Phillip Rogaway. <p>
+	 * Some encryption schemes use a tweak and instead of encrypting directly on the entry of the plaintext encrypt on the tweak 
+	 * and then XOR the result with the plaintext. 
+	 * Some encryption schemes do not make use of a tweak, in which case calls to set the tweak have no effect.
+	 * If you are implementing an encryption scheme that does not use a tweak, just leave the body of this method blank.
 	 * 
-	 * @param plaintext
-	 *          The plaintext to be encrypted
-	 * @return the ciphertext--i.e. the plainetext encrypted with the currently
-	 *         set key.
+	 * @param tweak The tweak to be used for this encryption scheme.
+	 */
+	public void setTweak(byte[] tweak);
+	
+	/**
+	 * This method used the individual {@code SecretKey}s that make up the {@code MultiSecretKey} to encrypt the plaintext.
+	 * 
+	 * @param plaintext The plaintext to be encrypted.
+	 * @return the ciphertext--i.e. the plainetext encrypted with the currently set key.
 	 * @throws KeyNotSetException
 	 * @throws TweakNotSetException
 	 * @throws InvalidKeyException
@@ -125,15 +113,12 @@ public interface MultiKeyEncryptionScheme  extends Serializable{
 	 * @throws PlaintextTooLongException
 	 */
 
-	byte[] encrypt(byte[] plaintext)
-			throws KeyNotSetException, TweakNotSetException, InvalidKeyException,
-			IllegalBlockSizeException, PlaintextTooLongException;
+	public byte[] encrypt(byte[] plaintext) throws KeyNotSetException, TweakNotSetException, IllegalBlockSizeException, PlaintextTooLongException, InvalidKeyException;
 
 	/**
-	 * @param ciphertext
-	 *          The ciphertext to be decrypted
-	 * @return the plaintext--i.e. the ciphertext decrypted with the currently set
-	 *         key.
+	 * Decrypts the ciphertext.
+	 * @param ciphertext The ciphertext to be decrypted
+	 * @return the plaintext--i.e. the ciphertext decrypted with the currently set key.
 	 * @throws CiphertextTooLongException
 	 * @throws KeyNotSetException
 	 * @throws TweakNotSetException
@@ -141,35 +126,19 @@ public interface MultiKeyEncryptionScheme  extends Serializable{
 	 * @throws IllegalBlockSizeException
 	 */
 
-	byte[] decrypt(byte[] ciphertext)
-			throws CiphertextTooLongException, KeyNotSetException,
-			TweakNotSetException, InvalidKeyException, IllegalBlockSizeException;
+	public byte[] decrypt(byte[] ciphertext) throws CiphertextTooLongException, KeyNotSetException, TweakNotSetException, InvalidKeyException, IllegalBlockSizeException;
 
 	/**
-	 * This method checks if the key for this {@code MultiKeyEncryptionScheme} has
-	 * been set returning {@code true} if it has been and {@code false} if it has
-	 * not been. Before encrypting and decrypting, the key must be set.
+	 * This method checks if the key for this {@code MultiKeyEncryptionScheme} has been set.
+	 * Returning {@code true} if it has been and {@code false} if it has not been. 
+	 * Before encrypting and decrypting, the key must be set.
 	 * 
-	 * @return {@code true} if the key has been set, {@code false} otherwise
+	 * @return {@code true} if the key has been set, {@code false} otherwise.
 	 */
-	boolean isKeySet();
-
-	/**
-	 * See <i> Garbling Schemes</i> by Mihir Bellare, Viet Tung Hoang, and Phillip
-	 * Rogaway. Some encryption schemes use a tweak and instead of encrypting
-	 * directly on the entry of the plaintext encrypt on the tweak and then XOR
-	 * the result with the plaintext. Some encryption schemes do not make use of a
-	 * tweak, in which case calls to set the tweak have no effect.If you are
-	 * implementing an encryption scheme that does not use a tweak, just leave the
-	 * body of this method blank.
-	 * 
-	 * @param tweak
-	 *          The tweak to be used for this encryption scheme
-	 */
-	public void setTweak(byte[] tweak);
+	public boolean isKeySet();
 	
 	/**
-	 * Returns the size of the ciphertext
+	 * Returns the size of the ciphertext.
 	 */
 	public int getCipherSize();
 
