e14a7fd8029fd90c2aa30b8c80e6e9e5bb626bab
Moriya Farbstein <moriyaw@gmail.com>
2014-02-03 08:39:13 +0000

    We decided not to use the concept of sub circuit. Therefore, the subCircuit interface was deleted and all the circuit functionalities moved to this interface. The circuit functionalities that this interface declare are: 1. generate keys from scratch or from given keys. The given keys can be either the input keys or the output keys. 2. set input. The input can be set from a file or not. 3. compute the circuit. 4. verify the circuit. 5. translate the circuit.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2902 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/GarbledBooleanCircuit.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/GarbledBooleanCircuit.java
@@ -24,57 +24,159 @@
 */
 package edu.biu.scapi.circuits.garbledCircuit;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.crypto.SecretKey;
+
+import edu.biu.scapi.circuits.circuit.BooleanCircuit;
 import edu.biu.scapi.circuits.circuit.Wire;
+import edu.biu.scapi.exceptions.InvalidInputException;
+import edu.biu.scapi.exceptions.NoSuchPartyException;
+import edu.biu.scapi.exceptions.NotAllInputsSetException;
 
 /**
- * {@code GarbledBooleanCircuit} is a general interface implemented by all
- * garbled circuits--optimized or not. 
- * The difference between a sub circuit and a circuit is that a sub circuit does not have the 
- * ability to translate the output of compute into a result using some kind of translation from 
- * keys to 0/1 output since the translation table simply does not exist. The reason is that 
- * letting a party compute the result on several steps on the way to the final result may 
- * leak some information and thus not possible. A circuit, on the other way must have the 
- * functionality of translating the computed result into a 0/1 result which is the output
- * of the function we wish to compute. The circuit interface add the functionality of translate.
- * All garbled circuits have the added function {@link #translate(Map)} that  
- * translates the garbled output from {@link #compute()} into meaningful output.
+ * {@code GarbledBooleanCircuit} is a general interface implemented by all garbled circuits--optimized or not. <p>
+ * All garbled circuits have four main operations: <p>
+ * 1. The construct operation which is provided by the constructors of the implementing classes following by generateWireKeysAndSetTables function. <p>
+ * 2. The {@link #compute()} function computes a result on a garbled circuit that's input has been set. <p>
+ * 3. The {@link #verify(BooleanCircuit, Map)} method is used in the case of a malicious adversary to verify that the garbled circuit 
+ * created is an honest garbling of the agreed upon non garbled circuit. The constructing party constructs many garbled circuits and
+ * the second party chooses all but one of them to verify and test the honesty of the constructing party.<p>
+ * 4. The {@link #translate(Map)} that translates the garbled output from {@link #compute()} into meaningful output.<p>
  * 
  * @author Steven Goldfeder
  * 
  */
-public interface GarbledBooleanCircuit extends GarbledBooleanSubCircuit{
-  
+public interface GarbledBooleanCircuit {
+	
+	/**
+	 * This method generates both keys for each input wire. Then, creates the garbled table according to that values.<p>
+	 * @param ungarbledCircuit the circuit that this {@code GarbledBooleanCircuit} is supposed to be a garbling of. 
+	 * @return CircuitCreationValues contains both keys for each input and output wire and the translation table.
+	 */
+	public CircuitCreationValues generateWireKeysAndSetTables(BooleanCircuit ungarbledCircuit) ;
+	
+	/**
+	 * This method gets part of the keys and generates the missing keys for each wire. <p>
+	 * Then, creates the garbled table according to that values.<p>
+	 * This method can receive the garbled values of the input wires or the garbled values of the output wires.<p>
+	 * In addition, it gets the signal bits corresponding to the partial wire's keys. <p>
+	 * In case the given keys are the keys of the output wires than the given signal bits are actually the translation table.
+	 * @param ungarbledCircuit the circuit that this {@code GarbledBooleanCircuit} is supposed to be a garbling of.
+	 * @param partialWireValues can contain the keys of the input wires of the keys of the output wires.
+	 * @param signalBits the signal bits corresponding to the given wire's keys.
+	 * @return CircuitCreationValues contains both keys for each input and output wire and the translation table.
+	 */
+	public CircuitCreationValues generateWireKeysAndSetTables(BooleanCircuit ungarbledCircuit, Map<Integer, SecretKey[]> partialWireValues,
+			HashMap<Integer, Byte> signalBits);
+	
+	/**
+	 * This method sets the input of the circuit. <p>
+	 * It takes as a parameter a {@code Map} that maps the input Wire labels to a garbled wire containing the appropriate garbled values. <p>
+	 * See {@link #setGarbledInputFromUngarbledInput(File, Map)} for an alternate way of setting the input.
+	 * @param presetInputWires a {@code Map} containing the input wires that have been preset with their values
+	 * @param partyNumber the number of the party which we inputs belong to.
+	 * @throws NoSuchPartyException 
+  	 */
+	public void setInputs(Map<Integer, GarbledWire> presetInputWires, int partyNumber) throws NoSuchPartyException;
 
-  /**
-   * Translates the garbled output obtained from the {@link #compute()} function
-   * into meaningful(i.e. 0-1) output.
-   * 
-   * @param garbledOutput
-   *          a {@code Map) that containing the garbled output. This map maps
-   *          the output wire labels to {@code GarbledWire}s
-   * @return a {@code Map} that maps the output wire labels to ungarbled
-   *         {@code Wire}s that are set to either 0 or 1.
-   */
-  public Map<Integer, Wire> translate(Map<Integer, GarbledWire> garbledOutput);
+	/**
+	 * This method takes a map containing the {@code GarbledWire} label and <b> non garbled</b> value. <p>
+	 * This method than performs the lookup on the accompanying allInputWireValues {@code Map} and sets the inputs 
+	 * to the corresponding garbled outputs.
+	 * @param ungarbledInput a map containing the {@code GarbledWire} label and <b> non garbled</b> value for that label. 
+	 * @param allInputWireValues the map containing both garbled values for each input wire.
+	 * @param partyNumber the number of the party which we inputs belong to.
+	 * @throws NoSuchPartyException 
+	 */
+	public void setGarbledInputFromUngarbledInput(Map<Integer, Byte> ungarbledInput, Map<Integer, SecretKey[]> allInputWireValues, int partyNumber) throws NoSuchPartyException;
+ 
+	/**
+	 * This method computes the circuit if input has been set. <p>
+	 * It returns a {@code Map} containing the garbled output. This output can be translated via the {@link #translate(Map)} method.
+	 * @return returns a {@code Map} that maps the label of the output wire to the garbled value of the wire
+	 * @throws NotAllInputsSetException if not all the input has been set.
+	 *        
+	 */
+	public Map<Integer, GarbledWire> compute() throws NotAllInputsSetException;
 
+	/**
+     * The verify method is used in the case of malicious adversaries.<p>
+     * Alice constructs n circuits and Bob can verify n-1 of them(of his choosing) to confirm that they are indeed garbling of the 
+     * agreed upon non garbled circuit. In order to verify, Alice has to give Bob both keys for each of the input wires.
+     * @param ungarbledCircuit the circuit that this {@code GarbledBooleanCircuit} is supposed to be a garbling of. 
+     * This is the circuit that Alice and Bob agreed upon in Yao's protocol. We are verifying that this {@code GarbledBooleanCircuit} 
+     * is indeed a garbling of the agreed upon ungarbled circuit.
+     * @param allInputWireValues a {@Map} containing both keys for each input wire.
+     * For each input wire label, the map contains an array of two values. The value in the 0 position is the 0 encoding, and the
+     * value in the 1 position is the 1 encoding.
+     * @return {@code true} if this {@code GarbledBooleanCircuit} is a garbling of the ungarbledCircuit, {@code false} if it is not
+     * @throws InvalidInputException in case there is a problem with the given keys.
+     */
+	public boolean verify(BooleanCircuit ungarbledCircuit, Map<Integer, SecretKey[]> allInputWireValues) throws InvalidInputException;
+
+	/**
+	 * Translates the garbled output obtained from the {@link #compute()} function into meaningful(i.e. 0-1) output.<p>
+	 * @param garbledOutput a {@code Map) that containing the garbled output. This map maps the output wire labels to {@code GarbledWire}s
+	 * @return a {@code Map} that maps the output wire labels to ungarbled {@code Wire}s that are set to either 0 or 1.
+	 */
+	public Map<Integer, Wire> translate(Map<Integer, GarbledWire> garbledOutput);
+
+	/**
+	 * The garbled tables are stored in the circuit for all the gates. This method returns the garbled tables. <p>
+	 * This function is useful if we would like to pass many garbled circuits built on the same boolean circuit. <p>
+	 * This is a compact way to define a circuit, that is, two garbled circuit with the same multi encryption scheme and the same
+	 * basic boolean circuit only differ in the garbled tables and translation table. <p>
+	 * Thus we can hold one garbled circuit for all the circuits and only replace the garbled tables (and translation tables if 
+	 * necessary). The advantage is that the size of the tables only is much smaller that all the information stored in the circuit 
+	 * (gates and other member variables). The size becomes important when sending large circuits.
+	 * 
+	 */
+	public byte[][] getGarbledTables();
+	
+	
+	/**
+	 * Sets the garbled tables of this circuit.<p>
+	 * This function is useful if we would like to pass many garbled circuits built on the same boolean circuit. <p>
+	 * This is a compact way to define a circuit, that is, two garbled circuit with the same multi encryption scheme and the same
+	 * basic boolean circuit only differ in the garbled tables and translation table. <p>
+	 * Thus we can hold one garbled circuit for all the circuits and only replace the garbled tables (and translation tables if necessary).
+	 * The advantage is that the size of the tables only is much smaller that all the information stored in the circuit (gates and other 
+	 * member variables). The size becomes important when sending large circuits.<p>
+	 * The receiver of the circuits will set the garbled tables for the relevant circuit.
+	 */
+	public void setGarbledTables(byte[][] garbledTables);
+	
+	/**
+     * Returns the translation table of the circuit. <P>
+     * This is necessary since the constructor of the circuit may want to pass the translation table to other party. <p>
+     * Usually, this will be used when the other party (not the constructor of the circuit) creates a circuit, sets the garbled tables 
+     * and needs the translation table as well to complete the construction of the circuit.
+     * @return the translation table of the circuit.  
+     */
+	public HashMap<Integer, Byte> getTranslationTable();
   
-  /**
-   * Returns the translation table of the circuit. This is necessary since the conrtructor of the circuit may want to pass the
-   * translation table to other party. Usually, this will be used when the other party (not the constructor of the circuit) 
-   * sets the garbled tables and needs the translation table as well to complete the construction of the circuit
-   * 
-   * @return The translation table of the circuit.  
-   *           
-   */
-  public Map<Integer, Integer> getTranslationTable();
-  
-  
-/**
- * Sets the translation table of the circuit. This is necessary when the garbled tables where set and we would like to 
- * compute the circuit later on. 
- * @param translationTable
- */
-  public void setTranslationTable(Map<Integer, Integer> translationTable);
+	/**
+	 * Sets the translation table of the circuit. <p>
+	 * This is necessary when the garbled tables where set and we would like to compute the circuit later on. 
+	 * @param translationTable. This value should match the garbled tables of the circuit.
+	 */
+	public void setTranslationTable(HashMap<Integer, Byte> translationTable);
+	
+	/**
+	 * @param partyNumber the number of the party which we need his input labels.
+	 * @return an List containing the integer labels of the input wires of the given party number.
+	 * @throws NoSuchPartyException 
+	 */
+	public List<Integer> getInputWireLabels(int partyNumber) throws NoSuchPartyException;
+
+	/**
+	 * @param partyNumber the number of the party which we need his number of inputs.
+	 * @return the number of inputs of this circuit.
+	 * @throws NoSuchPartyException 
+	 */
+	public int getNumberOfInputs(int partyNumber) throws NoSuchPartyException ;  
 }
