141d738feacab34e25dde7f6366434d5eefcf2da
Moriya Farbstein <moriyaw@gmail.com>
2014-02-03 08:10:40 +0000

    Added row reduction gates that manage the functionalities of the row reduction techniques.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2900 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

---
+++ Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/MinimizeAESSetKeyRowReductionGate.java
@@ -0,0 +1,205 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.circuits.garbledCircuit;
+
+import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
+import java.util.Map;
+
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+
+import edu.biu.scapi.circuits.circuit.Gate;
+import edu.biu.scapi.circuits.encryption.MultiKeyEncryptionScheme;
+import edu.biu.scapi.exceptions.PlaintextTooLongException;
+import edu.biu.scapi.primitives.kdf.KeyDerivationFunction;
+import edu.biu.scapi.primitives.prf.PseudorandomFunction;
+
+/**
+ * {@link MinimizeAESSetKeyRowReductionGate} takes on the role of both a garbled gate and an {@code AES128MultiKeyEncryption} in order to encrypt the
+ * Gate using the row reduction technique while minimizing the number of AES setKey operations. <p>
+ * Note that currently only the constructor and not the verify method minimizes AES set key calls.
+ * 
+ * @author Cryptography and Computer Security Research Group Department of Computer Science Bar-Ilan University (Moriya Farbstein) 
+ * 
+ */
+public class MinimizeAESSetKeyRowReductionGate extends StandardRowReductionGarbledGate {
+
+	private PseudorandomFunction aes;
+	
+	/**
+	 * Constructs a MinimizeAESSetKeyRowReductionGate from an ungarbled gate using the given aes, {@code MultiKeyEncryptionScheme} and keys.
+	 * @param ungarbledGate the gate to garbled.
+	 * @param mes The encryption scheme used to garbled this gate.
+	 * @param aes The AES object to use to garbled this gate.
+	 * @param kdf to use in the row reduction technique
+	 * @param garbledTablesHolder a reference to the garbled tables of the circuit.
+	 * @param allWireValues both keys of all the circuit's wires.
+	 * @param signalBits of all the circuit's wires.
+   	 * @throws PlaintextTooLongException 
+   	 * @throws IllegalBlockSizeException 
+   	 * @throws InvalidKeyException 
+   	 */
+	MinimizeAESSetKeyRowReductionGate(Gate ungarbledGate, MultiKeyEncryptionScheme mes, PseudorandomFunction aes, KeyDerivationFunction kdf,
+			 GarbledTablesHolder garbledTablesHolder, Map<Integer, SecretKey[]> allWireValues, Map<Integer, Byte> signalBits) throws InvalidKeyException, IllegalBlockSizeException, PlaintextTooLongException {
+  		super(ungarbledGate, mes, kdf, garbledTablesHolder, allWireValues, signalBits);
+  		this.aes = aes;
+	}
+  
+	/**
+	 * Constructs a MinimizeAESSetKeyRowReductionGate from an ungarbled gate using the given aes and {@code MultiKeyEncryptionScheme}.
+	 * @param ungarbledGate the gate to garbled.
+	 * @param mes The encryption scheme used to garbled this gate.
+	 * @param aes The AES object to use to garbled this gate.
+	 * @param kdf to use in the row reduction technique
+	 * @param garbledTablesHolder a reference to the garbled tables of the circuit.
+   	 */
+	MinimizeAESSetKeyRowReductionGate(Gate ungarbledGate, MultiKeyEncryptionScheme mes, PseudorandomFunction aes, KeyDerivationFunction kdf, GarbledTablesHolder garbledTablesHolder){
+		super(ungarbledGate, mes, kdf, garbledTablesHolder);
+		this.aes = aes;
+	    
+	}	
+   
+	@Override
+	void createGarbledTable(Gate ungarbledGate, Map<Integer, SecretKey[]> allWireValues, Map<Integer, Byte> signalBits) throws InvalidKeyException, IllegalBlockSizeException {
+		
+		//The number of rows truth table is 2^(number of inputs).
+		int numberOfInputs = inputWireLabels.length;
+		int numberOfRows = (int) Math.pow(2, numberOfInputs)-1;
+		
+		//Allocate memory to the garbled table.
+		byte[] garbledTable = new byte[numberOfRows * mes.getCipherSize()];
+		garbledTablesHolder.getGarbledTables()[gateNumber] = garbledTable;
+    
+		/*
+	     * Rather than encrypt right away as we do in StandardGarbledGate, here we create arrays to hold the data. 
+	     * This way, we only encrypt once we have all of the data ready, and thus we can minimize the number of times we set the key for AES.
+	     */
+	    byte[][] tweaksToEncrypt = new byte[numberOfRows][];
+	    byte[][] outputValuesToEncrypt = new byte[numberOfRows][];
+	    int[][] valuesToEncryptOn = new int[numberOfRows][];
+
+	    // An array where we put the output values.
+	    byte[][] outputValues = new byte[numberOfRows][16];
+
+	    //Calculate the garbled table row by row.
+	    for (int rowOfTruthTable = 0; rowOfTruthTable <= numberOfRows; rowOfTruthTable++) {
+	    	int[] temp = new int[numberOfInputs];
+
+	    	// tweak - what is to be encrypted.
+	    	// value - which output wire to xor the encrypted tweak to, 0 or 1.
+	    	// permuted position - where to put the result in the output array.
+	    	ByteBuffer tweak = ByteBuffer.allocate(16);
+	    	tweak.putInt(gateNumber);
+	    	byte permutedPosition = 0;
+
+	    	//This for loop goes through from left to right the input of the given row of the truth table.
+	    	for (int i = 0, j = (int) Math.pow(2, numberOfInputs - 1), reverseIndex = numberOfInputs - 1; i < numberOfInputs; i++, j /= 2, reverseIndex--) {
+	    		
+	    		/* 
+		  		 * Truth table inputs are arranged according to binary number values. j is the value that begins as a 1 in the
+			   	 * leftmost(most significant bit) of the binary number that is the size of the truth table. Say for example that there are
+			   	 * 3 inputs. So the truth table has 3 input columns. j begins as the binary number 100 and we use it to check whether the leftmost bit in
+			   	 * the row of the truth table is set. If it is, that means that the input value is a 1.  Otherwise it's a 0. We then divide j by 2 to 
+			   	 * obtain the binary number 010 and we use this to determine the value of the inputs in the second column. We then divide by 2 again 
+			   	 * to obtain the binary number 001 and use it to determine the value of the inputs in the third column.
+			   	 */
+	    		
+	    		byte input = (byte) (((rowOfTruthTable & j) == 0) ? 0 : 1);
+	    		/*
+	    		 * The signal bits tell us the position on the garbled truth table for the given row of an ungarbled truth table. 
+	    		 * See Fairplay — A Secure Two-Party Computation System by Dahlia Malkhi, Noam Nisan1, Benny Pinkas, and Yaron Sella for more on signal bits.
+	    		 */
+	    		byte signalBit = signalBits.get(inputWireLabels[i]);
+		        
+	    		// Update the permuted position. For a better understanding on how this works, see the getIndexToDecrypt method in this class.
+		        permutedPosition += (input ^ signalBit) * (Math.pow(2, reverseIndex));
+		        temp[i] = input;
+
+		        /*
+		  		 * We add the signalBit that is placed on the end of the wire's value which is given by input XOR signalBit (i.e. the random bit for the
+		  		 * wire). Again, to clarify we use the term signal bit to mean both the random but assigned to each wire as well as the bit that is
+		  		 * associated with each of the wire's 2 values. The latter value is obtained by XORing the signal bit of the wire with the actual value
+		  		 * that the garbled value is encoding. So, for example if the signal bit for the wire is 0. Then the 0-encoded value will have 0 XOR 
+		  		 * 0 = 0 as its signal bit. The 1-encoded value will have 0 XOR 1 = 1 as its signal bit.
+		  		 */
+		        tweak.putInt(input ^ signalBit);
+	    	}
+	    	
+	    	//In case that is not the last row, save the calculated values to use later, all at once.
+	    	//In case of the last row, do not save the values. They will be calculated by the kdf in he compute function.
+	    	if (permutedPosition != 3){
+		    	valuesToEncryptOn[permutedPosition] = temp;
+		    	tweaksToEncrypt[permutedPosition] = tweak.array();
+		    	int value = (ungarbledGate.getTruthTable().get(rowOfTruthTable) == true) ? 1 : 0;
+		    	outputValuesToEncrypt[permutedPosition] = allWireValues.get(outputWireLabels[0])[value].getEncoded();
+	    	}
+	    	
+	    }
+	    
+	    /*
+	     * Now encrypt the tweaks on the necessary value. 
+	     * Set AES to each value and then look for all rows that need to be encrypted on this value before we reset the key.
+	     */
+	    for (int i = 0; i < numberOfInputs; i++) {
+	    	aes.setKey(allWireValues.get(inputWireLabels[i])[0]);
+	    	for (int rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+	    		if (valuesToEncryptOn[rowNumber][i] == 0) {
+	    			byte[] tempo = new byte[16];
+	    			aes.computeBlock(tweaksToEncrypt[rowNumber], 0, tempo, 0);
+	    			for (int byteNumber = 0; byteNumber < tempo.length; byteNumber++) {
+
+	    				outputValues[rowNumber][byteNumber] ^= tempo[byteNumber];
+	    			}
+	    		}
+
+	    	}
+
+	    	aes.setKey(allWireValues.get(inputWireLabels[i])[1]);
+	    	for (int rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+	    		if (valuesToEncryptOn[rowNumber][i] == 1) {
+	    			byte[] tempo = new byte[16];
+	    			aes.computeBlock(tweaksToEncrypt[rowNumber], 0, tempo, 0);
+	    			for (int byteNumber = 0; byteNumber < tempo.length; byteNumber++) {
+	    				outputValues[rowNumber][byteNumber] ^= tempo[byteNumber];
+	    			}
+	    		}
+	    	}
+	    }
+	    
+	    // Now that we encrypted the tweaks and XOR them to each other, we XOR the result to outputValue, the plaintext.
+	    for (int rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+	    	for (int byteNumber = 0; byteNumber < 16; byteNumber++)
+	    		outputValues[rowNumber][byteNumber] ^= outputValuesToEncrypt[rowNumber][byteNumber];
+	    }
+	    
+	    // Finally we assign the encrypted results to the corresponding row of the garbled truth table. 
+    	for (int rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+    	
+    		System.arraycopy(outputValues[rowNumber], 0, garbledTablesHolder.getGarbledTables()[gateNumber], rowNumber * mes.getCipherSize() , mes.getCipherSize());
+    	
+    	}
+	}
+}
---
+++ Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/StandardRowReductionGarbledGate.java
@@ -0,0 +1,302 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2012 - SCAPI (http://crypto.biu.ac.il/scapi)
+* This file is part of the SCAPI project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
+* http://crypto.biu.ac.il/SCAPI.
+* 
+* SCAPI uses Crypto++, Miracl, NTL and Bouncy Castle. Please see these projects for any further licensing issues.
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+package edu.biu.scapi.circuits.garbledCircuit;
+
+import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Map;
+
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import edu.biu.scapi.circuits.circuit.Gate;
+import edu.biu.scapi.circuits.encryption.MultiKeyEncryptionScheme;
+import edu.biu.scapi.exceptions.CiphertextTooLongException;
+import edu.biu.scapi.exceptions.KeyNotSetException;
+import edu.biu.scapi.exceptions.PlaintextTooLongException;
+import edu.biu.scapi.exceptions.TweakNotSetException;
+import edu.biu.scapi.primitives.kdf.KeyDerivationFunction;
+
+/**
+ * This is a standard Garbled Gate using the row reduction technique. <P>
+ * By standard we mean that it is not specialized for specific optimizations. Note though that even optimized circuits may make use 
+ * of {@code StandardRowReductionGarbledGate}. For example, FreeXORRowReductionGarbledBooleanCircuit, a circuit that is optimized with the
+ * Free XOR technique uses {@code StandardRowReductionGarbledGate}s for all of its non-XOR gates.
+ * 
+ * @author Steven Goldfeder
+ * 
+ */
+class StandardRowReductionGarbledGate extends StandardGarbledGate{
+
+	private KeyDerivationFunction kdf;
+	
+	/**
+	 * Constructs a garbled gate from an ungarbled gate using the given {@code MultiKeyEncryptionScheme} and keys.
+	 * @param ungarbledGate the gate to garbled.
+	 * @param mes The encryption scheme used to garbled this gate.
+	 * @param kdf to use in the row reduction technique
+	 * @param garbledTablesHolder a reference to the garbled tables of the circuit.
+	 * @param allWireValues both keys of all the circuit's wires.
+	 * @param signalBits of all the circuit's wires.
+   	 * @throws PlaintextTooLongException
+   	 * @throws IllegalBlockSizeException 
+   	 * @throws InvalidKeyException 
+   	 */
+	StandardRowReductionGarbledGate(Gate ungarbledGate, MultiKeyEncryptionScheme mes, KeyDerivationFunction kdf, GarbledTablesHolder garbledTablesHolder, Map<Integer, SecretKey[]> allWireValues, Map<Integer, Byte> signalBits) throws InvalidKeyException, IllegalBlockSizeException, PlaintextTooLongException{
+		super(ungarbledGate, mes, garbledTablesHolder, allWireValues, signalBits);
+		this.kdf = kdf;
+	}
+  
+	/**
+	 * Constructs a garbled gate from an ungarbled gate using the given {@code MultiKeyEncryptionScheme}.
+	 * @param ungarbledGate the gate to garbled
+	 * @param mes The encryption scheme used to garbled this gate
+	 * @param kdf to use in the row reduction technique
+	 * @param garbledTablesHolder a reference to the garbled tables of the circuit.
+   	 */
+	StandardRowReductionGarbledGate(Gate ungarbledGate, MultiKeyEncryptionScheme mes, KeyDerivationFunction kdf, GarbledTablesHolder garbledTablesHolder){
+		super(ungarbledGate, mes, garbledTablesHolder);
+		this.kdf = kdf;;
+	}
+  
+	/**
+	 * Creates the garbled table of this gate using the row reduction technique. <p>
+	 * Meaning, the last row is not saved and will be calculated when the compute function will be called by the kdf.
+	 */
+	@Override
+	void createGarbledTable(Gate ungarbledGate, Map<Integer, SecretKey[]> allWireValues, Map<Integer, Byte> signalBits) throws  IllegalBlockSizeException, PlaintextTooLongException, InvalidKeyException{
+		//The number of rows is 2^numberOfInputs - 1. The last row will be calculated by the row reduction technique.
+		int numberOfInputs = inputWireLabels.length;
+		int numberOfRows = (int) Math.pow(2, numberOfInputs)-1;
+		
+		//Allocate memory to the garbled table.
+		byte[] garbledTable = new byte[numberOfRows * mes.getCipherSize()];
+		garbledTablesHolder.getGarbledTables()[gateNumber] = garbledTable;
+		
+		//Calculate the garbled table row by row.
+		for (int rowOfTruthTable = 0; rowOfTruthTable <= numberOfRows; rowOfTruthTable++) {
+		  	ByteBuffer tweak = ByteBuffer.allocate(16);
+		  	tweak.putInt(gateNumber);
+		  	
+		  	// Permuted position will be the index of the garbled truth table corresponding to rowOfTruthTable.
+		  	int permutedPosition = 0;
+		  	SecretKey[] keysToEncryptOn = new SecretKey[numberOfInputs];
+		  	
+		  	//This for loop goes through from left to right the input of the given row of the truth table.
+		  	for (int i = 0, j = (int) Math.pow(2, numberOfInputs - 1), reverseIndex = numberOfInputs - 1; i < numberOfInputs; i++, j /= 2, reverseIndex--) {
+		  	
+		  		/* Truth table inputs are arranged according to binary number values. j is the value that begins as a 1 in the
+			   	 * leftmost(most significant bit) of the binary number that is the size of the truth table. Say for example that there are
+			   	 * 3 inputs. So the truth table has 3 input columns. j begins as the binary number 100 and we use it to check whether the leftmost bit in
+			   	 * the row of the truth table is set. If it is, that means that the input value is a 1.  Otherwise it's a 0. We then divide j by 2 to obtain the binary
+			   	 * number 010 and we use this to determine the value of the inputs in the second column. We then divide by 2 again to obtain the 
+			   	 * binary number 001 and use it to determine the value of the inputs in the third column
+			   	 */
+        
+		  		int input = (((rowOfTruthTable & j) == 0) ? 0 : 1);
+		  		byte signalBit = signalBits.get(inputWireLabels[i]);
+		  		
+		  		// Update the permuted position. For a better understanding on how this works, see the getIndexToDecrypt method in this class.
+		  		permutedPosition += (input ^ signalBit) * (Math.pow(2, reverseIndex));
+		  		
+		  		// Add the current Wire value to the list of keys to encrypt on. These keys will then be used to construct a multikey.
+		  		keysToEncryptOn[i] = allWireValues.get(inputWireLabels[i])[input];
+		  		
+		  		/*
+		  		 * We add the signalBit that is placed on the end of the wire's value which is given by input XOR signalBit (i.e. the random bit for the
+		  		 * wire). Again, to clarify we use the term signal bit to mean both the random but assigned to each wire as well as the bit that is
+		  		 * associated with each of the wire's 2 values. The latter value is obtained by XORing the signal bit of the wire with the actual value
+		  		 * that the garbled value is encoding. So, for example if the signal bit for the wire is 0. Then the 0-encoded value will have 0 XOR 
+		  		 * 0 = 0 as its signal bit. The 1-encoded value will have 0 XOR 1 = 1 as its signal bit.
+		  		 */
+		  		tweak.putInt(input ^ signalBit);
+		  	}
+		  	
+		  	//In row reduction technique we compute all rows except the last one. The last row will be calculated by the KDF.
+		  	if (permutedPosition != numberOfRows){
+		  		//Set the keys and the tweak of the encryption scheme.
+		  		mes.setKey(mes.generateMultiKey(keysToEncryptOn));
+			  	mes.setTweak(tweak.array());
+			  	
+			  	// Get the output value that should be garbled.
+			  	int value = (ungarbledGate.getTruthTable().get(rowOfTruthTable) == true) ? 1: 0;
+	      
+			  	// Encrypt the output key and put the ciphertext in the garbled table.
+			  	try {
+					System.arraycopy(mes.encrypt(allWireValues.get(outputWireLabels[0])[value].getEncoded()) , 0, garbledTable, permutedPosition*mes.getCipherSize(), mes.getCipherSize());
+				} catch (KeyNotSetException e) {
+					// Should not occur since the encryption has a key.
+				} catch (TweakNotSetException e) {
+					// Should not occur since the encryption has a tweak.			
+				}
+		  	}
+		}
+	}
+  
+	@Override
+	public void compute(Map<Integer, GarbledWire> computedWires) throws InvalidKeyException, IllegalBlockSizeException, CiphertextTooLongException {
+		//Calculate the row in the garbled table we need to decrypt.
+		int garbledTableIndex = getIndexToDecrypt(computedWires);
+		SecretKey wireValue = null;
+		int numberOfInputs = inputWireLabels.length;
+		
+		//In case of the last row, calculate the output key by the KDF.
+		if (garbledTableIndex == 3){
+			
+			ByteBuffer kdfBytes = ByteBuffer.allocate(mes.getCipherSize()*numberOfInputs +16);
+			for (int i = 0; i < numberOfInputs; i++) {
+				kdfBytes.put(computedWires.get(inputWireLabels[i]).getValueAndSignalBit().getEncoded());
+			}
+			kdfBytes.putInt(gateNumber);
+			for (int i = 0; i < numberOfInputs; i++) {
+				kdfBytes.putInt(computedWires.get(inputWireLabels[i]).getSignalBit());
+			}
+			wireValue = kdf.deriveKey(kdfBytes.array(), 0, mes.getCipherSize()*numberOfInputs +16, mes.getCipherSize());
+			
+		}else {
+		
+			// Regenerate the multiSecretKey and the tweak. 
+			// Then, reset the tweak and the key to the MultiKeyEncryptionScheme and call its decrypt function.
+			wireValue = computeGarbledTable(computedWires, garbledTableIndex);
+		}
+		int numberOfOutputs = outputWireLabels.length;
+		for (int i = 0; i < numberOfOutputs; i++) {
+		
+			computedWires.put(outputWireLabels[i], new GarbledWire(wireValue));
+		}
+	}
+
+	@Override
+	protected boolean verifyGarbledTable(Gate g, Map<Integer, SecretKey[]> allWireValues)
+			throws CiphertextTooLongException, InvalidKeyException,	IllegalBlockSizeException {
+		int numberOfInputs = inputWireLabels.length;
+		
+		BitSet ungarbledTruthTable = g.getTruthTable();
+		
+		SecretKey outputZeroValue = null;
+		SecretKey outputOneValue = null;
+		
+		// The outer for loop goes through each row of the truth table.
+		for (int rowOfTruthTable = 0; rowOfTruthTable < Math.pow(2, numberOfInputs); rowOfTruthTable++) {
+		
+			// Permuted position will be the index of the garbled truth table corresponding to rowOfTruthTable.
+			int permutedPosition = 0;
+			ByteBuffer tweak = ByteBuffer.allocate(16);
+			tweak.putInt(gateNumber);
+			SecretKey[] keysToDecryptOn = new SecretKey[numberOfInputs];
+			
+			// This for loop goes through from left to right the input of the given row of the truth table.
+			for (int i = 0, j = (int) Math.pow(2, numberOfInputs - 1), reverseIndex = numberOfInputs - 1; i < numberOfInputs; i++, j /= 2, reverseIndex--) {
+				int input = ((rowOfTruthTable & j) == 0) ? 0 : 1;
+				SecretKey currentWireValue = allWireValues.get(inputWireLabels[i])[input];
+		    
+			    // Add the current Wire value to the list of keys to decrypt on. These keys will then be used to construct a multikey.
+				keysToDecryptOn[i] = currentWireValue;
+			    
+				// Look up the signal bit on this wire. This is the last bit of its value.
+			    int signalBit = (currentWireValue.getEncoded()[currentWireValue.getEncoded().length - 1] & 1) == 0 ? 0 : 1;
+			    
+			    // Update the permuted position. For a better understanding on how this works, see the getIndexToDecrypt method in this class.
+			    permutedPosition += signalBit * Math.pow(2, reverseIndex);
+			    
+			    // Add the signal bit of this input wire value to the tweak.
+			    tweak.putInt(signalBit);
+			}
+			byte[] pt = null;
+			//In case other than the last row, verify the row by the encryption scheme.
+			if (permutedPosition != 3){
+				 mes.setKey(mes.generateMultiKey(keysToDecryptOn));
+				 mes.setTweak(tweak.array());
+			  
+				 try {
+					 pt = mes.decrypt(Arrays.copyOfRange(garbledTablesHolder.getGarbledTables()[gateNumber], permutedPosition * mes.getCipherSize(), (permutedPosition + 1) *mes.getCipherSize()));
+				 } catch (KeyNotSetException e) {
+					 // Should not occur since the key has been set.
+				 } catch (TweakNotSetException e) {
+					 // Should not occur since the tweak has been set.
+				 }
+				 
+			//In case of the last row, verify the row by the KDF.
+			} else{
+				
+				// Get the input values.
+				int wire0Key = ((rowOfTruthTable & 2) == 0) ? 0 : 1;
+		  		int wire1Key = ((rowOfTruthTable & 1) == 0) ? 0 : 1;
+		  		
+		  		//Get the input keys.
+		  		SecretKey input0 = allWireValues.get(inputWireLabels[0])[wire0Key];
+		  		SecretKey input1 = allWireValues.get(inputWireLabels[1])[wire1Key];
+		  		
+		  		//Calculate the kdf result.
+		  		ByteBuffer kdfBytes = ByteBuffer.allocate(mes.getCipherSize()*numberOfInputs +16);
+				kdfBytes.put(input0.getEncoded());
+				kdfBytes.put(input1.getEncoded());
+				kdfBytes.putInt(gateNumber);
+				kdfBytes.putInt((input0.getEncoded()[input0.getEncoded().length - 1] & 1) == 0 ? 0 : 1);
+				kdfBytes.putInt((input1.getEncoded()[input1.getEncoded().length - 1] & 1) == 0 ? 0 : 1);
+				pt = kdf.deriveKey(kdfBytes.array(), 0, mes.getCipherSize()*numberOfInputs +16, mes.getCipherSize()).getEncoded();
+					
+			}
+			
+			// Check to see that rows of the truth table with the same ungarbled value have the same garbled value as well.
+			if (ungarbledTruthTable.get(rowOfTruthTable) == true) {// i.e this bit is set
+				
+				// This is the first time we face k1, create it.
+				if (outputOneValue == null) {
+					outputOneValue = new SecretKeySpec(pt, "");
+				// K1 has already been created, check that it is equal to the current value.
+				} else{
+					byte[] oneValueBytes = outputOneValue.getEncoded();
+					for (int byteArrayIndex = 0; byteArrayIndex < pt.length; byteArrayIndex++) {
+						if (pt[byteArrayIndex] != oneValueBytes[byteArrayIndex]) {
+							return false;
+						}
+					}
+			 	} 
+			} else { //Bit is not set.
+				// This is the first time we face k0, create it.
+				if (outputZeroValue == null) {
+					outputZeroValue = new SecretKeySpec(pt, "");
+				// K0 has already been created, check that it is equal to the current value.
+				} else {
+					byte[] zeroValueBytes = outputZeroValue.getEncoded();
+					for (int byteArrayIndex = 0; byteArrayIndex < pt.length; byteArrayIndex++) {
+						if (pt[byteArrayIndex] != zeroValueBytes[byteArrayIndex]) {
+							return false;
+						}
+					}	
+				}
+			}
+		}
+		//Add the output wire to the allWireValues Map.
+		for (int w : outputWireLabels) {
+			allWireValues.put(w, new SecretKey[] {outputZeroValue, outputOneValue });
+		}
+		return true;
+	}
+}
