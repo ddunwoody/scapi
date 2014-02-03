173aa263ba1be0ac60405756750b201873a6cec4
Moriya Farbstein <moriyaw@gmail.com>
2014-02-03 08:49:42 +0000

    1. Deleted the serialization of the class. It no longer sent in the communication between parties. 2. Deleted the label in the class. The label of the wire is kept in the circuit and should not be kept in the wire also. 3. changed signalBit to be byte instead of int. 4. fixed comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2907 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/GarbledWire.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/GarbledWire.java
@@ -24,99 +24,61 @@
 */
 package edu.biu.scapi.circuits.garbledCircuit;
 
-import java.io.Serializable;
-
 import javax.crypto.SecretKey;
 
 import edu.biu.scapi.circuits.circuit.BooleanCircuit;
 import edu.biu.scapi.circuits.circuit.Wire;
 
 /**
- * This class is a software representation a garbled Wire. Like, a standard
- * {@link Wire} in a non-garbled {@link BooleanCircuit}, a {@code GarbledWire}
- * can carry one of two values. However, these values, as its name suggests are
- * garbled meaning that the actual boolean value of the wire is encoded and
+ * This class is a software representation a garbled Wire.<P>
+ * Like, a standard {@link Wire} in a non-garbled {@link BooleanCircuit}, a {@code GarbledWire} can carry one of two values. 
+ * However, these values, as its name suggests, are garbled. Meaning that the actual boolean value of the wire is encoded and
  * cannot be determined from the garbled value.
  * 
  * @author Steven Goldfeder
  * 
  */
-public class GarbledWire implements Serializable {
+public class GarbledWire  {
 
+	/**
+	 * The garbled value of this {@code GarbledWire}. The least significant bit is the signal bit.
+	 */
+	private SecretKey valueAndSignalBit;
+  
+	/**
+	 * Constructs a {@code GarbledWire} and assigns it a label, value and signalBit. <P>
+	 * The least significant bit of the underlying {@code byte[]} of the {@link SecretKey} is the signal bit.
+	 * @param valueAndSignalBit a secretKey containing the {@code GarbledWire}'s garbled value.
+	 */
+	public GarbledWire(SecretKey valueAndSignalBit) {
+		this.valueAndSignalBit = valueAndSignalBit;
+	}
 
-	private static final long serialVersionUID = 2068757047549518851L;
-/**
-   * The garbled value of this {@code GarbledWire}. The least significant bit is
-   * the signal bit.
-   */
-  private SecretKey valueAndSignalBit;
-  /**
-   * The integer label of this {@code GarbledWire}
-   */
-  int label;
+	/**
+	 * @return a SecretKey containing the garbled value of this {@code GarbledWire}, and the least significant bit of the
+	 * underlying {@code byte[]} is the signal bit.
+	 */
+	public SecretKey getValueAndSignalBit() {
+		return valueAndSignalBit;
+	}
 
-  /**
-   * Constructs a {@code GarbledWire} and assigns it a label, value and
-   * signalBit.
-   * 
-   * @param label
-   *          the integer label of this {@code GarbledWire}
-   * @param valueAndSignalBit
-   *          a secretKey containing the {@code GarbledWire}'s garbled value.
-   *          The least significant bit(of the underlying {@code byte[]} of the
-   *          {@link SecretKey} is the signal bit.
-   */
-  public GarbledWire(int label, SecretKey valueAndSignalBit) {
-    this.label = label;
-    this.valueAndSignalBit = valueAndSignalBit;
-  }
-
-  /**
-   * Constructor that constructs a {@code GarbledWire} but does not assign it a
-   * label or a value/signal bit.
-   */
-  GarbledWire() {
-
-  }
-
-  /**
-   * @return a SecretKey containing the garbled value of this
-   *         {@code GarbledWire}, and the least significant bit of the
-   *         underlying {@code byte[]} is the signal bit.
-   */
-  public SecretKey getValueAndSignalBit() {
-    return valueAndSignalBit;
-  }
-
-  /**
-   * @return the integer label of this {@code GarbledWire}
-   */
-  int getLabel() {
-    return label;
-  }
-
-  /**
-   * @return the signal bit. Clarification: The signal bit works as follows:
-   *         When we are assigning possible values to a wire we have randomly
-   *         generate a signal bit. If the signal bit is o, then the 0-encoding
-   *         has a 0 as its least significant bit and the 1-encoding has a 1. If
-   *         the signal bit is a 1, then the 0-encoding has a 1 as its least
-   *         significant bit and the 1 encoding has a 0. What we are returning
-   *         here is the last bit on the Wire which we also call the signal
-   *         bit.We are not returning the initial signal bit that determined
-   *         which bit to put on each wire as this information cannot be
-   *         recovered(if it could be we would be able to determine the actual
-   *         value of a garbled wire and thus it would not be garbled.)
-   *         <p>
-   *         See <i>Fairplay — A Secure Two-Party Computation System</i> by
-   *         Dahlia Malkhi, Noam Nisan, Benny Pinkas, and Yaron Sella. Section
-   *         4.2 describes in more detail how the signal bit works.
-   *         </p>
-   */
-  public int getSignalBit() {
-    int signalBit = (valueAndSignalBit.getEncoded()[valueAndSignalBit
-        .getEncoded().length - 1] & 1) == 0 ? 0 : 1;
-    return signalBit;
-  }
+	/**
+	 * @return the signal bit. <p>
+	 * Clarification: The signal bit works as follows:<p>
+	 * When we are assigning possible values to a wire we have randomly generate a signal bit. <p>
+	 * If the signal bit is 0, then the 0-encoding has a 0 as its least significant bit and the 1-encoding has a 1. <p>
+	 * If the signal bit is a 1, then the 0-encoding has a 1 as its least significant bit and the 1 encoding has a 0. <p>
+	 * 
+	 * What we are returning here is the last bit on the Wire which we also call the signal bit.<p>
+	 * We are not returning the initial signal bit that determined which bit to put on each wire as this information cannot be
+	 * recovered (if it could be we would be able to determine the actual value of a garbled wire and thus it would not be garbled.)<p>
+  	 * 
+  	 * See <i>Fairplay — A Secure Two-Party Computation System</i> by Dahlia Malkhi, Noam Nisan, Benny Pinkas, and Yaron Sella. 
+  	 * Section 4.2 describes in more detail how the signal bit works. </p>
+  	 */
+	public byte getSignalBit() {
+		byte signalBit = (byte) ((valueAndSignalBit.getEncoded()[valueAndSignalBit.getEncoded().length - 1] & 1) == 0 ? 0 : 1);
+		return signalBit;
+	}
 
 }
