6c2d45a2edcba4b557b5be68bca175dea858eb68
Moriya Farbstein <moriyaw@gmail.com>
2014-02-03 07:58:12 +0000

    1. Deleted AbstractGarbledGate. Therefore, The concrete gates classes now have the components that were in the abstract class. 2. Deleted the serialization of the class. It no longer sent in the communication between parties. 3. Fixed Comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@2898 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/FreeXORGate.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/FreeXORGate.java
@@ -33,167 +33,185 @@ import javax.crypto.spec.SecretKeySpec;
 import edu.biu.scapi.circuits.circuit.Gate;
 
 /**
- * See <i>Free XOR Gates and Applications</i> by Validimir Kolesnikov and Thomas
- * Schneider for a full description of the Free XOR technique, pseudocode
- * implementation, as well as the proof of security. As they mention in that
- * paper, a Free XOR gate with n > 2 inputs can be broken down into n-1 2-input
- * XOR gates. Hence, we implement the Free XOR optimization for 2-input XOR
- * Gates, and we leave it to the circuit designer to maximize the number of
- * 2-input XOR gates.
- * <p>
- * The Free XOR optimization allows XOR Gates to be evaluated for free--i.e.
- * without encryption, and thus significantly speeds up Garbled Circuit
- * computations on large circuits with many XOR Gates. The technique dictates a
- * careful way to choose the Wire Values. See the
- * {@link FreeXORGarbledCBooleanCircuit} class where we construct the circuit
- * and choose the values according to this procedure. Once the Wwire values have
- * been chosen as such, evaluating XOR gates does not require encryption. See
- * the {@link #compute(Map)} method in this class where the computation is done.
+ * The Free XOR optimization allows XOR Gates to be evaluated for free -- i.e. without encryption, and thus significantly speeds up Garbled Circuit
+ * computations on large circuits with many XOR Gates. <p>
+ * The technique dictates a careful way to choose the Wire Values. <p>
+ * See the {@link FreeXORGarbledBooleanCircuitUtil} class that construct the circuit and choose the values according to this procedure. 
+ * Once the Wire values have been chosen as such, evaluating XOR gates does not require encryption. 
+ * See the {@link #compute(Map)} method in this class where the computation is done.
  * </p>
  * 
+ * See <i>Free XOR Gates and Applications</i> by Validimir Kolesnikov and Thomas Schneider for a full description of the Free XOR technique, pseudocode
+ * implementation, as well as the proof of security. 
+ * As they mention in that paper, a Free XOR gate with n > 2 inputs can be broken down into n-1 2-input XOR gates. 
+ * Hence, we implement the Free XOR optimization for 2-input XOR Gates, and we leave it to the circuit designer to maximize the number of
+ * 2-input XOR gates. <p>
  * 
  * @author Steven Goldfeder
  * 
  */
-public class FreeXORGate extends AbstractGarbledGate{
+class FreeXORGate implements GarbledGate{
 
-    
-	private static final long serialVersionUID = 3749393621892017604L;
+	/* An array containing the integer labels of the input Wire Labels to this gate. 
+	 * The order of the {@code GarbledWire}s in this array is significant as not all functions are symmetric.
+	 * For example consider the function ~y v x and the following truth table: 
+	 *  x y  ~y v x 
+	 *  0 0    1
+	 *  0 1    0 
+	 *  1 0    1
+	 *  1 1    1
+	 */
+	 protected int[] inputWireLabels;
+	  
+	 //An array containing the integer labels of the output {@code GarbledWire}(s).
+	 protected int[] outputWireLabels;
+	  
+	 /*
+	  * The integer label of this {@code FreeXORGarbledGate}. 
+	  * This label is used to order {@code FreeXORGarbledGate}s in a {@link FreeXORGarbledBooleanCircuitUtil}.
+	  */
+	 private int gateNumber;
 
-/**
-   * @param ungarbledGate
-   *          The ungarbled Gate that is to be Garbled. Since this is an XOR
-   *          Gate using the Free XOR technique, no encryption is required for
-   *          its Garbling. Rather, the {@code GarbledWire} values were
-   *          carefully chosen in the @ code GarbledBooleanCiircuit} . See
-   *          {@code StandardGarbledBooleanCircuit} and {@link #compute(Map)} for the
-   *          technical details on how this is achieved.
-   */
-  public FreeXORGate(Gate ungarbledGate) {
-    inputWireLabels = ungarbledGate.getInputWireLabels();
-    outputWireLabels = ungarbledGate.getOutputWireLabels();
-    numberOfInputs = inputWireLabels.length;
-    numberOfOutputs = outputWireLabels.length;
-    gateNumber = ungarbledGate.getGateNumber();
+	 /**
+	  * Constructs a free XOR garbled gate from an ungarbled gate.<p>
+	  * Since this is an XOR Gate using the Free XOR technique, no encryption is required for its Garbling. 
+	  * Rather, the {@code GarbledWire} values were carefully chosen in the GarbledBooleanCircuit.<p> 
+	  * See {@code FreeXORGarbledBooleanCircuitUtil} and {@link #compute(Map)} for the  technical details on how this is achieved.
+	  * @param ungarbledGate The ungarbled Gate that needs to be Garbled. 
+	  */
+	 FreeXORGate(Gate ungarbledGate) {
+		  inputWireLabels = ungarbledGate.getInputWireLabels();
+		  outputWireLabels = ungarbledGate.getOutputWireLabels();
+		  gateNumber = ungarbledGate.getGateNumber();
+	 }
 
-  }
+	 @Override
+	 public void compute(Map<Integer, GarbledWire> computedWires) {
+	    
+		 /*
+		  * The Free XOR Gate has only 2 inputs. We XOR the input values to obtain the Garbled output value. 
+		  * This is made possible by carefully choosing the garbled values for the {@code Garbled Wires} in the constructor of the
+		  * {@code FreeXORGarbledBooleanCircuitUtil} class. See there for details.
+		  */
+		 byte[] outputValue = computedWires.get(inputWireLabels[0]).getValueAndSignalBit().getEncoded();
+	     byte[] nextInput = computedWires.get(inputWireLabels[1]).getValueAndSignalBit().getEncoded();
+	
+	     // XORing the two input values.
+	     for (int currentByte = 0; currentByte < outputValue.length; currentByte++) {
+	    	 outputValue[currentByte] ^= nextInput[currentByte];
+	     }
+	
+	     SecretKey outputWireValue = new SecretKeySpec(outputValue, "");
+	    
+	     // Create the output GarbledWire(s) and set them with the value we just computed
+	     for (int w : outputWireLabels) {
+	    	 computedWires.put(w, new GarbledWire(outputWireValue));
+	     }
+	     
+	 }
 
-  @Override
-  public void compute(Map<Integer, GarbledWire> computedWires) {
-    byte[] outputValue = computedWires.get(inputWireLabels[0])
-        .getValueAndSignalBit().getEncoded();
-    /*
-     * The Free XOR Gate has only 2 inputs. We XOR the input values to obtain
-     * the Garbled output value. This is made possible by carefully choosing the
-     * garbled values for the {@code Garbled Wires} in the constructor of the
-     * {@code FreeXORGarbledBooleanCircuit} class. See there for details.
-     */
-    byte[] nextInput = computedWires.get(inputWireLabels[1])
-        .getValueAndSignalBit().getEncoded();
+	 @Override
+	 public boolean verify(Gate g, Map<Integer, SecretKey[]> allWireValues) {
+		//Verify that the gate number and input/output labels are the same as the given ungarbled circuit.
+		if (verifyGateComponents(g) == false){
+			return false;
+		}
+	
+	    /*
+	     * Step 3: Since this is a Free XOR Gate, the ungarbled Gate must be an XOR Gate if they are equivalent. 
+	     * Check to see that the truth table is 0110.
+	     */
+	    BitSet ungarbledTruthTable = g.getTruthTable();
+	    if (ungarbledTruthTable.get(0) != false
+	        || ungarbledTruthTable.get(1) != true
+	        || ungarbledTruthTable.get(2) != true
+	        || ungarbledTruthTable.get(3) != false) {
+	    	return false;
+	    }
+	    
+	    /*
+	     * Step 4: Add the values for the output wire(s) to the allWireValues map. 
+	     * This is necessary since the FreeXORGarbledBooleanCircuitUtil's verify method that calls this method needs this map updated since 
+	     * subsequent Gates may have this gate's output Wire as an input Wire and need the values to verify equality.
+	     * (Non FreeXORGate's need both Wire values to verify the that the truth table's are equal).
+	     */
+	
+	    // First get the 0 output value by using the 0-0 values as inputs and XORing them to get the 0-encoded output. 
+	    // The zero value of the 0th GarbledWire in the input array is called zeroZero.
+	    byte[] zeroZero = allWireValues.get(inputWireLabels[0])[0].getEncoded();
+	    
+	    // The zero value of the 1th GarbledWire in the input array is called oneZero.
+	    byte[] oneZero = allWireValues.get(inputWireLabels[1])[0].getEncoded();
+	    
+	    // XOR the values to obtain the result.
+	    byte[] outputZero = new byte[zeroZero.length];
+	    for (int currentByte = 0; currentByte < zeroZero.length; currentByte++) {
+	    	outputZero[currentByte] = (byte) (zeroZero[currentByte] ^ oneZero[currentByte]);
+	    }
+	    
+	    
+	    //Next, get the 1 output value by using the 0-1 values as inputs.
+	    //The one value of the input GarbledWire with index 1 in the inputwireLabels array is called oneOne.
+	    byte[] oneOne = allWireValues.get(inputWireLabels[1])[1].getEncoded();
+	    
+	    //XOR the 0-1 input values to get the output 1-encoded value.
+	    byte[] outputOne = new byte[zeroZero.length];
+	    for (int currentByte = 0; currentByte < zeroZero.length; currentByte++) {
+	      outputOne[currentByte] = (byte) (zeroZero[currentByte] ^ oneOne[currentByte]);
+	    }
+	    
+	    // Put the result in the output wires.
+	    for (int w : outputWireLabels) {
+	      allWireValues.put(w, new SecretKey[] { new SecretKeySpec(outputZero, ""),
+	          new SecretKeySpec(outputOne, "") });
+	    }
+	    return true;
+	 }
 
-    // XORing the two input values
-    for (int currentByte = 0; currentByte < outputValue.length; currentByte++) {
-      outputValue[currentByte] ^= nextInput[currentByte];
-    }
+	 /**
+	  * Verifies that the gate number and input/output labels are the same as the given ungarbled circuit.
+	  * @param g the ungarbled circuit that should be verified.
+	  * @return true if varified; false, otherwise.
+	  */
+	 protected boolean verifyGateComponents(Gate g) {
+		/*
+		 *  Step 1: Test to see that these gate's are labeled with the same integer label. if they're not, then for our purposes they are not
+		 *  identical. The reason that we treat this as unequal is since in a larger circuit corresponding gates must be identically labeled in 
+		 *  order for the circuits to be the same.
+		 */
+		if (gateNumber != g.getGateNumber()) {
+			 return false;
+		 }
 
-    SecretKey outputWireValue = new SecretKeySpec(outputValue, "");
-    // We now create the output GarbledWire(s) and set them with the value we
-    // just computed
-    for (int w : outputWireLabels) {
-      computedWires.put(w, new GarbledWire(w, outputWireValue));
-    }
-
-  }
-
-  @Override
-  public boolean verify(Gate g, Map<Integer, SecretKey[]> allWireValues) {
-    /*
-     * Step 1: First we test to see that these gates are labeled with the same
-     * integer label. if they're not, then for our purposes they are not
-     * identical. The reason that we treat this as unequal is since in a larger
-     * circuit corresponding gates must be identically labeled in order for the
-     * circuits to be the same.
-     */
-    if (gateNumber != g.getGateNumber()) {
-      return false;
-    }
-    /*
-     * Next we check to ensure that the two gates have the same respective
-     * numbers of input and output wires and that the inputWirelabels and
-     * ouputWireLabels are the same
-     */
-    int[] ungarbledInputWireLabels = g.getInputWireLabels();
-    int[] ungarbledOutputWireLabels = g.getOutputWireLabels();
-    // the numbe of inputs should always be 2 since this is a 20input free XOR
-    // gate, but nevertheless we test it
-    if (numberOfInputs != ungarbledInputWireLabels.length
-        || numberOfOutputs != ungarbledOutputWireLabels.length) {
-      return false;
-    }
-    for (int i = 0; i < numberOfInputs; i++) {
-      if (inputWireLabels[i] != ungarbledInputWireLabels[i]) {
-        return false;
-      }
-    }
-    for (int i = 0; i < numberOfOutputs; i++) {
-      if (outputWireLabels[i] != ungarbledOutputWireLabels[i]) {
-        return false;
-      }
-    }
-
-    /*
-     * Since this is a Free XOR Gate, we know that the ungarbled Gate must be an
-     * XOR Gate if they are equivalent. So, we check to see that the truth table
-     * is 0110
-     */
-    BitSet ungarbledTruthTable = g.getTruthTable();
-    if (ungarbledTruthTable.get(0) != false
-        || ungarbledTruthTable.get(1) != true
-        || ungarbledTruthTable.get(2) != true
-        || ungarbledTruthTable.get(3) != false) {
-      return false;
-    }
-    /*
-     * The last step that we must do is add the values for the output wire(s) to
-     * the allWireValues map. This is necessary since the
-     * FreeXORGarbledBooleanCircuit's verify method that calls this method needs
-     * this map updated since subsequent Gates may have this gate's output Wire
-     * as an input Wire and need the values to verify equality.(Non
-     * FreeXORGate's need both Wire values to verify the that the truth table's
-     * are equal)
-     */
-
-    /*
-     * first we get the 0 output value by using the 0-0 values as inputs and
-     * XORing them to get the 0-encoded output I call it zeroZero since its the
-     * zero value of the 0th GarbledWire in the input array
-     */
-    byte[] zeroZero = allWireValues.get(inputWireLabels[0])[0].getEncoded();
-    /*
-     * I call it oneZero since its the zero value of the 1th GarbledWire in the
-     * input array
-     */
-    byte[] oneZero = allWireValues.get(inputWireLabels[1])[0].getEncoded();
-    // We now XOR the values to obtain the result
-    byte[] outputZero = new byte[zeroZero.length];
-    for (int currentByte = 0; currentByte < zeroZero.length; currentByte++) {
-      outputZero[currentByte] = (byte) (zeroZero[currentByte] ^ oneZero[currentByte]);
-    }
-    /*
-     * next we get the 1 output value by using the 0-1 values as inputs oneOne
-     * is the one value of the input GarbledWire with index 1 in the
-     * inputwireLabels array
-     */
-    byte[] oneOne = allWireValues.get(inputWireLabels[1])[1].getEncoded();
-    // we now XOR the 0-1 input values to get the output 1-encoded value
-    byte[] outputOne = new byte[zeroZero.length];
-    for (int currentByte = 0; currentByte < zeroZero.length; currentByte++) {
-      outputOne[currentByte] = (byte) (zeroZero[currentByte] ^ oneOne[currentByte]);
-    }
-    for (int w : outputWireLabels) {
-      allWireValues.put(w, new SecretKey[] { new SecretKeySpec(outputZero, ""),
-          new SecretKeySpec(outputOne, "") });
-    }
-    return true;
-  }
+		// Step 2: Check to ensure that the inputWirelabels and ouputWireLabels are the same.
+	    int[] ungarbledInputWireLabels = g.getInputWireLabels();
+	    int[] ungarbledOutputWireLabels = g.getOutputWireLabels();
+	    int numberOfInputs = inputWireLabels.length;
+	    int numberOfOutputs = outputWireLabels.length;
+	    if (numberOfInputs != ungarbledInputWireLabels.length || numberOfOutputs != ungarbledOutputWireLabels.length) {
+	    	return false;
+	    }
+	    for (int i = 0; i < numberOfInputs; i++) {
+	    	if (inputWireLabels[i] != ungarbledInputWireLabels[i]) {
+	    		return false;
+	    	}
+	    }
+	    for (int i = 0; i < numberOfOutputs; i++) {
+	    	if (outputWireLabels[i] != ungarbledOutputWireLabels[i]) {
+	    		return false;
+	    	}
+	    }
+	    return true;
+	}
+  
+	 @Override
+	 public int[] getInputWireLabels() {
+		 return inputWireLabels;
+	 }
+	
+	 @Override
+	 public int[] getOutputWireLabels() {
+		 return outputWireLabels;
+	 }
+ 
 }
