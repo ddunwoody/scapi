428df9ddebb67b8d2e7a15cada631498276aeeda
Moriya Farbstein <moriyaw@gmail.com>
2014-06-12 09:46:20 +0000

    1. Garble function that accepts partial garbled values (keys) has been deleted. The option to give some of the keys moved to the extended circuit. 2. The return value of garble function with seed changed from CircuitSeedCreationValues to CircuitCreationValues. The hash result can be given by the getHashedCircuit of the extended circuit. 3. The getHashedTables function has been deleted (can be found in the extended circuit). 4. Fixed comments.
    git-svn-id: https://svn.cs.biu.ac.il/svn/development/SDK_branches/NoInitParams@3086 ba1fec17-5bc3-4c1d-a110-6a66ae20c876

--- Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/CircuitTypeUtil.java
+++ Code/JavaSrc/edu/biu/scapi/circuits/garbledCircuit/CircuitTypeUtil.java
@@ -25,18 +25,14 @@
 package edu.biu.scapi.circuits.garbledCircuit;
 
 import java.security.InvalidKeyException;
-import java.util.Map;
-
-import javax.crypto.SecretKey;
 
 import edu.biu.scapi.circuits.circuit.BooleanCircuit;
 import edu.biu.scapi.circuits.circuit.Gate;
-import edu.biu.scapi.primitives.hash.CryptographicHash;
 import edu.biu.scapi.primitives.prg.PseudorandomGenerator;
 
 /**
  * A general interface that contains some functionalities that are different in each circuit type. <p>
- * For example, keys generation is done differently in each circuit type. 
+ * For example, garbling is done differently in each circuit type. 
  * In a Standard garbled circuit all keys are random bits; In a FreeXOR circuit, the keys for each wire are XOR of each other with some delta.
  * 
  * In addition, the gates in each circuit are of different types. So, this interface also contains a function that creates the gates.
@@ -65,25 +61,6 @@ interface CircuitTypeUtil {
 			GarbledGate[] gates);
 	
 	/**
-	 * This method gets part of the keys and generates the missing keys for all the circuit's wires. 
-	 * It then creates the garbled table according to these values.<p>
-	 * This method can receive the keys of the input wires or the keys of the output wires.<p>
-	 * In addition, it gets the signal bits corresponding to the partial wire's keys. 
-	 * In case the given keys are the keys of the output wires, then the given signal bits are actually the translation table.
-	 * 
-	 * @param ungarbledCircuit The circuit that this {@code GarbledBooleanCircuit} is supposed to be a garbling of.
-	 * @param garbledTablesHolder The object that points to the garbledTable.
-	 * @param gates The gates of this circuit.
-	 * @param partialWireValues Can contain the keys of the input wires or the keys of the output wires.
-	 * Note: The least significant bit of the input and the output keys of each wire should be different. 
-	 * That is, for each wire, if the last bit of k0 is 0, the last bit of k1 must be 1. 
-	 * Clearly, the content of the last bit of all k0 should be picked at random. Else, the circuit is not secure. 
-	 * @return CircuitCreationValues contains both generated values for each input and output wire and the translation table.
-	 */
-	public CircuitCreationValues garble(BooleanCircuit ungarbledCircuit, GarbledTablesHolder garbledTablesHolder, 
-			GarbledGate[] gates, Map<Integer, SecretKey[]> partialWireValues) ;
-	
-	/**
 	 * This method generates both keys for each input wire using the given prg and seed. 
 	 * It then creates the garbled table according to these values.<p>
 	 * @param ungarbledCircuit The circuit that this {@code GarbledBooleanCircuit} is supposed to be a garbling of. 
@@ -94,20 +71,7 @@ interface CircuitTypeUtil {
 	 * @return CircuitCreationValues contains both generated values for each input and output wire and the translation table.
 	 * @throws InvalidKeyException in case the seed is an invalid key for the given PRG.
 	 */
-	public CircuitSeedCreationValues garble(BooleanCircuit ungarbledCircuit, GarbledTablesHolder garbledTablesHolder, 
-			GarbledGate[] gates, PseudorandomGenerator prg, byte[] seed, CryptographicHash hash) throws InvalidKeyException;
+	public CircuitCreationValues garble(BooleanCircuit ungarbledCircuit, GarbledTablesHolder garbledTablesHolder, 
+			GarbledGate[] gates, PseudorandomGenerator prg, byte[] seed) throws InvalidKeyException;
 	
-	/**
-	 * Calculates the garbled table of each gate using the given ungarbled circuit, prg and seed. 
-	 * It then updates the hash function with the garbled table and drops it.<p>
-	 * Nothing is kept in memory. 
-	 * @param ungarbledCircuit The circuit that this {@code GarbledBooleanCircuit} is supposed to be a garbling of. 
-	 * @param prg Used to generate both keys for each input wire.
-     * @param seed Used to initialize the given prg.
-     * @param hash CryptographicHash object used to compute the hash function on the circuit's garbled tables.
-	 * @return the result of the hash function on the garbled tables.
-	 * @throws InvalidKeyException in case the seed is an invalid key for the given PRG.
-	 */
-	public byte[] getHashedTables(BooleanCircuit ungarbledCircuit, PseudorandomGenerator prg, byte[] seed, CryptographicHash hash) throws InvalidKeyException;
-		
 }
